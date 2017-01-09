5a791e492447b5bb120661e94247d83b836552e7
August Trometer <august@homehero.org>
2017-01-09 14:36:15 -0800

    Added patient notes table and model

--- resources/views/dashboard/dashboard_practitioner.blade.php
+++ resources/views/dashboard/dashboard_practitioner.blade.php
@@ -30,9 +30,20 @@
 @if (count($recent_tests) > 0)
 <h3>Recent Tests</h3>
 <table class="table">
+    <tr>
+        <th>Test</th>
+        <th>Results</th>
+    </tr>
     @foreach ($recent_tests as $test)
         <tr>
             <td>{{ $test->name }}</td>
+            <td>
+                @if($test->results_url)
+                    $test->tempURL()
+                @else
+                    Pending
+                @endif
+            </td>
         </tr>
     @endforeach
 </table>
