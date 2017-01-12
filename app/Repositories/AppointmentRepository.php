f26de7c4e1049befb12c07e4197f04e178250782
August Trometer <august@homehero.org>
2017-01-12 15:09:09 -0800

    Appointment controller and repo

---
+++ app/Repositories/AppointmentRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Appointment;
+
+
+class AppointmentRepository extends BaseRepository
+{
+    protected $model = \App\Models\Appointment::class;
+}
+
