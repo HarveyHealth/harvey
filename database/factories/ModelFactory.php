cb9e3a8a431b3272326836ae4866b6c9df9dfcfb
Jose Soto <jose@homehero.org>
2017-01-12 20:10:26 -0800

    Adds model factories
    Users with states
    Appointments with states
    Tests

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,13 @@ use Carbon\Carbon;
 |
 */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
 
     static $password;
 
     return [
+        'enabled' => true,
+        'user_type' => $faker->randomElement(['admin', 'patient', 'practitioner']),
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'enabled' => true,
@@ -35,5 +36,46 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'longitude' => $faker->longitude,
         'timezone' => $faker->timezone,
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->state(App\Models\User::class, 'admin', function ($faker) {
+    return ['user_type' => 'admin'];
+});
+
+$factory->state(App\Models\User::class, 'patient', function ($faker) {
+    return ['user_type' => 'patient'];
+});
+
+$factory->state(App\Models\User::class, 'practitioner', function ($faker) {
+    return ['user_type' => 'practitioner'];
+});
+
+$factory->define(App\Models\SKU::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'item_type' => $faker->randomElement(['test', 'product', 'service']),
+        'price' => $faker->randomNumber(2),
+    ];
+});
+
+$factory->define(App\Models\Appointment::class, function (Faker\Generator $faker) {
+    return [
+        'patient_user_id' => factory(App\Models\User::class)->states('patient')->create()->id,
+        'practitioner_user_id' => factory(App\Models\User::class)->states('practitioner')->create()->id,
+        'appointment_at' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+7 days'),
+        'reason_for_visit' => $faker->sentence
+    ];
+});
+
+$factory->state(App\Models\Appointment::class, 'past', function ($faker) {
+    return ['appointment_at' => $faker->dateTimeBetween($startDate = '-3 days', $endDate = '-1 days'),];
+});
+
+$factory->define(App\Models\Test::class, function (Faker\Generator $faker) {
+    return [
+        'patient_user_id' => factory(App\Models\User::class)->states('patient')->create()->id,
+        'practitioner_user_id' => factory(App\Models\User::class)->states('practitioner')->create()->id,
+        'sku_id' => factory(App\Models\SKU::class)->create()->id
     ];
 });
