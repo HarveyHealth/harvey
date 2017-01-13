e2382ea9fb6f1447e64815dd0d1ad50688831c51
August Trometer <august@homehero.org>
2017-01-13 11:19:18 -0800

    Updates TestsSeeder

--- database/seeds/TestsTableSeeder.php
+++ database/seeds/TestsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class TestsTableSeeder extends Seeder
 {
@@ -11,6 +12,16 @@ class TestsTableSeeder extends Seeder
      */
     public function run()
     {
+        $patient = User::where('user_type', 'patient')->first();
+        $practitioner = User::where('user_type', 'practitioner')->first();
+
+        // create some records with specific users
+        factory(App\Models\Test::class, 10)->create([
+            'patient_user_id' => $patient->id,
+            'practitioner_user_id' => $practitioner->id
+        ]);
+
+        // then create some random ones
         factory(App\Models\Test::class, 10)->create();
     }
 }
