f66211e6d31523cdf5d4b2cd08da152294d61435
Jose Soto <jose@homehero.org>
2017-01-17 10:07:11 -0800

    Adds eager loading to reduce DB calls

--- app/Http/Traits/HasPatientAndPractitioner.php
+++ app/Http/Traits/HasPatientAndPractitioner.php
@@ -16,11 +16,11 @@ trait HasPatientAndPractitioner
 
     public function scopeForPatient($query, $patient_id)
     {
-        return $query->where('patient_user_id', $patient_id);
+        return $query->where('patient_user_id', $patient_id)->with('patient');
     }
 
     public function scopeForPractitioner($query, $practitioner_id)
     {
-        return $query->where('practitioner_user_id', $practitioner_id);
+        return $query->where('practitioner_user_id', $practitioner_id)->with('practitioner');
     }
 }
