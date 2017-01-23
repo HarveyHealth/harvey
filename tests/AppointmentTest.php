2911bccbc0cf49a3b81dcbd5031245887a4d2441
August Trometer <august@homehero.org>
2017-01-23 11:51:07 -0800

    Added appointment scope tests

--- tests/AppointmentTest.php
+++ tests/AppointmentTest.php
@@ -19,11 +19,12 @@ class AppointmentTest extends TestCase
     {
         parent::setUp();
         Artisan::call('migrate');
+
         $this->patient = factory(User::class)->create();
         $this->practitioner = factory(User::class)->create();
 
         // Create 5 past appointments for our test patient and practitioner
-        factory(Appointment::class, 2)->states('past')->create([
+        factory(Appointment::class, 5)->states('past')->create([
             'patient_user_id' => $this->patient->id,
             'practitioner_user_id' => $this->practitioner->id
         ]);
@@ -38,18 +39,42 @@ class AppointmentTest extends TestCase
         factory(Appointment::class, 3)->states('past')->create();
 
         // make 5 future appointments for other people
-        factory(Appointment::class, 5)->states('past')->create();
+        factory(Appointment::class, 5)->create();
     }
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_it_scopes_upcoming_correctly()
     {
         $upcoming = Appointment::upcoming();
 
-        $this->assertEquals($upcoming->count(), 1);
+        // this tests for all upcoming appointments
+        $this->assertEquals($upcoming->count(), 6);
+
+        // this tests for upcoming appointments for our patient
+        $upcoming_for_patient = $upcoming->forPatient($this->patient->id);
+
+        $this->assertEquals($upcoming_for_patient->count(), 1);
+
+        // this tests for upcoming appointments for our practitioner
+        $upcoming_for_practitioner = $upcoming->forPractitioner($this->practitioner->id);
+
+        $this->assertEquals($upcoming_for_practitioner->count(), 1);
+    }
+
+    public function test_it_scopes_recent_correctly()
+    {
+        $recent = Appointment::recent(50);
+
+        // this tests for all recent appointments
+        $this->assertEquals($recent->count(), 8);
+
+        // this tests for recent appointments for our patient
+        $recent_for_patient = $recent->forPatient($this->patient->id);
+
+        $this->assertEquals($recent_for_patient->count(), 5);
+
+        // this tests for recent appointments for our practitioner
+        $recent_for_practitioner = $recent->forPractitioner($this->practitioner->id);
+
+        $this->assertEquals($recent_for_practitioner->count(), 5);
     }
 }
