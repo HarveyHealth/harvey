dd6a6a13288ef4286d1bb950d7e3b90272dc1929
Jose Soto <jose@homehero.org>
2017-01-23 09:20:47 -0800

    Appointment Test

--- tests/AppointmentTest.php
+++ tests/AppointmentTest.php
@@ -15,31 +15,30 @@ class AppointmentTest extends TestCase
     protected $patient;
     protected $practitioner;
 
-    function setUpDB()
+    function setUp()
     {
-        if ($this->patient)
-            return;
-
-        $this->patient = factory(User::class)->make();
-        $this->practitioner = factory(User::class)->make();
+        parent::setUp();
+        Artisan::call('migrate');
+        $this->patient = factory(User::class)->create();
+        $this->practitioner = factory(User::class)->create();
 
         // Create 5 past appointments for our test patient and practitioner
-        factory(Appointment::class, 2)->states('past')->make([
+        factory(Appointment::class, 2)->states('past')->create([
             'patient_user_id' => $this->patient->id,
             'practitioner_user_id' => $this->practitioner->id
         ]);
 
         // make 1 upcoming appointment
-        factory(Appointment::class)->make([
+        factory(Appointment::class)->create([
             'patient_user_id' => $this->patient->id,
             'practitioner_user_id' => $this->practitioner->id
         ]);
 
         // make 3 past appointments for other people
-        factory(Appointment::class, 3)->states('past')->make();
+        factory(Appointment::class, 3)->states('past')->create();
 
         // make 5 future appointments for other people
-        factory(Appointment::class, 5)->states('past')->make();
+        factory(Appointment::class, 5)->states('past')->create();
     }
 
     /**
@@ -49,10 +48,8 @@ class AppointmentTest extends TestCase
      */
     public function test_it_scopes_upcoming_correctly()
     {
-        $this->setUpDB();
-
         $upcoming = Appointment::upcoming();
 
-        $this->assertEquals($upcoming->count(), 6);
+        $this->assertEquals($upcoming->count(), 1);
     }
 }
