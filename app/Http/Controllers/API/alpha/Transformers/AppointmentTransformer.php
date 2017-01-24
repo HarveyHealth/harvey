cfb950673915dc2f1791f7eb18f0c91d71a94a15
Jose Soto <jose@homehero.org>
2017-01-24 07:13:34 -0800

    Updated appointment transformer

--- app/Http/Controllers/API/alpha/Transformers/AppointmentTransformer.php
+++ app/Http/Controllers/API/alpha/Transformers/AppointmentTransformer.php
@@ -7,6 +7,7 @@ class AppointmentTransformer extends Transformer
     public function transform($appointment)
     {
         return [
+            'id' => $appointment->id,
             'appointment_at' => $appointment->appointment_at,
             'reason_for_visit' => $appointment->reason_for_visit,
             'created_at' => $appointment->created_at
