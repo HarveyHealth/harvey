87f6fc6e6d6eacf573550a02b9240260a99b072a
Jose Soto <jose@homehero.org>
2017-01-27 09:32:11 -0800

    HAR-27 #send-to-qa Removes redirect middleware for legal pages

--- app/Http/Controllers/LegalController.php
+++ app/Http/Controllers/LegalController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class LegalController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-    
     public function terms()
     {
-        $html = html_from_markdown_file(resource_path() . '/views/legal/markdown/terms.md');
+        $html = html_from_markdown_file(resource_path('/views/legal/markdown/terms.md'));
 
         return view('legal.legal')->with('html', $html)->with('page_title', 'Terms and Conditions');
     }
 
     public function privacy()
     {
-        $html = html_from_markdown_file(resource_path() . '/views/legal/markdown/privacy.md');
+        $html = html_from_markdown_file(resource_path('/views/legal/markdown/privacy.md'));
 
         return view('legal.legal')->with('html', $html)->with('page_title', 'Privacy Policy');
     }
