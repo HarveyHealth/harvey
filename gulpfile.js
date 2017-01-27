35eaac92db792284542de3423f3be516ce11f5ea
Kan Dong <kan@homehero.org>
2017-01-26 16:15:50 -0800

    Turn off vue debug tool for production build.

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,22 @@ const elixir = require('laravel-elixir');
 require('laravel-elixir-vue-2');
 require('laravel-elixir-imagemin');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
-
 elixir((mix) => {
     mix.sass('app.scss')
-       .webpack('app_public.js')
-       .webpack('app_logged_in.js', null, null, {
-           resolve: {
-               alias: {
-                   'vue$': 'vue/dist/vue.js'
-               }
-           },
-           devtool: '#source-map'
-       })
-       .imagemin({
-        optimizationLevel: 3,
-        progressive: true,
-        interlaced: true
-       })
-       .scriptsIn('resources/assets/js/scripts', 'public/js/vendors.js')
-       .browserSync({proxy: 'https://harvey.app'});
+        .webpack('app_public.js')
+        .webpack('app_logged_in.js', null, null, {
+            resolve: {
+                alias: {
+                    'vue$': 'vue/dist/vue.js'
+                }
+            },
+            devtool: '#source-map'
+        })
+        .imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })
+        .scriptsIn('resources/assets/js/scripts', 'public/js/vendors.js')
+        .browserSync({proxy: 'https://harvey.app'});
 });
