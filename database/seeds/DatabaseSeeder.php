6477f38b36d4648627346a0fdac2112541231030
Jose Soto <jose@homehero.org>
2017-01-26 19:04:34 -0800

    Updates seeder output

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
 
         $this->command->getOutput()->writeln("Seeding Successful!");
         $this->command->getOutput()->writeln("");
-        $this->command->getOutput()->writeln("<info>Admin Email: </info>admin@goharvey.co");
-        $this->command->getOutput()->writeln("<info>Patient Email: </info> patient@goharvey.co");
-        $this->command->getOutput()->writeln("<info>Practitioner Email: </info> practitioner@goharvey.co");
+        $this->command->getOutput()->writeln("<info>Admin Email: </info>admin@goharvey.com");
+        $this->command->getOutput()->writeln("<info>Patient Email: </info> patient@goharvey.com");
+        $this->command->getOutput()->writeln("<info>Practitioner Email: </info> practitioner@goharvey.com");
         $this->command->getOutput()->writeln("<info>All user passwords: </info> secret");
     }
 }
