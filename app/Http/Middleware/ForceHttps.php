7dd642ee7e9041910d3227e1a466c0e1f185a02d
Jose Soto <jose@homehero.org>
2017-02-11 14:30:11 -0800

    Force www on https

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -15,14 +15,13 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
-
-        if(starts_with($request->root(), "http://goharvey.com")){
-            return redirect()
-                ->to('https://www.goharvey.com' . $request->getRequestUri(), 301);
-        }
-
         if (!$request->secure() && !\App::environment('local', 'testing')) {
             return redirect()->secure($request->getRequestUri());
+        }
+
+        if(starts_with($request->root(), "https://goharvey.com")){
+            return redirect()
+                ->to('https://www.goharvey.com' . $request->getRequestUri(), 301);
         }
 
         return $next($request);
