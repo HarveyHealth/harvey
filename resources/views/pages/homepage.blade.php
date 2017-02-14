d743a500177a35cc19235ff187229a229a29bc32
Kyle Hill <kyle@homehero.org>
2017-02-14 02:46:57 -0800

    Hide icons from homepage tab table

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -270,27 +270,27 @@
             <h2 class="title is-4 section-header"><span>Learn the Facts</span></h2>
             <vertical-tabs>
                 <vertical-tab class="content" label="Doctors and nutrition">
-                    <img class="is-pulled-right" src="/images/facts/doctors_nutrition.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/doctors_nutrition.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Doctors and nutrition</strong></h3>
                     <p>When was the last time you talked to your doctor for longer than 5 minutes about your diet and nutrition? It's probably been awhile. The reason is that medical doctors get less than 24 hours of coursework related to nutrition in medical school, and they <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2779722/" alt="US National Library of Medicine National Institutes of Health">will admit themselves</a> that they are not properly trained to talk to their patients about nutrition.</p>
                     <p>Lab tests play an important role in your health journey. These tests give doctors valuable insights into what’s happening in your body. Unfortunately, it’s rare for medical doctors to recommend anything other than a handful of basic lab tests, mainly because they lack the specialized training necessary to interpret the lab results and they lack knowledge about diet nutrition to help you troubleshoot health issues through a naturopathic lens.</p>
                     <p>Additionally, medical doctors will typically use a population of sick people as the benchmark when determining if their patients are healthy and properly nourished, instead of optimizing for perfect health.</p>
                 </vertical-tab>
                 <vertical-tab class="content" label="Preventable deaths">
-                    <img class="is-pulled-right" src="/images/facts/leading_cause_of_death.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/leading_cause_of_death.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Preventable deaths</strong></h3>
                     <p>At least a third of deaths each year in the U.S. <a href="https://www.cdc.gov/media/releases/2014/p0501-preventable-deaths.html" alt="Center for Disease Control">could have been prevented</a> with better nutrition and lifestyle changes. An estimated 9 out of every 10 Americans are considered malnourished — with significant vitamin or mineral deficiencies or toxic metals and chemicals in their blood. It’s no surprise that 50% of people who suffer from heart attacks had normal cholesterol numbers, and the root cause was some other nutritional imbalance that was overlooked.</p>
                     <p>Naturopathic and integrative medicine play a critical role in properly identifying nutritional deficiencies and other root causes of symptoms that can eventually lead to more serious conditions such as heart disease, cancer, stroke, respiratory disease (COPD) and diabetes (responsible for over 50% of deaths in the U.S. each year).</p>
                 </vertical-tab>
                 <vertical-tab class="content" label="Specialized lab tests">
-                    <img class="is-pulled-right" src="/images/facts/specialized_lab_testing.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/specialized_lab_testing.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Specialized lab tests</strong></h3>
                     <p>Your body has thousands of biological markers. A large majority of chronic symptoms can be linked to imbalances in these biomarkers, such as vitamins, minerals, amino acids, toxic metals and chemicals, allergies, digestive chemicals and hormone imbalances.</p>
                     <p>These imbalances can be easily identified using a combination of conventional and specialized laboratory tests that can help your doctor evaluate functional disorders that may have been missed or misdiagnosed by medical doctors.</p>
                     <p>Because these tests are not offered by the giant lab testing companies like Quest or LabCorp who serve 90% of health systems, they are not typically recommended to patients. These tests are one of the most important tools utilized by integrative physicians to help combat preventable diseases.</p>
                 </vertical-tab>
                 <vertical-tab class="content" label="Clinical evidence">
-                    <img class="is-pulled-right" src="/images/facts/clinical_evidence.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/clinical_evidence.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Clinical evidence</strong></h3>
                     <p>The concept of holistic wellness — empowering the body’s self-healing mechanisms through natural therapies — has been around for thousands of years. But now naturopathic remedies are being supported by clinical studies, and being published in real medical journals. And as more trained medical professionals turn to the practice of integrative medicine, it's becoming easier for patients to separate the real from the fake.</p>
                     <p>Below are some of the thousands of studies that prove, through actuarial studies, the profound benefits of naturopathic medicine:</p>
@@ -307,7 +307,7 @@
                     <p>These medical studies are fundamentally changing the world’s perception about complementary and alternative therapies — for the better. As proof, <a href="http://www.webmd.com/diet/features/vitamins-supplements-test-your-knowledge" alt="WebMD">72% of medical doctors</a> now turn to various forms of naturopathic treatments themselves.</p>
                 </vertical-tab>
                 <vertical-tab class="content" label="Naturopathic doctors">
-                    <img class="is-pulled-right" src="/images/facts/naturopathic_doctors.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/naturopathic_doctors.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Naturopathic doctors</strong></h3>
                     <p>Different from "naturopaths", Naturopathic Doctors (NDs) graduated from an accredited four-year residential naturopathic medical school and passed an extensive board examination. They must receive a license from their practicing state and complete 60 hours of continuing education every 2 years to maintain their license.</p>
                     <p>NDs take a highly personalized approach to medicine and treat all forms of health concerns — ranging from pediatric to geriatric, irritating symptoms to chronic illness, and physical to psychological. They emphasize prevention and self-healing through the use of evidence-based natural therapies.</p>
@@ -323,14 +323,14 @@
 
                 </vertical-tab>
                 <vertical-tab class="content" label="Dangers of multivitamins">
-                    <img class="is-pulled-right" src="/images/facts/identifying_quackery.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/identifying_quackery.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Dangers of multivitamins</strong></h3>
                     <p>Eating one multivitamin and thinking you're healthy is like reading one book and thinking you're smart. Contrary to popular belief, one size does not fit all when it comes to vitamins and supplements. There is not one universal health treatment that is best for everyone, nor is there a single diet or nutritional plan that works for everyone.</p>
                     <p>Therefore, the daily multivitamin you are taking is probably a waste of money. It's a ridiculous assumption that all men, women or children can take the same single pill and optimize for health. What if you’re a high-endurance athlete? Or a diabetic suffering from an autoimmune disease? What if you’re pregnant?</p>
                     <p>Our bodies are extremely unique and complex, with thousands of biomarkers and interlocking parts. That's why we individualized recommendations.</p>
                 </vertical-tab>
                 <vertical-tab class="content" label="Tainted vitamins">
-                    <img class="is-pulled-right" src="/images/facts/identifying_quackery.jpg" alt="">
+                    <!-- <img class="is-pulled-right" src="/images/facts/identifying_quackery.jpg" alt=""> -->
                     <h3 class="title is-4"><strong>Tainted vitamins and supplements</strong></h3>
                     <p>People like making their own decisions about treatments. That's totally understandable. The problem is, most people aren’t very good at being their own doctor.</p>
                     <p>There are thousands of vitamin and supplement products on the market. Virtually <a href="http://www.health.harvard.edu/blog/fda-needs-stronger-rules-to-ensure-the-safety-of-dietary-supplements-201202024182" alt="Harvard article">unregulated by the FDA</a>, most dietary supplements are tainted with GMOs, steroids, synthetic fillers, colorants, contaminants and other toxic ingredients. But that doesn’t stop people. Over 50% of Americans takes at least one vitamin or supplement every day, spending $35 billion a year. And 80% of them are taken without any recommendation from a medical professional.</p>
