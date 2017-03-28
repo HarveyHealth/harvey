9bde0e484f2db42855e372d599cfd17808ce1ea7
Ryan Buttrey <ryan@ryanbuttrey.com>
2017-03-28 15:25:08 -0400

    styles for the practitioner step

--- resources/assets/js/components/signup/_components/Practitioner.vue
+++ resources/assets/js/components/signup/_components/Practitioner.vue
@@ -17,7 +17,46 @@
     <div class="container">
       <div class="signup-form-container large">
         <form @submit.prevent="nextStep('registration')">
-          <div class="input-wrap">
+
+          <div class="flex-wrapper">
+            <div class="input-wrap radio-block">
+              <input
+                type="radio" name="practitioner"
+                id="naturopathic" class="radio_block input-hidden" />
+              <label class="block" for="naturopathic">
+                <div class="radio-block_container">
+                  <h2 class="header-large text-centered">Naturopathic Doctor</h2>
+                  <p class="practitioner-main-content text-centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.</p>
+                  <ul class="practitioner-features">
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                  </ul>
+                  <p class="text-centered"><em class="bold">$150</em> per hour</p>
+                </div>
+              </label>
+            </div>
+
+            <div class="input-wrap radio-block">
+              <input
+                type="radio" name="practitioner"
+                id="osteopathy" class="radio_block input-hidden" />
+              <label class="block" for="osteopathy">
+                <div class="radio-block_container">
+                  <h2 class="header-large text-centered">Doctor of Osteopathy</h2>
+                  <p class="practitioner-main-content text-centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.</p>
+                  <ul class="practitioner-features">
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                  </ul>
+                  <p class="text-centered"><em class="bold">$150</em> per hour</p>
+                </div>
+              </label>
+            </div>
+          </div>
+
+          <div class="text-centered">
             <input type="submit" class="btn btn-default" value="Continue">
           </div>
         </form>
