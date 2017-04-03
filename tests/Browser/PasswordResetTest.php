0f04fd29678bd217e5345f918bae8be7418bda05
Alejandro <alexx.vazquezz@gmail.com>
2017-04-03 16:20:55 -0700

    modified test

--- tests/Browser/PasswordResetTest.php
+++ tests/Browser/PasswordResetTest.php
@@ -9,15 +9,16 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class PasswordResetTest extends DuskTestCase
 {
     use DatabaseMigrations;
-    
+
     public function test_a_user_can_request_a_password_reset_link()
     {
         $user = factory(User::class)->create();
-        
+
         $this->browse(function ($browser) use ($user) {
             $browser->visit('/password/reset')
                 ->type('email', $user->email)
                 ->press('Send Password Reset Link')
+                ->waitForText('We have e-mailed your password reset link!')
                 ->assertSee('We have e-mailed your password reset link!');
         });
     }
