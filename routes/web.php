5d5feb0a1a5f7a88641663449a1746715a5a874a
Ryan Buttrey <ryan@ryanbuttrey.com>
2017-04-04 10:03:24 -0400

    merging routes

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Routing\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -7,10 +8,39 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "Web" middleware group. Build something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// AUTHENTICATION
+Auth::routes();
+
+// Additional routing to not use Laravel's built-in "register" route
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register');
+Route::get('verify/{user_id}/{token}', 'Auth\EmailVerificationController@verify')->middleware('guest');
+Route::post('verify/{user_id}/{token}', 'Auth\EmailVerificationController@setPassword')->middleware('guest');
+
+// basic public pages
+Route::get('/', 'PagesController@getHomepage')->name('home');
+Route::get('lab-tests', 'PagesController@getLabTests');
+
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('api/dashboard', 'DashboardController@index');
+Route::get('api/user', 'DashboardController@getUser');
+
+// INVITE USERS
+Route::get('invite', 'InviteController@getInvite');
+Route::post('invite', 'InviteController@postInvite');
+
+// TERMS
+Route::get('terms', 'LegalController@terms');
+Route::get('privacy', 'LegalController@privacy');
+
+// SITEMAP
+Route::get('sitemap.xml', 'SitemapController@index');
+Route::get('sitemap-{map?}.xml', 'SitemapController@index');
+
+if (App::environment('local')) {
+    Route::get('test', 'TestController@index');
+}
