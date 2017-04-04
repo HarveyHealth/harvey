fe93f28bd4e19b8300ec3bb19b21c8d6cd9b6d68
Ryan Buttrey <ryan@ryanbuttrey.com>
2017-04-04 13:43:26 -0400

    pulling back in this file

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'alpha', 'middleware' => 'auth:api'], function () {
+    Route::resource('users', 'API\alpha\UsersController');
+    Route::resource('appointments', 'API\alpha\AppointmentsController');
+    Route::post('tests/{test}/results', 'API\alpha\TestsController@results');
+    Route::resource('tests', 'API\alpha\TestsController');
+});
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
+    Route::get('tests/{test}', 'API\V1\TestsController@show')->name('tests.show');
+    Route::post('tests/{test}/results', 'API\V1\TestsController@results')->name('test.results');
+
+    Route::get('users/{user}', 'API\V1\UsersController@show')->name('users.show');
+    Route::patch('users/{user}', 'API\V1\UsersController@update')->name('users.update');
+
+    Route::get('patients/{patient}', 'API\V1\PatientsController@show')->name('patients.show');
+    Route::patch('patients/{patient}', 'API\V1\PatientsController@update')->name('patients.update');
+
+    Route::get('appointments', 'API\V1\AppointmentsController@index')->name('appointments.index');
+    Route::get('appointments/{appointment}', 'API\V1\AppointmentsController@show')->name('appointments.show');
+    Route::post('appointments', 'API\V1\AppointmentsController@store')->name('appointments.store');
+
 });
