1611c36385187b24e4915f407a5c1fbcc2caf5c2
Ryan Buttrey <ryan@ryanbuttrey.com>
2017-04-05 10:44:19 -0400

    removing old signup pages and componenets

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,7 +2,9 @@ import './bootstrap';
 
 import Signup from './App.vue'
 
-new Vue({
-  el: '#signup',
-  render: h => h(Signup)
-});
+if (document.querySelector('#signup')) {
+  new Vue({
+    el: '#signup',
+    render: h => h(Signup)
+  });
+}
