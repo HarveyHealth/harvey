843ae464887faaad5ae8ca5c339fa1efb39eeaa1
Andrew Rossi <aroccorossi@gmail.com>
2017-04-07 10:50:40 -0400

    creating empty interstitial and GetStarted component

---
+++ resources/assets/js/pages/routes.js
@@ -0,0 +1,22 @@
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/signup',
+      component: require('./signup/Signup.vue')
+    },
+    {
+      path: '/confirmation',
+      component: require('./signup/Confirmation.vue')
+    },
+    {
+      path: '/get-started',
+      component: require('./signup/GetStarted.vue')
+    }
+  ]
+})
+
+export default router
--- resources/assets/js/pages/signup/main.js
+++ resources/assets/js/pages/signup/main.js
@@ -1,5 +1,5 @@
 import '../../bootstrap';
-import router from './routes';
+import router from '../routes';
 
 import Signup from './Signup.vue'
 
