114559f90679e955cf6d8d04e3b586dde295fdc4
Jose Soto <josecanhelp@gmail.com>
2017-04-12 14:29:16 -0700

    Implements Welcome Email via User Model Observer and Event Call

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Postmark\PostmarkClient;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    /**
+     * @param UserRegistered $event
+     */
+    public function handle(UserRegistered $event)
+    {
+        try{
+            $template_model = [
+                'name' => $event->user->fullName(),
+                'action_url' => url(route('scheduler'))
+            ];
+            
+            $client = new PostmarkClient(config('services.postmark.token'));
+            
+            $client->sendEmailWithTemplate(
+                config('services.postmark.signature'),
+                $event->user->email,
+                1450461, // Postmark Template ID for welcome email
+                $template_model
+            );
+        } catch(\Exception $exception){
+            \Log::error("Unable to send email to {$event->user->email}.", [$exception->getMessage()]);
+        }
+        
+    }
+}
