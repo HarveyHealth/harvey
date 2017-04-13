db7477e467e768288cb1462657ab27fec5826c11
Jose Soto <josecanhelp@gmail.com>
2017-04-13 13:18:53 -0700

    Fixes spacing.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const { mix } = require('laravel-mix');
 
 mix.options({
         processCssUrls: false
-    })
-    .js('resources/assets/js/pages/signup/main.js', 'public/js/signup')
+   })
+   .js('resources/assets/js/pages/signup/main.js', 'public/js/signup')
    .js('resources/assets/js/pages/schedule/main.js', 'public/js/schedule')
    .js('resources/assets/legacy/js/app_public.js', 'public/legacy/js')
    .js('resources/assets/legacy/js/app_logged_in.js', 'public/legacy/js')
