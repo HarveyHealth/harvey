33e3e40d054392fa4b5c36e7a3a6a21d0fbc6c88
Andrew Rossi <aroccorossi@gmail.com>
2017-04-14 10:06:05 -0400

    building date grid

--- resources/assets/js/pages/schedule/_components/_DatePicker.vue
+++ resources/assets/js/pages/schedule/_components/_DatePicker.vue
@@ -1,17 +1,39 @@
 <template>
   <ul class="calendar-week-container_days-wrapper">
-    <li class="calendar-item"><button class="calendar-item_link">Mon</button></li>
+    <li v-for="date in dates" class="calendar-item">
+      <button class="calendar-item_link"></button>
+    </li>
+    <!-- <li class="calendar-item"><button class="calendar-item_link">Mon</button></li>
     <li class="calendar-item"><button class="calendar-item_link">Tue</button></li>
     <li class="calendar-item"><button class="calendar-item_link">Wed</button></li>
     <li class="calendar-item"><button class="calendar-item_link">Thu</button></li>
     <li class="calendar-item"><button class="calendar-item_link">Fri</button></li>
     <li class="calendar-item"><button class="calendar-item_link">Sat</button></li>
-    <li class="calendar-item selected"><button class="calendar-item_link">Sun</button></li>
+    <li class="calendar-item selected"><button class="calendar-item_link">Sun</button></li> -->
   </ul>
 </template>
 
 <script>
+  import moment from 'moment';
+
   export default {
+    props: ['selectedDate', 'maximumDays', 'startDateTime'],
     name: 'DatePicker',
+    methods: {
+
+    },
+    computed: {
+      dates() {
+        const dates = new Array();
+        let currentDate = moment(this.startDateTime).local();
+
+        for (var i = 0; i < this.maximumDays; i++) {
+          dates.push(currentDate);
+          currentDate = moment(this.startDateTime).local().add(i, 'days');
+        }
+
+        return dates;
+      }
+    }
   }
 </script>
