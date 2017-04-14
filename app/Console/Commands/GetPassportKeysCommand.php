3e355463522d0ce21266de9ba4ce4a0118cf1939
Jose Soto <josecanhelp@gmail.com>
2017-04-13 18:06:03 -0700

    Minor adjustments

--- app/Console/Commands/GetPassportKeysCommand.php
+++ app/Console/Commands/GetPassportKeysCommand.php
@@ -7,25 +7,10 @@ use Illuminate\Support\Facades\Storage;
 
 class GetPassportKeysCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'passport:getkeys';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    
     protected $description = 'Downloads Harvey Passport Keys from S3.';
     
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         try {
