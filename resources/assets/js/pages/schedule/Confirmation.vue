c80dbea6af9b46ca51698ede2d0b42596686d45b
Andrew Rossi <aroccorossi@gmail.com>
2017-04-17 16:12:58 -0400

    confirmation page shows real date

--- resources/assets/js/pages/schedule/Confirmation.vue
+++ resources/assets/js/pages/schedule/Confirmation.vue
@@ -6,8 +6,8 @@
 
       <h1 class="header-xlarge">{{ title }}</h1>
 
-      <p class="confirmation_date"><span class="confirmation_day">Thursday, March 30th</span> at <span class="confirmation_time">5:00pm</span> <button class="confirmation_calendar-add"><img src="/images/signup/calendar-add.png" alt=""></button></p>
-      <a class="confirmation_reschedule" href="#">Reschedule</a>
+      <p class="confirmation_date"><span class="confirmation_day">{{date.format('dddd')}}, {{date.format('MMMM')}} {{date.format('Do')}}</span> at <span class="confirmation_time">{{time}}</span> <button class="confirmation_calendar-add"><img src="/images/signup/calendar-add.png" alt=""></button></p>
+      <!-- <a class="confirmation_reschedule" href="#">Reschedule</a> -->
       <p class="confirmation_text large">{{ subtitle }}</p>
       <div class="text-centered">
         <a :href="intakeUrl" class="button">Start Intake Form</a>
@@ -26,13 +26,19 @@
       return {
         title: 'Your appointment is confirmed!',
         subtitle: 'We just sent you a text message and email confirmation — make sure you received them both. Please note, before talking with your doctor, you must complete our patient intake form (link below).',
-        intakeUrl: `https://goharvey.intakeq.com/new/Qqy0mI/DpjPFg?harveyID=${Laravel.user.userId}`,
+        intakeUrl: `https://goharvey.intakeq.com/new/Qqy0mI/DpjPFg?harveyID=${Laravel.user.id}`,
       }
     },
     computed: {
-      formattedDate() {
+      time() {
+        const timeObject = moment(this.appointmentDate).format('HH:mm a');
+        return timeObject;
+      },
+
+      date() {
         // console.log(moment('2017-04-17 07:00:00').format('MMMM'));
-        return moment(this.appointmentDate);
+        const dateObject = moment(this.appointmentDate);
+        return dateObject;
       }
     }
   }
