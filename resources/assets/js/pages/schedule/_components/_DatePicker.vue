058b1eaa29a5f28365cbfea0117a1ceaff9befc2
Andrew Rossi <aroccorossi@gmail.com>
2017-04-17 10:31:28 -0400

    adding dynamic week range display

--- resources/assets/js/pages/schedule/_components/_DatePicker.vue
+++ resources/assets/js/pages/schedule/_components/_DatePicker.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="calendar-week-container">
+
     <div class="calendar-week-container_title-wrapper">
       <h3 class="calendar-week-container_title">This week</h3>
-      <span class="calendar-week-container_date">March 27 - April 2nd</span>
+      <span class="calendar-week-container_date">
+        {{getWeekWithOffset().start}} - {{getWeekWithOffset().end}}</span>
     </div>
+
     <ul class="calendar-week-container_days-wrapper">
       <li v-for="date in dates" class="calendar-item" :class="[{'selected' : selectedDate.day() === date.day()}]">
         <button class="calendar-item_link" @click.prevent="onDateChange(date)">
@@ -28,6 +31,16 @@
         const dateValue = moment(date).utc();
         this.$eventHub.$emit('datetime-change', {type: 'date', value: dateValue});
       },
+
+      getWeekWithOffset(_offset = 0) {
+        const weekObject = {
+          start: moment().add(_offset, 'days').startOf('isoweek').format('MMMM D'),
+          end: moment().add(_offset, 'days').endOf('isoweek').format('MMMM D'),
+        };
+        
+        return weekObject;
+      },
+
       isDateAvailable(_date) {
         const testDate = _date;
         const testDayString = testDate.format('dddd');
@@ -64,6 +77,7 @@
 
         return dates;
       },
+
       formattedDates() {
         // right now, this is getting two arrays, so we need to hard-code the index
         return this.availability[1].map((item) => {
