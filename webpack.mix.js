6212bf2e8bb2f9d944453af74651236997f66cce
Ryan Buttrey <ryan@ryanbuttrey.com>
2017-04-18 13:49:10 -0400

    adding the four tracking codes to the signup page

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,11 @@ mix.options({processCssUrls: false})
    .js('resources/assets/legacy/js/app_logged_in.js', 'public/legacy/js')
    .copy([
         'resources/assets/js/vendors/modernizr-custom.js',
-        'resources/assets/js/vendors/typekit.js'
+        'resources/assets/js/vendors/typekit.js',
+        'resources/assets/js/vendors/facebook.js',
+        'resources/assets/js/vendors/googleanalytics.js',
+        'resources/assets/js/vendors/intercom.js',
+        'resources/assets/js/vendors/mixpanel.js'
     ], 'public/js/vendors')
    .copy('resources/assets/legacy/images', 'public/images', false)
    .sass('resources/assets/scss/application.scss', 'public/css')
