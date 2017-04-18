0e6a6739105b141734b77b35fce3b02f368ba847
Jose Soto <josecanhelp@gmail.com>
2017-04-18 12:25:32 -0700

    Removes guest middleware for email verification

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Auth::routes();
 // Additional routing to not use Laravel's built-in "register" route
 // Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 // Route::post('signup', 'Auth\RegisterController@register');
-Route::get('verify/{user_id}/{token}', 'Auth\EmailVerificationController@verify')->middleware('guest');
-Route::post('verify/{user_id}/{token}', 'Auth\EmailVerificationController@setPassword')->middleware('guest');
+Route::get('verify/{user_id}/{token}', 'Auth\EmailVerificationController@verify');
+Route::post('verify/{user_id}/{token}', 'Auth\EmailVerificationController@setPassword');
 
 
 
