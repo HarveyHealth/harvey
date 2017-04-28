9e98a5ea764afce3be066987600cd38821c4cf85
Tate Peñaranda <tate@goharvey.com>
2017-04-28 18:24:08 -0300

    - Fix to unnecessary intercom script on local env, which was doing a POST and receiving a "not authorized domain" error. - Fix for fullName() method on User model. For users with no first_name and last_name yet (user in registration process) it was returning " ", fixed to return null on that case. - Fix for VueHelperViewComposer class, userData (lastName and UserType) had two typos.
    Conflicts:
    	app/Http/Composers/VueHelperViewComposer.php

--- app/Http/Controllers/API/V1/PractitionerController.php
+++ app/Http/Controllers/API/V1/PractitionerController.php
@@ -11,20 +11,21 @@ use Illuminate\Http\Request;
 class PractitionerController extends BaseAPIController
 {
     protected $resource_name = 'practitioner';
-    
-    public function __construct(PractitionerTransformer $transformer)
+
+    public function __construct(PractitionerTransformer $transformer, PractitionerAvailabilityTransformer $availabilityTransformer)
     {
         parent::__construct();
         $this->transformer = $transformer;
+        $this->availabilityTransformer = $availabilityTransformer;
     }
-    
+
     public function show(Practitioner $practitioner)
     {
         $data = $this->baseTransformItem($practitioner);
-        
+
         if (request()->get('include') == 'availability') {
             $data = $data->addMeta([
-                'availability' => (new PractitionerAvailabilityTransformer())->transform($practitioner)
+                'availability' => $this->availabilityTransformer->transform($practitioner)
             ]);
         }
         return $data->respond();
