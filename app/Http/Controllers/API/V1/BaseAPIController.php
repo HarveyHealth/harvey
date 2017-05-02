b384a42b282f23d02779f161307cedca825e63be
Jose Soto <josecanhelp@gmail.com>
2017-05-02 15:10:15 -0700

    HAR-251 Add patient and user data include

--- app/Http/Controllers/API/V1/BaseAPIController.php
+++ app/Http/Controllers/API/V1/BaseAPIController.php
@@ -81,28 +81,32 @@ class BaseAPIController extends Controller
         return $this->setStatusCode(ResponseCode::HTTP_UNPROCESSABLE_ENTITY)
             ->respondWithError($problem);
     }
-
+    
     /**
      * @param      $item
+     * @param      $include
      * @param null $transformer
      * @return Fractal
      */
-    public function baseTransformItem($item, $transformer = null)
+    public function baseTransformItem($item, $include = null, $transformer = null)
     {
         return fractal()->item($item)
+            ->parseIncludes($include)
             ->withResourceName($this->resource_name)
             ->transformWith($transformer ?? $this->transformer)
             ->serializeWith($this->serializer);
     }
-
+    
     /**
      * @param      $collection
+     * @param      $include
      * @param null $transformer
      * @return Fractal
      */
-    public function baseTransformCollection($collection, $transformer = null)
+    public function baseTransformCollection($collection, $include, $transformer = null)
     {
         return fractal()->collection($collection)
+            ->parseIncludes($include)
             ->withResourceName($this->resource_name)
             ->transformWith($transformer ?? $this->transformer)
             ->serializeWith($this->serializer);
