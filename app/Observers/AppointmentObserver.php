75cd6011800bb6ed772866ac794b0317d96940a5
Tate Peñaranda <tate@goharvey.com>
2017-05-04 17:42:48 -0300

    Added 'AppointmentObserver' so we can check when 'status' is set to 'cancelled'.

---
+++ app/Observers/AppointmentObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Appointment;
+
+class AppointmentObserver
+{
+    /**
+     * Listen to the Appointment updating event.
+     *
+     * @param  Appointment  $appointment
+     * @return void
+     */
+    public function updating(Appointment $appointment)
+    {
+
+        //WIP
+
+    }
+}
