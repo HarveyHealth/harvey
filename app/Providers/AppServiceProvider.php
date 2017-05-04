192e267b535f01d52374174ba010829c96766999
Tate Peñaranda <tate@goharvey.com>
2017-05-03 23:50:54 -0300

    HAR-133 Removed unused Model and Observer.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,13 @@
 namespace App\Providers;
 
 use App\Lib\ZipCodeValidator;
-use App\Models\User;
-use App\Observers\UserObserver;
 use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\ServiceProvider;
 use Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
+
     /**
      * Bootstrap any application services.
      *
@@ -22,12 +20,12 @@ class AppServiceProvider extends ServiceProvider
         // load any blade extensions
         require base_path('extensions/blade.php');
         require base_path('extensions/validator.php');
-        
+
         Validator::extend('serviceable', function ($attribute, $value, $parameters, $validator) {
             return $validator = app()->make(ZipCodeValidator::class)->setZip($value)->isServiceable();
         });
     }
-    
+
     /**
      * Register any application services.
      *
@@ -38,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('local', 'testing', 'dev')) {
             $this->app->register(DuskServiceProvider::class);
         }
-        
+
         // bugsnag
         $this->app->alias('bugsnag.multi', \Illuminate\Contracts\Logging\Log::class);
         $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
