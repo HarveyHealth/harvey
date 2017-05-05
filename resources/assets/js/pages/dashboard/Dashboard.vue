2a63e74fadd285a81db8d5ff098dfa41e9f9c583
jonmcgill <jon.d.mcgill@gmail.com>
2017-05-04 21:32:41 -0400

    Code formatting and fixing console errors on dashboard page

--- resources/assets/js/pages/dashboard/Dashboard.vue
+++ resources/assets/js/pages/dashboard/Dashboard.vue
@@ -1,106 +1,121 @@
 <template>
-    <div class="main-container">
-      <div class="nav-bar">
-        <nav class="admin-nav">
-          <a class="admin-nav-link dashboard" href="#">
-            <svg class="icon icon-person"><use xlink:href="#person" /></svg>
-          </a>
-        </nav>
-        <a href="#" class="nav-bar-account">
-          <svg class="harvey-mark"><use xlink:href="#harvey-mark" /></svg>
+  <div class="main-container">
+    <div class="nav-bar">
+      <nav class="admin-nav">
+        <a class="admin-nav-link dashboard" href="#">
+          <svg class="icon icon-person"><use xlink:href="#person" /></svg>
         </a>
-      </div>
-      <div class="main-content">
-        <div class="main-header">
-          <div class="container">
-            <h1 class="title header-xlarge">Your Dashboard
-            <a href="#" class="button main-action disabled">New Appointment</a></h1>
-          </div>
-        </div>
-        <div class="card-wrapper">
-          <div class="card">
-              <Appointments :user-type="userType"
-                            :recent-appointments="recent_appointments"
-                            :upcoming-appointments="upcoming_appointments"></Appointments>
-          </div>
-          <div class="card smaller">
-            <div class="card-heading-container">
-               <h2 class="card-header">Your Contact</h2>
-            </div>
-            <div class="card-content-container">
-              <div class="card-content-wrap">
-                <h3 class="card-contact-name"><svg class="icon-person"><use xlink:href="#small-person" /></svg>{{ displayName }}</h3>
-              </div>
-              <div v-if="upcoming_appointments.length > 0" class="card-content-wrap">
-                <h4 class="card-contact-sublabel">Doctor</h4>
-                <p class="card-contact-info">{{ upcoming_appointments[0].attributes.practitioner_name }}</p>
-              </div>
-              <div class="card-content-wrap">
-                <h4 class="card-contact-sublabel">Email</h4>
-                <p class="card-contact-info">{{ user.attributes.email }}</p>
-                <h4 class="card-contact-sublabel">Zip</h4>
-                <p class="card-contact-info">{{ user.attributes.zip }}</p>
-                <h4 class="card-contact-sublabel">Phone</h4>
-                <p class="card-contact-info">{{ user.attributes.phone }}</p>
-                <h4 class="card-contact-sublabel">ID</h4>
-                <p class="card-contact-info">#100{{ user.id }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Flyout></Flyout>
+      </nav>
+      <a href="#" class="nav-bar-account">
+        <svg class="harvey-mark"><use xlink:href="#harvey-mark" /></svg>
+      </a>
     </div>
+    <div class="main-content">
+      <div class="main-header">
+        <div class="container">
+          <h1 class="title header-xlarge">Your Dashboard
+            <a href="#" class="button main-action disabled">New Appointment</a>
+          </h1>
+        </div>
+      </div>
+      <div class="card-wrapper">
+        <div class="card">
+          <Appointments :user-type="userType"
+                        :recent-appointments="recent_appointments"
+                        :upcoming-appointments="upcoming_appointments"></Appointments>
+        </div>
+        <div class="card smaller">
+          <div class="card-heading-container">
+            <h2 class="card-header">Your Contact</h2>
+          </div>
+          <div class="card-content-container">
+            <div class="card-content-wrap">
+              <h3 class="card-contact-name">
+                <svg class="icon-person"><use xlink:href="#small-person" /></svg>{{ patientName }}
+              </h3>
+            </div>
+            <div v-if="upcoming_appointments.length > 0" class="card-content-wrap">
+              <h4 class="card-contact-sublabel">Doctor</h4>
+              <p class="card-contact-info">{{ upcoming_appointments[0].attributes.practitioner_name }}</p>
+            </div>
+            <div class="card-content-wrap">
+              <h4 class="card-contact-sublabel">Email</h4>
+              <p class="card-contact-info">{{ email }}</p>
+              <h4 class="card-contact-sublabel">Zip</h4>
+              <p class="card-contact-info">{{ zip }}</p>
+              <h4 class="card-contact-sublabel">Phone</h4>
+              <p class="card-contact-info">{{ phone }}</p>
+              <h4 class="card-contact-sublabel">ID</h4>
+              <p class="card-contact-info">#100{{ user_id }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Flyout></Flyout>
+  </div>
 </template>
 
 <script>
-    import Appointments from '../../appointments/Appointments.vue';
-    import Flyout from './_components/Flyout.vue';
-    import {capitalize, phone, hyperlink} from '../../filters/textformat.js';
-    import Contact from '../../mixins/Contact';
+  import Appointments from '../../appointments/Appointments.vue';
+  import Flyout from './_components/Flyout.vue';
+  import {capitalize, phone, hyperlink} from '../../filters/textformat.js';
+  import Contact from '../../mixins/Contact';
 
-    export default {
-        name: 'dashboard',
-        data() {
-          return {
-            'upcoming_appointments': [],
-            'recent_appointments': []
-          };
-        },
-        props: ['user', 'patient'],
-        components: {
-            Appointments,
-            Flyout
-        },
-        methods: {
-            viewAppointmentPage() {
-                this.$eventHub.$emit('mixpanel', "View New Appointment Page");
-            }
-        },
-        computed: {
-            userType() {
-                return this.user.user_type;
-            },
-            displayName() {
-                if(this.user.attributes === undefined) {
-                    return 'Harvey Client';
-                } else {
-                    return this.user.attributes.first_name;
-                }
-            }
-        },
-        mounted() {
-            this.$http.get(this.$root.apiUrl + '/appointments?filter=upcoming')
-                .then((response) => {
-                  this.upcoming_appointments = response.data.data;
-                });
-
-            this.$http.get(this.$root.apiUrl + '/appointments?filter=recent')
-                .then((response) => {
-                    this.recent_appointments = response.data.data;
-                });
+  export default {
+    name: 'dashboard',
+    data() {
+      return {
+        patientName: Laravel.user.fullName,
+        upcoming_appointments: [],
+        recent_appointments: [],
+      };
+    },
+    props: ['user', 'patient'],
+    components: {
+      Appointments,
+      Flyout
+    },
+    methods: {
+      viewAppointmentPage() {
+        this.$eventHub.$emit('mixpanel', "View New Appointment Page");
+      }
+    },
+    computed: {
+      displayName() {
+        if(this.user.attributes === undefined) {
+          return '';
+        } else {
+          return `${this.user.attributes.first_name} ${this.user.attributes.last_name}`;
         }
-    }
+      },
+      email() {
+        return this.user.attributes ? this.user.attributes.email : '';
+      },
+      phone() {
+        return this.user.attributes ? this.user.attributes.phone : '';
+      },
+      user_id() {
+        return this.user.id || '';
+      },
+      userType() {
+        return this.user.user_type;
+      },
+      zip() {
+        return this.user.attributes ? this.user.attributes.zip : '';
+      }
+    },
+    created() {
+      this.$http.get(this.$root.apiUrl + '/appointments?filter=upcoming')
+        .then((response) => {
+          this.upcoming_appointments = response.data.data;
+        });
+      this.$http.get(this.$root.apiUrl + '/appointments?filter=recent')
+        .then((response) => {
+          this.recent_appointments = response.data.data;
+        });
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
--- resources/assets/js/pages/dashboard/main.js
+++ resources/assets/js/pages/dashboard/main.js
@@ -1,9 +1,6 @@
 import '../../bootstrap';
 import router from './routes';
 
-
-
-
 // FILTERS
 import filter_datetime from '../../filters/datetime';
 Vue.filter('datetime', filter_datetime);
@@ -19,30 +16,30 @@ const eventHub = new Vue();
 Vue.prototype.$eventHub = eventHub;
 
 const app = new Vue({
-    router,
-    mixins: [TopNav],
-    components: {
-        Alert
-    },
-    data: {
-        guest: false,
-        user: {},
-        userId: '',
-        apiUrl: '/api/v1'
-    },
-    mounted() {
-        Stripe.setPublishableKey(Laravel.services.stripe.key);
+  router,
+  mixins: [TopNav],
+  components: {
+    Alert
+  },
+  data: {
+    guest: false,
+    user: {},
+    userId: '',
+    apiUrl: '/api/v1'
+  },
+  created() {
+    Stripe.setPublishableKey(Laravel.services.stripe.key);
 
-        this.userId = Laravel.user.id;
+    this.userId = Laravel.user.id;
 
-        this.$http.get(this.apiUrl + '/users/' + this.userId)
-            .then( response => {
-                this.user = response.data.data;
-            } )
-            .catch( error => this.user = {} );
+    this.$http.get(this.apiUrl + '/users/' + this.userId)
+      .then(response => {
+        this.user = response.data.data;
+      })
+      .catch(error => this.user = {});
 
-        this.$eventHub.$on('mixpanel', (event) => {
-            if (typeof mixpanel !== 'undefined') mixpanel.track(event);
-        });
-    }
+    this.$eventHub.$on('mixpanel', (event) => {
+      if (typeof mixpanel !== 'undefined') mixpanel.track(event);
+    });
+  }
 }).$mount('#app');
