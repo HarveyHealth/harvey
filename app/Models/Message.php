cd07c977b66841a9815932b85778e3f88b9c313a
Tate Peñaranda <tate@goharvey.com>
2017-05-12 02:33:17 -0300

    HAR-229 Added Message model functions.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,13 +2,45 @@
 
 namespace App\Models;
 
+use Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
+use App\Models\User;
 
 class Message extends Model
 {
-
     use Searchable;
+
+    protected $dates = [
+        'created_at',
+        'read_at',
+        'updated_at',
+    ];
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'read_at', 'is_admin', 'sender_user_id'];
+
+    /*
+     * Relationships
+     */
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_user_id');
+    }
+
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'recipient_user_id');
+    }
+
+    /*
+     * Custom attributes
+     */
+
+    public function getIsSenderAdminAttribute()
+    {
+        return (bool) $this->is_admin;
+    }
 
     /**
      * Get the indexable data array for the model.
@@ -20,6 +52,18 @@ class Message extends Model
         return [
             'id' => $this->id,
             'message' => $this->message,
-        ];
+       ];
     }
+
+    /*
+     * Model functions
+     */
+
+    public function touchReadAt()
+    {
+        $this->read_at = Carbon::now();
+
+        return $this->save();
+    }
+
 }
