625f169af4c8037873b8d1ed749b89224ce78156
laynefaler <laynefaler@gmail.com>
2017-05-13 16:43:21 -0700

    new config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,15 +12,15 @@ const { mix } = require('laravel-mix');
  */
 
 mix.options({processCssUrls: false})
-   .js('resources/assets/legacy/js/app_public.js', 'public/legacy/js')
+   .js('resources/assets/vue/app_public.js', 'public/js')
    .js('resources/assets/vue/app.js', 'public/js')
    .js('resources/assets/vue/vendors/modernizr-custom.js', 'public/js/vendors')
    .js('resources/assets/vue/vendors/intercom.js', 'public/js/vendors')
    .js('resources/assets/vue/vendors/typekit.js', 'public/js/vendors')
    .copy('resources/assets/images', 'public/images', false)
    .sass('resources/assets/scss/application.scss', 'public/css')
-   .sass('resources/assets/legacy/sass/pages/legal.scss', 'public/legacy/css')
-   .sass('resources/assets/legacy/sass/app_public.scss', 'public/legacy/css');
+   .sass('resources/assets/scss/public/pages/legal.scss', 'public/css')
+   .sass('resources/assets/scss/public/app_public.scss', 'public/css');
 
 if (mix.config.inProduction) {
     mix.version();
