7b028a3533a07ae5565490bd021b2d1fb36f3d43
laynefaler <laynefaler@gmail.com>
2017-05-16 14:25:01 -0700

    persisted sign up

--- resources/assets/vue/pages/signup/Signup.vue
+++ resources/assets/vue/pages/signup/Signup.vue
@@ -14,17 +14,17 @@
         <div class="signup-form-container">
 
           <div class="input-wrap">
-            <input class="form-input form-input_text error" name="zipcode" type="text" placeholder="Zip Code" v-model="zip" v-validate="{ required: true, digits: 5 }" data-vv-validate-on="blur" maxlength="5"/>
+            <input class="form-input form-input_text error" v-on:change="persistTextFields('zip', zip)" name="zipcode" type="text" placeholder="Zip Code" v-model="zip" v-validate="{ required: true, digits: 5 }" data-vv-validate-on="blur" maxlength="5"/>
             <span v-show="errors.has('zipcode')" class="error-text">{{ errors.first('zipcode') }}</span>
           </div>
 
           <div class="input-wrap">
-            <input class="form-input form-input_text" name="email" type="email" placeholder="Personal Email" v-model="email" v-validate="'required|email'" data-vv-validate-on="blur" />
+            <input class="form-input form-input_text" v-on:change="persistTextFields('email', email)" name="email" type="email" placeholder="Personal Email" v-model="email" v-validate="'required|email'" data-vv-validate-on="blur" />
             <span v-show="errors.has('email')" class="error-text">{{ errors.first('email') }}</span>
           </div>
 
           <div class="input-wrap">
-            <input class="form-input form-input_text" name="password" type="password" placeholder="Create Password" v-model="password" v-validate="{ required: true, min: 6 }" data-vv-validate-on="blur" />
+            <input class="form-input form-input_text" v-on:change="persistTextFields('password', password)" name="password" type="password" placeholder="Create Password" v-model="password" v-validate="{ required: true, min: 6 }" data-vv-validate-on="blur" />
             <span v-show="errors.has('password')" class="error-text">{{ errors.first('password') }}</span>
           </div>
 
@@ -58,9 +58,9 @@
       return {
         title: 'Your journey starts here',
         subtitle: '',
-        email: '',
-        password: '',
-        zip: '',
+        email: localStorage.getItem('sign up email') || '',
+        password: localStorage.getItem('sign up password') || '',
+        zip: localStorage.getItem('sign up zip') || '',
         zipInRange: false,
         terms: false,
         isComplete: false,
@@ -85,10 +85,14 @@
               terms: this.terms,
             })
             .then(response => {
-                this.login(this.email, this.password);
+              this.login(this.email, this.password);
               // the form is complete
               this.isComplete = true;
               this.zipInRange = true;
+
+              localStorage.removeItem('sign up zip');
+              localStorage.removeItem('sign up email');
+              localStorage.removeItem('sign up password');
             })
             .catch(error => {
               this.responseErrors = error.response.data.errors;
@@ -105,6 +109,9 @@
             // TODO: catch error
           });
         },
+        persistTextFields(field, value) {
+          localStorage.setItem(`sign up ${field}`, value)
+        }
     },
     mounted () {
       if (this.env === 'prod') {
