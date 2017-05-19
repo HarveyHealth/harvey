1ed5936099ae7605758875893a56dbb88cd8dcf8
jonmcgill <jon.d.mcgill@gmail.com>
2017-05-19 15:45:43 -0400

    Fix sort function

--- resources/assets/js/pages/appointments/Appointments.vue
+++ resources/assets/js/pages/appointments/Appointments.vue
@@ -322,6 +322,19 @@
           this.dataCollected = true;
         })
       },
+      sortByLastName(names) {
+        return names.sort((a, b) => {
+          const nameA = a.name.replace(/,.+/g, '').toUpperCase();
+          const nameB = b.name.replace(/,.+/g, '').toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          } else if (nameA > nameB) {
+            return 1
+          } else {
+            return 0;
+          }
+        })
+      },
       toLocalTimezone
     },
     filters: {
