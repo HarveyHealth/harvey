a1ca58db2bf27b8ec7192acec80e978ab8721ba5
laynefaler <laynefaler@gmail.com>
2017-05-24 22:44:06 -0700

    router links

--- resources/assets/js/pages/messages/Messages.vue
+++ resources/assets/js/pages/messages/Messages.vue
@@ -16,7 +16,7 @@
             <preview v-if="renderNewMessage" />
             <div style="padding: 20px;">
                 <div v-for="chat in messageList">
-                  <a v-link="{path: '/detail', message_id: chat.id}" style="padding: 4px;">
+                  <router-link to="/detail" style="padding: 4px;">
                     <MessagePost
                         name="Alicia Keys"
                         :day="chat.attributes.created_at.date.split('').splice(0, 10).join('')"
@@ -24,7 +24,7 @@
                         :subject="chat.attributes.subject"
                         :message="chat.attributes.message"
                      />
-                  </a>
+                  </router-link>
                 </div>
             </div>
       </div>
