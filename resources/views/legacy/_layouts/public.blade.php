34ef90e477f8a4a8bdd6aa2024afa365fc63b407
Kyle Hill <kyle@homehero.org>
2017-05-25 14:08:08 -0700

    Fix to headers bug

--- resources/views/legacy/_layouts/public.blade.php
+++ resources/views/legacy/_layouts/public.blade.php
@@ -26,13 +26,12 @@
 
     @if (Auth::guest())
         
-        <!-- Modernizr -->    
-        <script type="text/javascript" src="{{ mix('js/app_public.js') }}"></script>
+        <!-- Modernizr -->
+        <script type="text/javascript" src="{{ mix('js/vendors/modernizr-custom.js') }}"></script>
 
         <!-- Juicer -->
         <link rel="stylesheet" href="https://assets.juicer.io/embed.css">
         <script type="text/javascript" src="https://assets.juicer.io/embed.js"></script>
-
 
         <!-- Lity -->
         <link rel="stylesheet" href="{{ mix('css/vendors/lity.css') }}">
@@ -43,11 +42,13 @@
         <script type="text/javascript" src="{{ mix('js/app_public.js') }}"></script>
 
     @else
+    
         <!-- Stripe -->
         <script type="text/javascript" src="https://js.stripe.com/v2"></script>
         
         <!-- App.js -->
         <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+
     @endif
 
     @stack('scripts')
