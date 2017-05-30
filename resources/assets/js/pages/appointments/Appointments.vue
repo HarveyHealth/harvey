dd02ba764ed9bee19ed4da47908ad437e23757ee
jonmcgill <jon.d.mcgill@gmail.com>
2017-05-30 15:16:05 -0400

    Adds filters to appointments page

--- resources/assets/js/pages/appointments/Appointments.vue
+++ resources/assets/js/pages/appointments/Appointments.vue
@@ -15,10 +15,16 @@
               <svg><use xlink:href="#addition"/></svg>
             </button>
           </h1>
+          <div>
+            <button
+              v-for="(obj, index) in tableFilters"
+              :class="{'button--filter': true, 'isactive': activeFilter === index}"
+              @click="filterTable(index, obj.name)">{{ obj.name }}</button>
+          </div>
         </div>
       </div>
 
-      <TableData :config="tableConfig"/>
+      <TableData :config="tableConfig" :filter="tableFilter" />
 
     </div>
 
@@ -105,6 +111,7 @@
 
   // Helpers
   import { capitalize, phone } from '../../utils/filters/textformat.js';
+  import combineAppointmentData from '../../utils/methods/combineAppointmentData';
   import moment from 'moment-timezone';
   import sortByLastName from '../../utils/methods/sortByLastName';
   import tableConfig from './utils/tableconfig';
@@ -128,6 +135,7 @@
     },
     data() {
       return {
+        activeFilter: 0,
         _appointmentDetails: this.$root.$data.global.appointments,
         appointmentData: {
           appointmentId: '',
@@ -168,6 +176,11 @@
         },
         dataCollected: false,
         doctorAvailability: {},
+        tableFilters: [
+          { name: 'All', active: true },
+          { name: 'Upcoming', active: false },
+          { name: 'Completed', active: false }
+        ],
         statuses: {
           'pending': 'Pending',
           'no_show_patient': 'No-Show-Patient',
@@ -176,12 +189,23 @@
           'canceled': 'Canceled',
           'complete': 'Complete'
         },
-        tableConfig: tableConfig
+        tableFilter: 'All',
+        tableConfig,
       };
     },
     computed: {
       availability() {
         return transformAvailability(this.doctorAvailability);
+      },
+      upcomingAppointments() {
+        return this.$root.$data.global.upcoming_appointments.length
+          ? combineAppointmentData(this.$root.$data.global.upcoming_appointments)
+          : [];
+      },
+      recentAppointments() {
+        return this.$root.$data.global.recent_appointments.length
+          ? combineAppointmentData(this.$root.$data.global.recent_appointments)
+          : [];
       },
       noAvailability() {
         if (this.doctorAvailability.length) {
@@ -276,6 +300,10 @@
         this.confirmationText['Purpose'] = this.dataForUpdate.reason_for_visit;
 
         this.$eventHub.$emit('callAppointmentModal');
+      },
+      filterTable(index, name) {
+        this.activeFilter = index;
+        this.tableFilter = name;
       },
       sortByLastName,
       toLocalTimezone
