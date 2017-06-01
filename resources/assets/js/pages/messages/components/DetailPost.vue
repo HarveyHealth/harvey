0b6303ac09cb47a5c54776da221db72aff053784
laynefaler <laynefaler@gmail.com>
2017-05-31 17:27:17 -0700

    ... icon added

--- resources/assets/js/pages/messages/components/DetailPost.vue
+++ resources/assets/js/pages/messages/components/DetailPost.vue
@@ -8,7 +8,7 @@
                 <h4 class="top-layer top-layer-margin">{{ name }}</h4>
                 <h4 class="top-layer">{{ moment(day).format("MMM Do YYYY") }}</h4>
                 <h4 class="top-layer">{{ moment.utc(time).local().format("h:mm a") }}</h4>
-                <h4 class="top-layer">...</h4>
+                <h4 class="top-layer"><i class="fa fa-ellipsis-h"></i></h4>
             </div>
             <div class="message-margin"><h3 class="message-layer">{{ message }}</h3></div>
         </div>
