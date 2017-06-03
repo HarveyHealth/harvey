667061fc91ed60afe4fca9a73a6637455de89fb0
laynefaler <laynefaler@gmail.com>
2017-06-02 18:05:19 -0700

    authEndpoint url needed

--- resources/assets/js/pages/messages/websocket.js
+++ resources/assets/js/pages/messages/websocket.js
@@ -4,7 +4,11 @@ import Pusher from 'pusher-js';
 const socket = new Pusher('921f783fca361d2ac622', {
   cluster: 'mt1',
   enabledTransports: ['ws', 'xhr_streaming'],
-  disabledTransports: ['xhr_streaming']
+  disabledTransports: ['xhr_streaming'],
+  authEndpoint: 'http://harvey.app/api/v1/',
+  auth: {
+    headers: { 'X-CSRF-Token': window.Laravel.app.csrfToken }
+  }
 });
 
 export default socket;
