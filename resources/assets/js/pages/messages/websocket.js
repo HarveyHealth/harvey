fc3d2c7cfc8a1597b1c6b6df7707a2c12963291f
laynefaler <laynefaler@gmail.com>
2017-06-02 17:35:49 -0700

    wrong app cluster

--- resources/assets/js/pages/messages/websocket.js
+++ resources/assets/js/pages/messages/websocket.js
@@ -1,8 +1,10 @@
 import Pusher from 'pusher-js';
 
+
 const socket = new Pusher('921f783fca361d2ac622', {
   cluster: 'mt1',
-  enabledTransport: ['wss']
+  enabledTransports: ['ws', 'xhr_streaming'],
+  disabledTransports: ['xhr_streaming']
 });
 
 export default socket;
