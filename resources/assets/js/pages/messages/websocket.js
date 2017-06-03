dc9ab9a227ca87116f4cdfdcfad57085789a9d96
Tate Peñaranda <tate@goharvey.com>
2017-06-03 12:57:59 -0300

    Fix for authEndpoint param.

--- resources/assets/js/pages/messages/websocket.js
+++ resources/assets/js/pages/messages/websocket.js
@@ -5,7 +5,7 @@ const socket = new Pusher('921f783fca361d2ac622', {
   cluster: 'mt1',
   enabledTransports: ['ws', 'xhr_streaming'],
   disabledTransports: ['xhr_streaming'],
-  authEndpoint: `http://harvey.app/broadcasting/auth`,
+  authEndpoint: `/broadcasting/auth`,
   auth: {
     headers: { 'X-CSRF-Token': window.Laravel.app.csrfToken }
   }
