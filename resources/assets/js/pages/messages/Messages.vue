5c15ef557d5f6e6368a3b988b7883bba39d04cb6
laynefaler <laynefaler@gmail.com>
2017-06-05 15:42:12 -0700

    web socket fixes

--- resources/assets/js/pages/messages/Messages.vue
+++ resources/assets/js/pages/messages/Messages.vue
@@ -96,9 +96,9 @@
                 this.messageList = this.$root.$data.global.messages;
               })
           channel.bind('App\\Events\\MessageCreated', (data) => {
-            this.$root.$data.global.detailMessages[window.Laravel.user.id][data.attributes.subject].push(data.data)
-            this.$root.$data.global.detailMessages[window.Laravel.user.id][data.attributes.subject].sort((a, b) => a.attributes.created_at - b.attributes.created_at)
-            this.$root.$data.global.messages = Object.values(data[window.Laravel.user.id]).map(e => e[e.length -1])
+            this.$root.$data.global.detailMessages[data.attributes.subject].push(data.data)
+            this.$root.$data.global.detailMessages[data.attributes.subject].sort((a, b) => a.attributes.created_at - b.attributes.created_at)
+            this.$root.$data.global.messages = Object.values(this.$root.$data.global.detailMessages).map(e => e[e.length -1])
           })
         }
     }
