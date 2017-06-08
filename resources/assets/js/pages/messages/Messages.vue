6395f8aeec7799834b2283af50b46e7e6d25d3f4
laynefaler <laynefaler@gmail.com>
2017-06-08 13:19:08 -0700

    unread user at the top

--- resources/assets/js/pages/messages/Messages.vue
+++ resources/assets/js/pages/messages/Messages.vue
@@ -84,7 +84,8 @@
                   Object.values(data).map(e => _.uniq(e.sort((a, b) => a.attributes.created_at - b.attributes.created_at)));
                   this.$root.$data.global.detailMessages = data;
                   this.$root.$data.global.messages = Object.values(data).map(e => e[e.length - 1]).sort((a, b) => {
-                    if (a.attributes.read_at == null || b.attributes.read_at == null) {
+                    if ((a.attributes.read_at == null || b.attributes.read_at == null) &&
+                      (this.$root.$data.global.user.id == a.attributes.recipient_user_id || this.$root.$data.global.user.id == b.attributes.recipient_user_id)) {
                       return 1;
                     }
                     return -1;
@@ -98,7 +99,8 @@
             this.$root.$data.global.messages = Object.values(this.$root.$data.global.detailMessages)
               .map(e => e[e.length -1])
               .sort((a, b) => {
-                    if (a.attributes.read_at == null || b.attributes.read_at == null) {
+                    if ((a.attributes.read_at == null || b.attributes.read_at == null) &&
+                      (this.$root.$data.global.user.id == a.attributes.recipient_user_id || this.$root.$data.global.user.id == b.attributes.recipient_user_id)) {
                       return 1;
                     }
                     return -1;
