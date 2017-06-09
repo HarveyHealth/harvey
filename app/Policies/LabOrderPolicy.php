cf1699bcf29198b7ae0c26dd2115e6f59b619f43
Tate Peñaranda <tate@goharvey.com>
2017-06-09 15:08:23 -0300

    LabOrderPolicy enabled.

--- app/Policies/LabOrderPolicy.php
+++ app/Policies/LabOrderPolicy.php
@@ -48,7 +48,7 @@ class LabOrderPolicy
      */
     public function update(User $user, LabOrder $labOrder)
     {
-        return $user->is($labOrder->patient->user) || $user->is($labOrder->practitioner->user);
+        return $user->is($labOrder->practitioner->user);
     }
 
     /**
