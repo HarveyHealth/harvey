871d29f1efd78d98efef19abaac412ff02537dba
Tate Peñaranda <tate@goharvey.com>
2017-06-08 21:55:51 -0300

    Fix on Observer logic.

--- app/Observers/LabTestObserver.php
+++ app/Observers/LabTestObserver.php
@@ -7,17 +7,15 @@ use App\Models\LabTest;
 class LabTestObserver
 {
     /**
-     * Listen to the LabTest updating event.
+     * Listen to the LabTest updated event.
      *
      * @param  LabTest $labTest
      * @return void
      */
-    public function updating(LabTest $labTest)
+    public function updated(LabTest $labTest)
     {
-        if ($labTest->isDirty('status_id')
-        && LabTest::COMPLETE_STATUS_ID == $labTest->status_id
-        && $labTest->labOrder->areLabTestsCompleted()) {
-                $labTest->labOrder->markAsComplete();
+        if (LabTest::COMPLETE_STATUS_ID == $labTest->status_id && $labTest->labOrder->areLabTestsCompleted()) {
+            $labTest->labOrder->markAsComplete();
         }
     }
 }
