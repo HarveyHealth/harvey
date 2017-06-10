d7d689130c181f7803369a91ab7eded0bf3ebb31
Tate Peñaranda <tpenaranda@users.noreply.github.com>
2017-06-09 23:59:34 -0300

    Lab Orders/Tests BE (#169)
    * Labs orders/tests tables creation.
    
    * Model factories.
    
    * Added soft deletes to tables.
    
    * Added models and seeders.
    
    * Added controllers and policies.
    
    * Added transformers.
    
    * Added missing curly brackets.
    
    * Small fix after a merge conflict solved incorrectly.
    
    * Added routing.
    
    * Added store and update methods.
    
    * Update on Controllers and added LabTest observer.
    
    * Fix on Observer logic.
    
    * Fix for small typo.
    
    * Logic to cancel Lab Orders.
    
    * Allow only 'status' to be updated on LabTests PATCH endpoint.
    
    * Bugfixing here and there.
    
    * Fix for typo.
    
    * LabOrderPolicy enabled.
    
    * Missing namespace for Rule class.
    
    * Fix for key on LabOrdersTransformer.
    
    * Added missing curly braces.
    
    * Fixes for routing.
    
    * Updates for Policies and 'delete' endpoints.
    
    * out of scope, fix for Message 'delete' endpoint. Since Message was deleted 'baseTransformItem($message)' has no sense, actually the BE was returning an empty request body. Here we are adding validation for deletion and proper response code, without breaking backwards compatibility.
    
    * Fixes after code review. Refactor using higher order messages.
    
    * Moving some methods to HasStatusColumn trait.
    
    * Unit tests for LabOrder class.
    
    * Small update for Policy not catching Admin role.
    
    * Added one more test.
    
    * Unit tests for LabTestsController.
    
    * Fix for wrong key name in tests.
    
    * Allow Patient and Practitioner to get they own lab Tests/Orders.

---
+++ tests/Feature/LabTestTest.php
@@ -0,0 +1,288 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\{Admin, Patient, Practitioner, LabTest, LabOrder, SKU};
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Carbon;
+use ResponseCode;
+
+class LabTestTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_it_allows_a_patient_to_retrieve_only_his_labs_tests()
+    {
+        $labTests = factory(LabTest::class, 3)->create();
+        $patient = $labTests->first()->patient;
+
+        Passport::actingAs($patient->user);
+        $response = $this->json('GET', 'api/v1/lab/tests');
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertCount(1, $response->original['data']);
+    }
+
+    public function test_it_allows_a_practitioner_to_retrieve_only_his_labs_tests()
+    {
+        $labTests = factory(LabTest::class, 3)->create();
+        $practitioner = $labTests->first()->practitioner;
+
+        Passport::actingAs($practitioner->user);
+        $response = $this->json('GET', 'api/v1/lab/tests');
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertCount(1, $response->original['data']);
+    }
+
+    public function test_it_allows_an_admin_to_retrieve_lab_tests()
+    {
+        factory(LabTest::class, 3)->create();
+
+        $admin = factory(Admin::class)->create();
+        Passport::actingAs($admin->user);
+        $response = $this->json('GET', 'api/v1/lab/tests');
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertCount(3, $response->original['data']);
+    }
+
+    public function test_it_allows_an_admin_to_retrieve_a_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        $admin = factory(Admin::class)->create();
+        Passport::actingAs($admin->user);
+        $response = $this->json('GET', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertEquals($labTest->id, $response->original['data']['id']);
+    }
+
+    public function test_it_allows_a_practitioner_to_retrieve_his_own_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->practitioner->user);
+        $response = $this->json('GET', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertEquals($labTest->id, $response->original['data']['id']);
+    }
+
+    public function test_it_allows_a_patient_to_retrieve_his_own_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->patient->user);
+        $response = $this->json('GET', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertEquals($labTest->id, $response->original['data']['id']);
+    }
+
+    public function test_it_does_not_allows_a_practitioner_to_retrieve_others_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs(factory(Practitioner::class)->create()->user);
+        $response = $this->json('GET', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_does_not_allows_a_patient_to_retrieve_others_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs(factory(Patient::class)->create()->user);
+        $response = $this->json('GET', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_allows_an_admin_to_create_a_lab_test()
+    {
+        $labOrder = factory(LabOrder::class)->create();
+        $sku = factory(SKU::class)->create();
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $parameters = [
+            'lab_order_id' => $labOrder->id,
+            'sku_id' => $sku->id,
+        ];
+
+        $response = $this->json('POST', 'api/v1/lab/tests', $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $response->assertJsonFragment(['lab_order_id' => "{$labOrder->id}"]);
+        $response->assertJsonFragment(['sku_id' => "{$sku->id}"]);
+
+        $this->assertDatabaseHas('lab_tests', ['lab_order_id' => $labOrder->id]);
+        $this->assertDatabaseHas('lab_tests', ['sku_id' => $sku->id]);
+    }
+
+    public function test_sku_id_is_required_when_creating_a_lab_test()
+    {
+        $labOrder = factory(LabOrder::class)->create();
+        $sku = factory(SKU::class)->create();
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $parameters = [
+            'lab_order_id' => $labOrder->id,
+        ];
+
+        $response = $this->json('POST', 'api/v1/lab/tests', $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_BAD_REQUEST);
+    }
+
+    public function test_lab_order_id_is_required_when_creating_a_lab_test()
+    {
+        $labOrder = factory(LabOrder::class)->create();
+        $sku = factory(SKU::class)->create();
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $parameters = [
+            'sku_id' => $sku->id,
+        ];
+
+        $response = $this->json('POST', 'api/v1/lab/tests', $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_BAD_REQUEST);
+    }
+
+    public function test_it_does_not_allows_a_practitioner_to_create_a_lab_test()
+    {
+        Passport::actingAs(factory(Practitioner::class)->create()->user);
+
+        $response = $this->json('POST', 'api/v1/lab/tests');
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_does_not_allows_a_patient_to_create_a_lab_test()
+    {
+        Passport::actingAs(factory(Patient::class)->create()->user);
+
+        $response = $this->json('POST', 'api/v1/lab/tests');
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_doesn_not_allows_an_admin_to_update_if_lab_order_is_complete()
+    {
+        $labTest = factory(LabTest::class)->create(['status' => 'complete']);
+
+        $labTest->LabOrder->markAsComplete();
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $parameters = [
+            'status' => 'pending',
+        ];
+
+        $response = $this->json('PATCH', "api/v1/lab/tests/{$labTest->id}", $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_allows_an_admin_to_update_a_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create(['status' => 'pending']);
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $parameters = [
+            'status' => 'canceled',
+        ];
+
+        $response = $this->json('PATCH', "api/v1/lab/tests/{$labTest->id}", $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $response->assertJsonFragment($parameters);
+
+        $this->assertDatabaseHas('lab_tests', ['status_id' => LabTest::CANCELED_STATUS_ID]);
+    }
+
+    public function test_it_allows_a_practitioner_to_update_his_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->practitioner->user);
+
+        $parameters = [
+            'status' => 'canceled',
+        ];
+
+        $response = $this->json('PATCH', "api/v1/lab/tests/{$labTest->id}", $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $response->assertJsonFragment($parameters);
+
+        $this->assertDatabaseHas('lab_tests', ['status_id' => LabTest::CANCELED_STATUS_ID]);
+    }
+
+    public function test_it_does_not_allows_a_patient_to_update_his_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->patient->user);
+
+        $parameters = [
+            'status' => 'canceled',
+        ];
+
+        $response = $this->json('PATCH', "api/v1/lab/tests/{$labTest->id}", $parameters);
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_does_not_allows_a_patient_to_delete_his_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->patient->user);
+
+        $response = $this->json('DELETE', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_does_not_allows_a_practitioner_to_delete_his_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs($labTest->practitioner->user);
+
+        $response = $this->json('DELETE', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_allows_an_admin_to_delete_a_lab_test()
+    {
+        $labTest = factory(LabTest::class)->create();
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+
+        $response = $this->json('DELETE', "api/v1/lab/tests/{$labTest->id}");
+
+        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+    }
+}
