618d83e6dbdd0582b122f65f56a197d48058b45f
jonmcgill <jon.d.mcgill@gmail.com>
2017-06-15 17:06:37 -0400

    documenting Purpose component

--- resources/assets/js/pages/appointments/components/Purpose.vue
+++ resources/assets/js/pages/appointments/components/Purpose.vue
@@ -15,27 +15,35 @@
 <script>
 export default {
   props: {
+    // Limits the number of characters the user can input
     characterLimit: {
       type: Number,
       required: true
     },
+    // Is the purpose editable or display only?
     editable: {
       type: Boolean,
       default: true
     },
+    // What happens when a user inputs something into the textarea?
+    // Function takes the textarea value if not empty.
+    // This should probably update the textValue prop in immitation of v-model.
     onInput: {
       type: Function,
       required: true
     },
+    // The initial value of the textarea or display.
     textValue: {
       type: String,
       required: true
     }
   },
   computed: {
+    // Checking if the value is over the character limit
     $$count() {
       return this.characterLimit - this.textValue.length;
     },
+    // Add class if character limit has been reaced.
     $$countClasses() {
       return {
         'charcount': true,
