79a305fa0af8d632f36735f4dd7afaeb23821f73
Tate Peñaranda <tate@goharvey.com>
2017-06-21 11:35:11 -0300

    HAR-247 Create a table in the DB that will store the lab-tests.

---
+++ database/migrations/2017_06_21_055456_create_lab_tests_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLabTestsDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lab_tests_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sku_id')->unsigned();
+            $table->mediumText('description');
+            $table->string('image')
+            $table->string('lab_name');
+            $table->string('name')
+            $table->string('sample');
+            $table->text('qoute');
+            $table->foreign('sku_id')->references('id')->on('skus');
+            $table->softDeletes();
+        });
+
+        $this->seedTable();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lab_tests_details');
+    }
+}
+
