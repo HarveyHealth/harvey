8b1f311a91d4dc8d68f9e36b3726cd724dd506af
jonmcgill <jon.d.mcgill@gmail.com>
2017-06-26 11:21:57 -0400

    tweaks container name and interstitial margins

--- resources/assets/js/pages/getting-started/children/Welcome.vue
+++ resources/assets/js/pages/getting-started/children/Welcome.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class="containerClasses">
-    <div class="signup-welcome-container text-centered">
+    <div class="signup-interstitial-container text-centered">
       <div class="signup-main-icon">
         <svg class="icon-rocket"><use xlink:href="#rocket" /></svg>
       </div>
       <p>You will need to answer a few questions to book a consultation with one of our Naturopathic doctors.</p>
       <p>This process will take about 5 minutes and you will need to have your mobile phone handy.</p>
-      <button class="button button--blue">Continue</button>
+      <button class="button button--blue" @click="$router.push('practitioner')">Continue</button>
     </div>
   </div>
 </template>
