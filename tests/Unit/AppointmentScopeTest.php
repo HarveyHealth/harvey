1d94e177036b4ca354e30813bf9e7baf2d0379e9
Jose Soto <josecanhelp@gmail.com>
2017-06-26 16:12:51 -0700

    Add global scope to appointments model
    This will only return appointments with associated patients AND practioners that are enabled.
    
    # Conflicts:
    #	app/Models/Appointment.php

---
+++ tests/Unit/AppointmentScopeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Appointment;
+use App\Models\Patient;
+use App\Models\Practitioner;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AppointmentScopeTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_it_only_returns_appointments_with_enabled_practitioners()
+    {
+        $practitioner = factory(Practitioner::class)->create();
+        $patient = factory(Patient::class)->create();
+        $appointment = factory(Appointment::class)->create([
+            'practitioner_id' => $practitioner->id,
+            'patient_id' => $patient->id,
+            'appointment_at' => '2017-01-01 00:00:00'
+        ]);
+
+        $this->assertCount(1, Appointment::all());
+
+        $practitioner->user->enabled = false;
+        $practitioner->user->save();
+
+        $this->assertCount(0, Appointment::all());    	
+    }
+
+    public function test_it_only_returns_appointments_with_enabled_patients()
+    {
+        $practitioner = factory(Practitioner::class)->create();
+        $patient = factory(Patient::class)->create();
+        $appointment = factory(Appointment::class)->create([
+            'practitioner_id' => $practitioner->id,
+            'patient_id' => $patient->id,
+            'appointment_at' => '2017-01-01 00:00:00'
+        ]);
+
+        $this->assertCount(1, Appointment::all());
+
+        $patient->user->enabled = false;
+        $patient->user->save();
+
+        $this->assertCount(0, Appointment::all());      
+    }
+}
