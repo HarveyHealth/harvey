01ed380dc79fc499b9dfc9e73f083042134d9cbc
Tate Peñaranda <tpenaranda@users.noreply.github.com>
2017-06-27 14:33:35 -0300

    Global scopes to Message, Patient and Practitioner models. (#241)
    * Add global scope to appointments model
    
    This will only return appointments with associated patients AND practioners that are enabled.
    
    # Conflicts:
    #	app/Models/Appointment.php
    
    * Remove unused use statement
    
    * Added globaScopes to Message, Patient and Practitioner.
    
    * Added tests.

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,9 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\{Model, Builder, SoftDeletes};
 use Lang;
 
 class Appointment extends Model
