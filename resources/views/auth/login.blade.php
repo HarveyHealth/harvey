63c224d7ff3e3e454d914f316d52f992d277f3f2
laynefaler <laynefaler@gmail.com>
2017-06-29 18:09:53 -0700

    login page draft 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,10 @@
 @section('page_title','Log in')
 
 @section('main_content')
-<section class="signup-content" style="background-color: rgba(119,119,116, 0.08); width: 100%; height: 100%;">
-    <div class="container small" style="width: 600px;">
+<section class="signup-content">
+    <div class="container small login-width">
         <img src="/images/signup/tree.png" class="registration-tree">
-        <h1 class="header-xlarge" style="font-size: 32px; font-weight: 500; text-align: center; color: #777777; line-height: 1.3; margin-bottom: 15px;">Log in</h1>
+        <h1 class="header-xlarge login-heading">Log in</h1>
                 <form
                     id="login"
                     role="form"
@@ -16,7 +16,7 @@
                     @submit.prevent.self="onSubmit"
                     @keydown="login.form.errors.clear($event.target.name)"
                 >
-                <div class="card" style="padding: 50px 90px;">
+                <div class="card card-padding">
                 <div class="card-section">
                     {{ csrf_field() }}
 
@@ -24,39 +24,10 @@
                         <input
                             v-model="login.form.email"
                             name="email"
-                            class="input"
+                            class="input login-input"
                             :class="{'is-danger' : login.form.errors.has('email')}"
                             type="email"
                             placeholder="Email"
-                            style="
-                                    -webkit-appearance: textfield;
-                                    background-color: white;
-                                    -webkit-rtl-ordering: logical;
-                                    user-select: text;
-                                    cursor: auto;
-                                    text-rendering: auto;
-                                    color: initial;
-                                    letter-spacing: normal;
-                                    word-spacing: normal;
-                                    text-transform: none;
-                                    text-indent: 0px;
-                                    text-shadow: none;
-                                    display: inline-block;
-                                    font: 11px system-ui;
-                                    text-align: start;
-                                    -webkit-writing-mode: horizontal-tb;
-                                    font-family: 'proxima-nova';
-                                    font-style: normal;
-                                    font-weight: 500;
-                                    line-height: 1.3;
-                                    width: 100%;
-                                    border: none;
-                                    border-bottom: 1px solid #999999;
-                                    padding: 0 0 10px;
-                                    font-size: 18px;
-                                    box-shadow: none;
-                                    margin-bottom: 30px;
-                            "
                             required
                             :autofocus="!login.form.errors.length || login.form.errors.has('email')"
                             @change="login.form.errors.clear()"
@@ -71,40 +42,11 @@
                         <input
                             v-model="login.form.password"
                             name="password"
-                            class="input"
+                            class="input login-input"
                             :class="{'is-danger' : login.form.errors.has('password')}"
                             type="password"
                             placeholder="Password"
                             required
-                            style="
-                                    -webkit-appearance: textfield;
-                                    background-color: white;
-                                    -webkit-rtl-ordering: logical;
-                                    user-select: text;
-                                    cursor: auto;
-                                    text-rendering: auto;
-                                    color: initial;
-                                    letter-spacing: normal;
-                                    word-spacing: normal;
-                                    text-transform: none;
-                                    text-indent: 0px;
-                                    text-shadow: none;
-                                    display: inline-block;
-                                    font: 11px system-ui;
-                                    text-align: start;
-                                    -webkit-writing-mode: horizontal-tb;
-                                    font-family: 'proxima-nova';
-                                    font-style: normal;
-                                    font-weight: 500;
-                                    line-height: 1.3;
-                                    width: 100%;
-                                    border: none;
-                                    border-bottom: 1px solid #999999;
-                                    padding: 0 0 10px;
-                                    font-size: 18px;
-                                    box-shadow: none;
-                                    margin-bottom: 30px;
-                            "
                             :autofocus="login.form.errors.has('password')"
                             @change="login.form.errors.clear()"
                         >
@@ -135,9 +77,9 @@
             <div class="card-footer-item level">
                 <button
                     type="submit"
-                    class="button is-primary is-pulled-right"
+                    class="button is-primary is-pulled-right login-buttons"
                 >Log In</button>
-                <a href="/signup" class="level-right button">Sign Up</a>
+                <a href="/signup"><button href="/signup" class="button login-buttons signup-color">Sign Up</button></a>
             </div>
         </footer>
        </form>
