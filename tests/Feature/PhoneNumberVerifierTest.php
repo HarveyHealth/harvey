79f4276284e88d85db16afc507c5ad948abe7586
Tate Peñaranda <tate@goharvey.com>
2017-07-10 18:08:11 -0300

    Added one more test.

--- tests/Feature/PhoneNumberVerifierTest.php
+++ tests/Feature/PhoneNumberVerifierTest.php
@@ -2,14 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Models\{Admin, Message, Patient, Practitioner};
-use App\Events\MessageCreated;
-use Illuminate\Support\Facades\{Event, Notification};
-use Faker\Factory as Faker;
+use App\Lib\PhoneNumberVerifier;
+use App\Models\Patient;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Laravel\Passport\Passport;
 use Tests\TestCase;
-use Carbon, ResponseCode;
+use Carbon, Log, Redis;
 
 class PhoneNumberVerifierTest extends TestCase
 {
@@ -18,14 +15,17 @@ class PhoneNumberVerifierTest extends TestCase
     public function test_phone_verified_at_is_set_to_null_if_phone_is_changed()
     {
         $patient = factory(Patient::class)->create();
+
         $now = Carbon::now();
+        $patient->user->phone_verified_at = $now;
+        $patient->user->save();
 
         $this->assertDatabaseHas('users', [
             'phone' => $patient->user->phone,
             'phone_verified_at' => $now->toDateTimeString(),
         ]);
 
-        $newPhone = 555123123;
+        $newPhone = $this->getRandomValidPhone();
         $patient->user->phone = $newPhone;
         $patient->user->save();
 
@@ -33,5 +33,21 @@ class PhoneNumberVerifierTest extends TestCase
             'phone' => $newPhone,
             'phone_verified_at' => null,
         ]);
+    }
+
+    public function test_confirmation_sms_is_sent_after_phone_is_changed()
+    {
+        Log::spy();
+
+        $patient = factory(Patient::class)->create();
+        $user = $patient->user;
+
+        $user->phone = $this->getRandomValidPhone();
+        $user->save();
+
+        $code = Redis::get("phone_validation:{$user->id}:{$user->phone}");
+        $message = "Your Harvey phone verification code is {$code}";
+
+        Log::shouldHaveReceived('info')->with("Faking sending text message to {$user->phone} with message: {$message}")->once();
     }
 }
