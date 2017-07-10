14c7107d051085d55e122708c179099d986ee8f2
jonmcgill <jon.d.mcgill@gmail.com>
2017-07-10 10:47:43 -0400

    removes new_registration localStorage item and simplifies GettingStarted.vue

--- resources/assets/js/pages/getting-started/GettingStarted.vue
+++ resources/assets/js/pages/getting-started/GettingStarted.vue
@@ -11,12 +11,6 @@
 
   export default {
     name: 'getting-started',
-    data() {
-      return {
-        new_registration: localStorage.getItem('new_registration') || false,
-        signed_in: Laravel.user.signedIn
-      }
-    },
     components: {
       Signup,
       Welcome,
@@ -28,21 +22,8 @@
         }
       }
     },
-    created() {
-      // If new_registration exists, it means the user has just signed up and may go
-      // through the signup funnel for one session only. If they refresh the page they
-      // will be redirected to /dashboard
-      if (this.new_registration) {
-        this.new_registration = false;
-        this.setBeforeUnload();
-        localStorage.removeItem('new_registration');
-        this.$router.push({ name: 'welcome', path: '/welcome' });
-      } else if (this.signed_in) {
-        this.setBeforeUnload();
-        // window.location.href = '/dashboard';
-      } else {
-        this.$router.push({ name: 'sign-up', path: '/signup' });
-      }
+    mounted() {
+      if (Laravel.user.signedIn) this.setBeforeUnload();
     }
   }
 </script>
