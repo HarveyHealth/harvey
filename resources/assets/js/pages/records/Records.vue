9b6da93fb113ac75103d6ff297d2c38add1e524d
laynefaler <laynefaler@gmail.com>
2017-07-12 10:12:05 -0700

    buttons

--- resources/assets/js/pages/records/Records.vue
+++ resources/assets/js/pages/records/Records.vue
@@ -66,7 +66,7 @@
               </div>
 
               <div style="height: 600px; padding: 10px; overflow-x: hidden; overflow-y: scroll;">  
-                  <div style="float: left; height: 400px; width: 50%; position: relative; top: 15px;">
+                  <div style="float: left; height: 400px; width: 60%; position: relative; top: 15px;">
                     <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Treatment</h7>
                     <p style="margin: 15px; padding: 5px;">
                         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
@@ -78,7 +78,22 @@
                         The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                     </p>
                   </div>
+                  <div style="float: left; height: 400px; width: 35%; position: relative; top: 15px;">
+                    <div style="padding: 10px;">
+                      <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Client Intake</h7>
+                      <div class="inline-centered" style="background-color: #ccc; height: 100px;">
+                        <button class="button" style="margin: 33px auto;">Intake Form</button>
+                      </div>
+                    </div>
+                    <div style="padding: 10px;">
+                      <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Prescriptions</h7>
+                      <div class="inline-centered" style="background-color: #ccc; height: 100px;">
+                        <button class="button" style="margin: 33px auto;">Prescriptions</button>
+                      </div>
+                    </div>
+                  </div>
               </div>
+
           </div>
 
           <Flyout :active="true" :onClose="null" heading="Record History" style="width: 20%; z-index: 0;">
