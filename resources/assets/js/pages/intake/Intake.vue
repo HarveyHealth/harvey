4928343f4c2d1238d48b5de0417a5c25d635373b
Andrew Rossi <aroccorossi@gmail.com>
2017-07-17 10:48:08 -0400

    height fix for the form

--- resources/assets/js/pages/intake/Intake.vue
+++ resources/assets/js/pages/intake/Intake.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="signup-wrapper">
     <div class="container">
-      <div class="typeform-widget" data-url="https://kyleahill.typeform.com/to/XGnCna" data-transparency="50" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 500px;" ></div>
+      <div class="typeform-widget" data-url="https://kyleahill.typeform.com/to/XGnCna" data-transparency="50" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 80vh;" ></div>
       <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;" >powered by <a href="https://www.typeform.com/examples/?utm_campaign=XGnCna&amp;utm_source=typeform.com-9034666-Pro&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=EN" style="color: #999" target="_blank">Typeform</a></div>
     </div>
   </div>
