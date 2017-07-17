f7c40a2dcb8e85380f27f1eda129f68b2c73da81
laynefaler <laynefaler@gmail.com>
2017-07-17 11:24:04 -0700

    record page styling

--- resources/assets/js/pages/records/Records.vue
+++ resources/assets/js/pages/records/Records.vue
@@ -32,42 +32,63 @@
               <input v-modal="search" placeholder="Search by name, email or date of birth..." @keydown="updateInput($event)" type="text" class="search-bar" />
             </form>
 
-              <div style="height: 600px;">  
+              <div style="height: 800px;">  
                 <div class="card" style="width: 76%;">
                   <div class="card-heading-container">
                       <div>
                         Doctor with Patient
                       </div>
                   </div>
-
-                    <div style="height: 600px; padding: 10px; overflow-x: hidden; overflow-y: scroll;">  
-                        <div style="float: left; height: 400px; width: 60%; position: relative; top: 15px;">
-                          <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Treatment</h7>
-                          <p style="margin: 15px; padding: 5px;">
-                              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                              <br>
-                              <br>
-                              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                              <br>
-                              <br>
-                              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                          </p>
+                    <div style="height: 800px; padding: 10px; overflow-x: hidden; overflow-y: scroll;">  
+                        
+                        <div style="float: left; width: 64%; position: relative; top: 25px;">
+                          <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Subject</h7>
+                          <textarea class="input--textarea" placeholder="Enter your text..." style="min-height: 100px; margin: 15px;" />
                         </div>
-                        <div style="float: left; height: 400px; width: 35%; position: relative; top: 15px;">
+
+                        <div style="float: left; width: 35%; position: relative; top: 15px; left: 15px;">
                           <div style="padding: 10px;">
                             <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Client Intake</h7>
-                            <div class="inline-centered" style="background-color: #f8f8f8; height: 100px;">
+                            <div class="inline-centered" style="background-color: #f8f8f8; height: 100px; margin: 15px;">
                               <button class="button" style="margin: 33px auto;">Intake Form</button>
                             </div>
                           </div>
-                          <div style="padding: 10px;">
-                            <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Prescriptions</h7>
-                            <div class="inline-centered" style="background-color: #f8f8f8; height: 100px;">
-                              <button class="button" style="margin: 33px auto;">Prescriptions</button>
-                            </div>
-                          </div>
                         </div>
-                    </div>
+
+                        <div style="width: 97%; position: relative; top: 15px;">
+                            <h7 class="card-header" style="height: 20px; margin: 20px 15px; padding: 5px;">Objective</h7>
+                            <textarea class="input--textarea" placeholder="Enter your text..." style="min-height: 100px; margin: 15px;" />
+                          </div>
+
+                          <div style="width: 97%; position: relative; top: 15px;">
+                            <h7 class="card-header" style="height: 20px; margin: 20px 15px; padding: 5px;">Assessment</h7>
+                            <textarea class="input--textarea" placeholder="Enter your text..." style="min-height: 100px; margin: 15px;"/>
+                          </div>
+
+                          <div style="color: #EDA1A6; padding: 5px; padding-top: 10px; width: 97%; margin: 0 20px;">
+                               - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - FIELDS BELOW THIS LINE VISIBLE TO PATIENT  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                          </div>
+
+                            <div style="float: left; width: 64%; position: relative; top: 25px;">
+                              <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Treatment</h7>
+                              <textarea class="input--textarea" placeholder="Enter your text..." style="min-height: 100px; margin: 15px;" />
+                            </div>
+
+                            <div style="float: left; width: 35%; position: relative; top: 15px; left: 15px;">
+                              <div style="padding: 10px;">
+                                <h7 class="card-header" style="height: 20px; margin: 15px; padding: 5px;">Prescription</h7>
+                                <div class="inline-centered" style="background-color: #f8f8f8; height: 100px; margin: 15px;">
+                                  <button class="button" style="margin: 33px auto;">Prescription</button>
+                                </div>
+                              </div>
+                          </div>
+                          
+                          <div class="inline-centered">
+                            <button class="button" style="margin-top: 35px;">Save Changes</button>
+                          </div>
+
+                      </div>
+
                 </div>
                 <Flyout :active="true" :onClose="null" heading="Record History" style="width: 20%; z-index: 0;">
                   <div style="border-bottom: 1px solid #F4F4F4; margin-bottom: 30px;">
@@ -78,33 +99,30 @@
                         <div style="color: #82BEF2;" class="input__label" for="patient_name">phone</div>
                     </div>
                   </div>
-                  <div style="border-bottom: 1px solid #F4F4F4; margin-bottom: 30px;">
+                   <div style="border-bottom: 1px solid #F4F4F4; margin-bottom: 30px;">
                     <div class="input__container">
-                        <div style="color: #82BEF2; float: left; width: 50%;" class="input__label" for="patient_name">ID</div>
-                        <div style="color: #82BEF2; float: left; width: 50%;" class="input__label" for="patient_name">JOINED</div>
-                        <div style="color: #82BEF2; float: left; width: 50%;" class="input__label" for="patient_name">DOB</div>
-                        <div style="color: #82BEF2; float: left; width: 50%;" class="input__label" for="patient_name">CITY</div>
-                        <div style="color: #82BEF2; float: left; width: 100%;" class="input__label" for="patient_name">STATE</div>
+                        <span style="color: #82BEF2; width: 50%;" class="input__label" for="patient_name">ID</span>
+                         <span style="color: #82BEF2; width: 50%;" class="input__label" for="patient_name">JOINED</span>
+                        <span style="color: #82BEF2; width: 50%;" class="input__label" for="patient_name">DOB</span>
+                        <span style="color: #82BEF2; width: 50%;" class="input__label" for="patient_name">CITY</span>
+                        <span style="color: #82BEF2; width: 100%;" class="input__label" for="patient_name">STATE</span> 
                     </div>
-                  </div>
-                  <div style="border-bottom: 1px solid #F4F4F4; margin-bottom: 30px;">
-                    <div class="input__container">
-                        <label class="input__label" for="patient_name">lab notes</label>
-                        <span style="color: #82BEF2; float: left;" class="input__label" for="patient_name">Dr. Amanda Frick</span>
-                        <span style="color: #82BEF2; float: right;" class="input__label" for="patient_name">12/18/15</span>
-                    </div>
-                  </div>
+                   </div>  
                   <div style="border-bottom: 1px solid #F4F4F4; margin-bottom: 30px;">
                     <div class="input__container">
                         <label class="input__label" for="patient_name">appointments</label>
-                        <span style="color: #82BEF2; float: left;" class="input__label" for="patient_name">Dr. Amanda Frick</span>
-                        <span style="color: #82BEF2; float: right;" class="input__label" for="patient_name">12/18/15</span>
+                        <div style="padding: 15px 0;">
+                          <span style="color: #82BEF2; float: left;" class="input__label" for="patient_name">Dr. Amanda Frick</span>
+                          <span style="color: #82BEF2; float: right;" class="input__label" for="patient_name">12/18/15</span>
+                        </div>
                     </div>
                   </div>
                   <div class="input__container">
                       <label class="input__label" for="patient_name">lab tests</label>
-                      <span style="color: #82BEF2; float: left;" class="input__label" for="patient_name">Micronutrients</span>
-                      <span style="color: #82BEF2; float: right;" class="input__label" for="patient_name">12/18/15</span>
+                      <div style="padding: 15px 0;">
+                        <span style="color: #82BEF2; float: left;" class="input__label" for="patient_name">Micronutrients</span>
+                        <span style="color: #82BEF2; float: right;" class="input__label" for="patient_name">12/18/15</span>
+                      </div>
                   </div>
                 </Flyout>
 
