9ca96ad174e85555741af4d88b4969f133cf3db8
Andrew Rossi <aroccorossi@gmail.com>
2017-07-17 11:49:26 -0400

    commenting out the route

--- resources/views/pages/intake.blade.php
+++ resources/views/pages/intake.blade.php
@@ -19,13 +19,13 @@
       window.Laravel = {!! $vue_data !!}
       window.$$context = 'intake';
 
-      if (!Laravel.user.signedIn) {
-        window.location.hash = '/signup';
-      } else if (Laravel.user.has_an_appointment || Laravel.user.user_type !== 'patient') {
-        window.location.href = '/dashboard';
-      } else {
-        window.location.hash = '/welcome';
-      }
+      // if (!Laravel.user.signedIn) {
+      //   window.location.hash = '/signup';
+      // } else if (Laravel.user.has_an_appointment || Laravel.user.user_type !== 'patient') {
+      //   window.location.href = '/dashboard';
+      // } else {
+      //   window.location.hash = '/welcome';
+      // }
 
     </script>
 
