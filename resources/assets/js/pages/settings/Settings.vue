0049eeddcb5701ba4fb9c10203576950d804f548
laynefaler <laynefaler@gmail.com>
2017-07-21 12:46:50 -0700

    credit card styling

--- resources/assets/js/pages/settings/Settings.vue
+++ resources/assets/js/pages/settings/Settings.vue
@@ -12,28 +12,28 @@
                 <div class="card-heading-container">
                     <h1 class="card-header">Payment Details</h1>
                 </div>
-                <div :style="{height: details ? '500px' : '125px'}">
+                <div :style="{height: details ? '460px' : '125px'}">
 
                     <div v-if="!details" class="inline-centered">
                         <button @click="addCard" class="button" style="margin-top: 35px;">Add Card</button>
                     </div>
 
                     <div v-if="details" style="padding: 20px;">
-                        <div class="input__container" style="margin-bottom: 1.5em;">
+                        <div class="input__container length" style="margin-bottom: 1.5em;">
                             <label class="input__label" for="patient_name">card number</label>
                             <input placeholder="Enter card number" v-model="cardNumber" class="input--text" type="text">
                         </div>
-                        <div class="input__container">
+                        <div class="input__container length">
                             <label class="input__label" for="patient_name">name on card</label>
                             <input placeholder="First name" style="width: 48%; float: left;" v-model="firstName" class="input--text" type="text">
                             <input placeholder="Last name" style="width: 48%; float: right;" v-model="lastName" class="input--text" type="text">
                         </div>
-                        <div class="input__container" style="padding-top: 25px;">
+                        <div class="input__container length" style="padding-top: 25px;">
                             <label class="input__label" for="patient_name">expiry date</label>
                             <input placeholder="Month" style="width: 48%; float: left;" v-model="month" class="input--text" type="text">
                             <input placeholder="Year" style="width: 48%; float: right;" v-model="year" class="input--text" type="text">
                         </div>
-                        <div class="input__container" style="padding-top: 25px;">
+                        <div class="input__container length" style="padding-top: 25px;">
                             <label style="width: 53%; float: left;" class="input__label" for="patient_name">security code</label>
                             <label style="width: 47%; float: left;" class="input__label" for="patient_name">zip code</label>
                             <input placeholder="CVV" style="width: 48%; float: left;" v-model="securityCode" class="input--text" type="text">
@@ -80,3 +80,9 @@ export default {
     }
 }
 </script>
+
+<style>
+    .length {
+        width: 100% !important;
+    }
+</style>
