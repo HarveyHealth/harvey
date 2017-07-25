b2f43003c60b2e12dd6f469438837d53288f942d
jonmcgill <jon.d.mcgill@gmail.com>
2017-07-25 11:44:38 -0400

    Styles the Billing inputs section

--- resources/assets/js/pages/get-started/children/Billing.vue
+++ resources/assets/js/pages/get-started/children/Billing.vue
@@ -16,6 +16,41 @@
       <router-link class="signup-back-button" :to="{ name: 'schedule', path: '/schedule' }"><i class="fa fa-long-arrow-left"></i><span>Schedule</span></router-link>
 
       <div class="phone-input-container">
+        <div class="input-container cf">
+          <!-- CARD NUMBER -->
+          <div class="input-wrap">
+            <input class="form-input form-input_text font-base font-darkest-gray"
+                   name="card_number" type="text" placeholder="Card Number" maxlength="16"
+                  v-validate="{ required: true, digits: 16 }" data-vv-as="Card Number" data-vv-validate-on="blur" />
+            <span v-show="errors.has('card_number')" class="error-text">{{ errors.first('card_number') }}</span>
+          </div>
+          <!-- CARD NAME -->
+          <div class="input-wrap">
+            <input class="form-input form-input_text font-base font-darkest-gray"
+                   name="card_name" type="text" placeholder="Name on Card"
+                  v-validate="'required|alpha_spaces'" data-vv-as="Name on Card" data-vv-validate-on="blur" />
+            <span v-show="errors.has('card_name')" class="error-text">{{ errors.first('card_name') }}</span>
+          </div>
+          <div>
+            <!-- CARD EXPIRATION DATE -->
+            <div class="input-wrap input-half--sm">
+              <input class="form-input form-input_text font-base font-darkest-gray"
+                     name="card_expiration" type="text" placeholder="MMYY" maxlength="4"
+                    v-validate="{ required: true, digits: 4 }" data-vv-as="Card Expiration" data-vv-validate-on="blur" />
+              <span v-show="errors.has('card_expiration')" class="error-text">{{ errors.first('card_expiration') }}</span>
+            </div>
+            <!-- CARD CVC NUMBER -->
+            <div class="input-wrap input-half--sm last">
+              <input class="form-input form-input_text font-base font-darkest-gray"
+                     name="card_cvc" type="text" placeholder="CVC" maxlength="3"
+                    v-validate="{ required: true, digits: 3 }" data-vv-as="Card CVC" data-vv-validate-on="blur" />
+              <span v-show="errors.has('card_cvc')" class="error-text">{{ errors.first('card_cvc') }}</span>
+            </div>
+          </div>
+        </div>
+
+        <p>We will not charge you. You can confirm your appointment date and time on the next page.</p>
+
         <button class="button button--blue" style="width: 180px" :disabled="isProcessingInfo">
           <span v-if="!isProcessingInfo">Save &amp; Continue</span>
           <LoadingBubbles v-else-if="isProcessingInfo" :style="{ width: '12px', fill: 'white' }" />
