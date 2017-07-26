9373a1bc5c076bf5054a02a5b596b45e7361ff8f
jonmcgill <jon.d.mcgill@gmail.com>
2017-07-25 22:29:28 -0400

    Updates to use card.js plugin for billing page

--- resources/assets/js/pages/get-started/children/Billing.vue
+++ resources/assets/js/pages/get-started/children/Billing.vue
@@ -4,58 +4,40 @@
       <StagesNav :current="'billing'" />
       <h2 v-text="title"></h2>
       <p v-html="subtext"></p>
-      <div :class="{ 'credit-card': true, 'isInactive': cardInactive }">
-        <div class="credit-card__chip"></div>
-        <div class="credit-card__brand"><svg><use :xlink:href="'#cc'+cardBrand" /></svg></div>
-        <div class="credit-card__number">{{ cardNumber | cardNumberFilter }}</div>
-        <div class="credit-card__name">{{ cardName | cardNameFilter }}</div>
-        <div class="credit-card__date">{{ cardExpiration | cardExpirationFilter }}</div>
-      </div>
+      <div class="credit-card" v-show="!isComplete"></div>
     </div>
     <div class="signup-container signup-phone-container text-centered">
-      <router-link class="signup-back-button" :to="{ name: 'schedule', path: '/schedule' }"><i class="fa fa-long-arrow-left"></i><span>Schedule</span></router-link>
+      <router-link class="signup-back-button" :to="{ name: 'schedule', path: '/schedule' }">
+        <i class="fa fa-long-arrow-left"></i><span>Schedule</span>
+      </router-link>
 
       <div class="phone-input-container">
-        <div class="input-container cf">
-          <!-- CARD NUMBER -->
+        <form id="credit-card-form" class="input-container cf">
           <div class="input-wrap">
             <input class="form-input form-input_text font-base font-darkest-gray"
-                   :disabled="isComplete"
-                   name="card_number" type="text" placeholder="Card Number" maxlength="16" v-model="cardNumber"
-                   v-validate="{ required: true, digits: 16 }" data-vv-as="Card Number" data-vv-validate-on="blur" />
-            <span v-show="errors.has('card_number')" class="error-text">{{ errors.first('card_number') }}</span>
+                  :disabled="isComplete" name="card_number" type="text" placeholder="Card Number" v-model="cardNumber" />
           </div>
-          <!-- CARD NAME -->
           <div class="input-wrap">
             <input class="form-input form-input_text font-base font-darkest-gray"
-                   :disabled="isComplete"
-                   name="card_name" type="text" placeholder="Name on Card" v-model="cardName"
-                   v-validate="'required|alpha_spaces'" data-vv-as="Name on Card" data-vv-validate-on="blur" />
-            <span v-show="errors.has('card_name')" class="error-text">{{ errors.first('card_name') }}</span>
+                  :disabled="isComplete" name="card_name" type="text" placeholder="Name on Card" v-model="cardName" />
           </div>
           <div>
-            <!-- CARD EXPIRATION DATE -->
             <div class="input-wrap input-half--sm">
               <input class="form-input form-input_text font-base font-darkest-gray"
-                     :disabled="isComplete"
-                     name="card_expiration" type="text" placeholder="MMYY" maxlength="4" v-model="cardExpiration"
-                     v-validate="{ required: true, digits: 4 }" data-vv-as="Card Expiration" data-vv-validate-on="blur" />
-              <span v-show="errors.has('card_expiration')" class="error-text">{{ errors.first('card_expiration') }}</span>
+                    :disabled="isComplete" name="card_expiration" type="text" placeholder="MM/YY" v-model="cardExpiration" />
             </div>
-            <!-- CARD CVC NUMBER -->
             <div class="input-wrap input-half--sm last">
               <input class="form-input form-input_text font-base font-darkest-gray"
-                     :disabled="isComplete"
-                     name="card_cvc" type="text" placeholder="CVC" maxlength="3" v-model="cardCvc"
-                     v-validate="{ required: true, digits: 3 }" data-vv-as="Card CVC" data-vv-validate-on="blur" />
-              <span v-show="errors.has('card_cvc')" class="error-text">{{ errors.first('card_cvc') }}</span>
+                    :disabled="isComplete" name="card_cvc" type="text" placeholder="CVC" v-model="cardCvc" />
             </div>
           </div>
-        </div>
+        </form>
 
         <p>We will not charge you. You can confirm your appointment date and time on the next page.</p>
-        <p class="error-text" v-show="stripeError.length">{{ stripeError }}</p>
-        <button class="button button--blue" style="width: 180px" :disabled="isProcessing || isComplete" @click="onSubmit">
+
+        <p class="error-text" v-show="stripeError.length" v-html="stripeError"></p>
+
+        <button class="button button--blue" style="width: 180px" :disabled="isProcessing || isComplete" @click="onSubmit($event)">
           <span v-if="!isProcessing && !isComplete">Save &amp; Continue</span>
           <LoadingBubbles v-else-if="isProcessing" :style="{ width: '12px', fill: 'white' }" />
           <i v-else-if="isComplete" class="fa fa-check"></i>
@@ -67,6 +49,7 @@
 </template>
 
 <script>
+import card from 'card';
 import LoadingBubbles from '../../../commons/LoadingBubbles.vue';
 import StagesNav from '../util/StagesNav.vue';
 
@@ -78,43 +61,20 @@ export default {
   },
   data() {
     return {
-      cardBrand: '',
-      cardCvc: '',
-      cardExpiration: '',
-      cardName: '',
-      cardNumber: '',
+      card: null,
+      cardCvc: this.$root.$data.signup.cardCvc || '',
+      cardExpiration: this.$root.$data.signup.cardExpiration || '',
+      cardName: this.$root.$data.signup.cardName || '',
+      cardNumber: this.$root.$data.signup.cardNumber || '',
       containerClasses: {
         'anim-fade-slideup': true,
         'anim-fade-slideup-in': false,
         'container': true,
       },
-      isComplete: false,
+      isComplete: this.$root.$data.signup.billingConfirmed,
       isProcessing: false,
       stripeKey: 'pk_test_P1CltTCM3UIMzka8O1p7J2MT',
       stripeError: '',
-      subtext: 'Please enter a credit or debit card to save on file. We will not charge your card until after your first consultation is complete.',
-      title: 'Enter Payment Method',
-    }
-  },
-  watch: {
-    cardNumber(value) {
-      this.cardBrand = Stripe.card.cardType(value);
-    }
-  },
-  filters: {
-    cardExpirationFilter(value) {
-      let exp = value.split('');
-      exp.splice(2, 0, '/');
-      return value.length ? exp.join('') : 'MM/YY';
-    },
-    cardNameFilter(value) {
-      return value.toUpperCase().substring(0,18) || 'FULL NAME';
-    },
-    cardNumberFilter(value) {
-      return value
-        .split('')
-        .map((num, i) => (i + 1) % 4 === 0 ? `${num} ` : num)
-        .join('') || '•••• •••• •••• ••••';
     }
   },
   computed: {
@@ -122,51 +82,79 @@ export default {
       return {
         number: this.cardNumber,
         exp_month: this.cardExpiration.substring(0, 2),
-        exp_year: this.cardExpiration.substring(2, 4),
+        exp_year: this.cardExpiration.substring(5),
         cvc: this.cardCvc,
         address_zip: Laravel.user.zip,
         name: this.cardName,
       }
     },
-    cardInactive() {
-      return !this.cardCvc && !this.cardExpiration && !this.cardName && !this.cardNumber;
+    subtext() {
+      return this.isComplete
+        ? ''
+        : 'Please enter a credit or debit card to save on file. We will not charge your card until after your first consultation is complete.';
+    },
+    title() {
+      return this.isComplete
+        ? 'Payment Method Confirmed!'
+        : 'Enter Payment Method';
     }
   },
   methods: {
-    onSubmit() {
+    onSubmit(e) {
+      e.preventDefault();
       this.toggleProcessing();
       this.stripeError = '';
 
-      this.$validator.validateAll().then(response => {
-        if (!response) {
-          this.toggleProcessing();
-          return;
+      const errors = this.validateCardInputs();
+      if (errors) {
+        this.setStripeError(errors);
+        return;
+      }
+
+      // Setup stripe and create user token
+      Stripe.card.createToken(this.cardData, (status, response) => {
+        if (response.error) {
+          this.setStripeError(response.error.message)
         } else {
-          // Setup stripe and create user token
-          Stripe.card.createToken(this.cardData, (status, response) => {
-            if (response.error) {
-              this.toggleProcessing();
-              this.stripeError = response.error.message;
-            } else {
-              this.markComplete();
-              const stripeData = {
-                stripe_brand: response.card.brand,
-                stripe_customer_id: response.id,
-                stripe_expiry_month: response.card.exp_month,
-                stripe_expiry_year: response.card.exp_year,
-                stripe_last_four: response.card.last4
-              }
-            }
-          });
+          this.markComplete();
+          const stripeData = {
+            stripe_brand: response.card.brand,
+            stripe_customer_id: response.id,
+            stripe_expiry_month: response.card.exp_month,
+            stripe_expiry_year: response.card.exp_year,
+            stripe_last_four: response.card.last4
+          }
+          // Todo: path user data
+          console.log('Patch user data with stripe information here');
+          // on callback
+          setTimeout(() => {
+            this.$router.push({ name: 'confirmation', path: '/confirmation' });
+          }, 500);
         }
-      }).catch(error => {});
+      });
     },
     markComplete() {
-      this.isComplete = true;
+      this.$root.$data.signup.billingConfirmed = true;
+      this.$root.$data.signup.cardCvc = this.cardCvc;
+      this.$root.$data.signup.cardExpiration = this.cardExpiration;
+      this.$root.$data.signup.cardName = this.cardName;
+      this.$root.$data.signup.cardNumber = this.cardNumber;
       this.isProcessing = false;
+    },
+    setStripeError(msg) {
+      this.toggleProcessing();
+      this.stripeError = msg;
     },
     toggleProcessing() {
       this.isProcessing = !this.isProcessing;
+    },
+    validateCardInputs() {
+      let result = '';
+      if (!this.cardNumber.length) result += 'Card number is blank<br>'
+      if (!this.cardName.length) result += 'Card name is blank<br>'
+      if (!this.cardExpiration.length) result += 'Card expiration is blank<br>'
+      if (!this.cardCvc.length) result += 'Card CVC is blank<br>'
+      return result;
     }
   },
   mounted () {
@@ -174,6 +162,18 @@ export default {
     Stripe.setPublishableKey(this.stripeKey);
     this.$root.$data.signup.visistedStages.push('billing');
     this.$eventHub.$emit('animate', this.containerClasses, 'anim-fade-slideup-in', true, 300);
+
+    // Card.js - https://github.com/jessepollak/card
+    this.card = new Card({
+      container: '.credit-card',
+      form: '#credit-card-form',
+      formSelectors: {
+        numberInput: 'input[name="card_number"]',
+        expiryInput: 'input[name="card_expiration"]',
+        cvcInput: 'input[name="card_cvc"]',
+        nameInput: 'input[name="card_name"]'
+      },
+    });
   },
   beforeDestroy() {
     this.$eventHub.$emit('animate', this.containerClasses, 'anim-fade-slideup-in', false);
