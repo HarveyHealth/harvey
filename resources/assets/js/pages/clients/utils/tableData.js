1fddf9bb72bf838e0a32d72fa79ca6105022463e
laynefaler <laynefaler@gmail.com>
2017-07-27 13:35:52 -0700

    removing null from location

--- resources/assets/js/pages/clients/utils/tableData.js
+++ resources/assets/js/pages/clients/utils/tableData.js
@@ -13,7 +13,7 @@ export default function(clientList) {
             values: [
                 `${_.capitalize(data.first_name)} ${_.capitalize(data.last_name)}`,
                 data.created_at ? moment(data.created_at.date).format('ddd, MMM Do YYYY') : 'N/A',
-                `${data.city}, ${data.state}`,
+                data.city && data.state ? `${data.city}, ${data.state}` : 'N/A',
                 data.doctor_name ? `Dr. ${data.doctor_name}` : 'N/A',
                 data.has_an_appointment ? 'Yes' : 'No',
                 data.has_completed_an_appointment ? 'Yes' : 'No'
