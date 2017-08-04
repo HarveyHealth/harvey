b07dd1e74b9bc930eab740e08a1c4ac3d54400b7
Tate Peñaranda <tate@goharvey.com>
2017-08-04 13:38:45 -0300

    Update new calendar ID.

--- config/services.php
+++ config/services.php
@@ -83,6 +83,6 @@ return [
     'google_calendar' => [
         'client_secret_file' => storage_path('calendar_api/client_secret.json'),
         'access_token_file' => storage_path('calendar_api/access_token.json'),
-        'calendar_id' => 'goharvey.com_bfg97p33qthpp72t8g2c7ic8bk@group.calendar.google.com',
+        'calendar_id' => 'goharvey.com_l6hee9sjegfi86pik8mu0gu51o@group.calendar.google.com',
     ],
 ];
