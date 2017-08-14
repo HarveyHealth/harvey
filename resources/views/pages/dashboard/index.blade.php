efbac77312b692535f693d37beeca905b66053c5
laynefaler <laynefaler@gmail.com>
2017-08-14 09:31:40 -0700

    switching branches

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -51,7 +51,7 @@
     {{-- To add data here, see the VueHelperViewComposer --}}
 
     <!-- Stripe -->
-    <script type="text/javascript" src="https://js.stripe.com/v2"></script>
+    <script type="text/javascript" src="https://js.stripe.com/v3"></script>
 
     <!-- App.js -->
     <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
--- resources/views/pages/getstarted.blade.php
+++ resources/views/pages/getstarted.blade.php
@@ -68,7 +68,7 @@
       </script>
 
       <!-- Stripe -->
-      <script type="text/javascript" src="https://js.stripe.com/v2"></script>
+      <script type="text/javascript" src="https://js.stripe.com/v3"></script>
 
       <!-- App.js -->
       <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
