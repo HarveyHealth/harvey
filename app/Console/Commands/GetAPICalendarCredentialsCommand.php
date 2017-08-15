069d7789036c2b925331418fbbf52b5d38f3daae
Tate Peñaranda <tate@goharvey.com>
2017-08-15 14:00:37 -0300

    One more Update to Calendar command.

--- app/Console/Commands/GetAPICalendarCredentialsCommand.php
+++ app/Console/Commands/GetAPICalendarCredentialsCommand.php
@@ -19,8 +19,11 @@ class GetAPICalendarCredentialsCommand extends Command
                 File::makeDirectory($path);
             }
 
-            file_put_contents('storage/calendar_api/access_token.json', Storage::disk('s3')->get('calendar/access_token.json'));
-            file_put_contents('storage/calendar_api/client_secret.json', Storage::disk('s3')->get('calendar/client_secret.json'));
+            $accessToken = Storage::disk('s3')->get('calendar/access_token.json');
+            file_put_contents('storage/calendar_api/access_token.json', $accessToken);
+
+            $clientSecret = Storage::disk('s3')->get('calendar/client_secret.json');
+            file_put_contents('storage/calendar_api/client_secret.json', $clientSecret);
 
             $this->info('Harvey Google Calendar API credentials successfully!');
         } catch (Exception $e) {
