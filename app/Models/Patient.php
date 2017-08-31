ce110a035874aff421a3881f2777b1035111d4bb
Tate Peñaranda <tate@goharvey.com>
2017-08-31 01:55:20 -0300

    Update to Patient model.

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\{Model, Builder};
 
 class Patient extends Model
 {
-    protected $guarded = ['id', 'enabled', 'user_id', 'stripe_customer_id',
-                            'stripe_expiry_month', 'stripe_expiry_year',
-                            'stripe_brand', 'stripe_last_four',
-                            'created_at', 'updated_at'];
+    protected $guarded = [
+        'created_at',
+        'enabled',
+        'id',
+        'stripe_brand',
+        'stripe_customer_id',
+        'stripe_expiry_month',
+        'stripe_expiry_year',
+        'stripe_last_four',
+        'intake_token',
+        'updated_at',
+        'user_id',
+    ];
 
     protected $dates = ['created_at','updated_at'];
 
@@ -47,5 +56,20 @@ class Patient extends Model
     public function test()
     {
         return $this->hasMany(Test::class, 'patient_id', 'id');
+    }
+
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class, 'patient_id', 'id');
+    }
+
+    public function soapNotes()
+    {
+        return $this->hasMany(SOAPNote::class, 'patient_id', 'id');
+    }
+
+    public function prescriptions()
+    {
+        return $this->hasMany(Prescription::class, 'patient_id', 'id');
     }
 }
