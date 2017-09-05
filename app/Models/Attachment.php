a0a1acc99b22f74635dfd4e3e39794aa3b07dca5
Tate Peñaranda <tate@goharvey.com>
2017-09-05 13:16:53 -0300

    Attachment model.

---
+++ app/Models/Attachment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\{Model, Builder};
+use Carbon;
+
+class Attachment extends Model
+{
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'created_by_user_id',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('enabledUser', function (Builder $builder) {
+            return $builder->whereHas('patient', function (Builder $query) {
+                return $builder->whereHas('user', function (Builder $query) {
+                    return $query->where('users.enabled', true);
+                });
+            });
+        });
+    }
+
+    /*
+     * Relationships
+     */
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
+}
