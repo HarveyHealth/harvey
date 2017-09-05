4127b1f7cb8c6aba34ecf7df277a73c98baaace5
Tate Peñaranda <tate@goharvey.com>
2017-09-05 11:56:05 -0300

    Phone verifier route format fix.

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'API\V1'], function () {
         Route::patch('users/{user}', 'UsersController@update')->name('users.update');
         Route::post('users/{user}/image', 'UsersController@profileImageUpload')->name('users.profile-image-upload');
         Route::get('users/{user}/phone/verify', 'UsersController@phoneVerify')->name('users.phoneVerify');
-        Route::post('users/{user}/phone/sendverificationcode', 'UsersController@sendVerificationCode')->name('users.sendVerificationCode');
+        Route::post('users/{user}/phone/send_verification_code', 'UsersController@sendVerificationCode')->name('users.sendVerificationCode');
 
         Route::get('patients', 'PatientsController@index')->name('patients.index');
         Route::get('patients/{patient}', 'PatientsController@show')->name('patients.show');
