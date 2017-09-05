949e9bdc7421d610eb8c25227d684623ce2f93d3
Tate Peñaranda <tate@goharvey.com>
2017-09-05 19:25:01 -0300

    InitialDatabaseSeeder class update according with DB schema updates.

--- database/seeds/InitialDatabaseSeeder.php
+++ database/seeds/InitialDatabaseSeeder.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Laravel\Passport\Client;
+use App\Models\{
+    License,
+    Practitioner,
+    PractitionerSchedule,
+    PractitionerType,
+    User
+};
 
 class InitialDatabaseSeeder extends Seeder
 {
@@ -10,61 +18,58 @@ class InitialDatabaseSeeder extends Seeder
         // have at least a DO and an ND with some scheduled availability.
         // We will also need to have an Oauth Client for the Vue app to consume
         // The API.
-        $pt1 = \App\Models\PractitionerType::create([
-            'enabled' => true,
-            'name' => 'Naturopathic Doctor',
-            'rate' => 150.00
-        ]);
 
-        $nd = \App\Models\User::create([
-            'first_name' => 'Amanda',
-            'last_name' => 'Frick',
-            'email' =>  'test+amandafrick@goharvey.com',
-            'phone' => '3101231234',
-            'password' => bcrypt('secret'),
-            'timezone' => 'America/Los_Angeles'
-        ]);
+        Practitioner::create([
+            'practitioner_type' => PractitionerType::create([
+                'name' => 'Naturopathic Doctor',
+                'rate' => 150.00,
+            ])->id,
+            'user_id' => User::create([
+                'first_name' => 'Amanda',
+                'last_name' => 'Frick',
+                'email' =>  'test+amandafrick@goharvey.com',
+                'phone' => '3101231234',
+                'password' => bcrypt('secret'),
+                'timezone' => 'America/Los_Angeles',
+            ])->id,
+        ])->each(function ($practitioner) {
+            $practitioner->schedule()->save(factory(PractitionerSchedule::class)->make([
+                'day_of_week' => 'Wednesday',
+                'start_time' => '08:00:00',
+                'stop_time' => '10:00:00',
+            ]));
+            $practitioner->licenses()->save(factory(License::class)->make([
+                'state' => 'CA',
+                'title' => 'ND',
+            ]));
+        });
 
-        $practitioner1 = \App\Models\Practitioner::create([
-            'practitioner_type' => $pt1->id,
-            'user_id' => $nd->id
-        ]);
+        Practitioner::create([
+            'practitioner_type' => PractitionerType::create([
+                'name' => 'Doctor of Osteopathy',
+                'rate' => 300.00
+            ])->id,
+            'user_id' => User::create([
+                'first_name' => 'Rachel',
+                'last_name' => 'West',
+                'email' =>  'test+rachelwest@goharvey.com',
+                'phone' => '3101231235',
+                'password' => bcrypt('secret'),
+                'timezone' => 'America/Los_Angeles'
+            ])->id,
+        ])->each(function ($practitioner) {
+            $practitioner->schedule()->save(factory(PractitionerSchedule::class)->make([
+                'day_of_week' => 'Monday',
+                'start_time' => '11:00:00',
+                'stop_time' => '15:00:00',
+            ]));
+            $practitioner->licenses()->save(factory(License::class)->make([
+                'state' => 'CA',
+                'title' => 'DO',
+            ]));
+        });
 
-        \App\Models\PractitionerSchedule::create([
-            'practitioner_id' => $practitioner1->id,
-            'day_of_week' => 'Wednesday',
-            'start_time' => '08:00:00',
-            'stop_time' => '10:00:00'
-        ]);
-
-        $pt2 = \App\Models\PractitionerType::create([
-            'enabled' => true,
-            'name' => 'Doctor of Osteopathy',
-            'rate' => 300.00
-        ]);
-
-        $do = \App\Models\User::create([
-            'first_name' => 'Rachel',
-            'last_name' => 'West',
-            'email' =>  'test+rachelwest@goharvey.com',
-            'phone' => '3101231235',
-            'password' => bcrypt('secret'),
-            'timezone' => 'America/Los_Angeles'
-        ]);
-
-        $practitioner2 = \App\Models\Practitioner::create([
-            'practitioner_type' => $pt2->id,
-            'user_id' => $do->id
-        ]);
-
-        \App\Models\PractitionerSchedule::create([
-            'practitioner_id' => $practitioner2->id,
-            'day_of_week' => 'Monday',
-            'start_time' => '11:00:00',
-            'stop_time' => '15:00:00'
-        ]);
-
-        factory(Laravel\Passport\Client::class)->create([
+        factory(Client::class)->create([
             'name' => 'Harvey-Vue',
             'secret' => 'Uew3kusLxnzNT5t4EcjxMM4Qso4XJDJbEO7tYyCu',
             'redirect' => config('app.url')
