8ddd5bf572615371d8badd2635565eaeb14736f8
jonmcgill <jon.d.mcgill@gmail.com>
2017-09-12 19:43:27 -0400

    add flexgrid to incorporate into grid component

--- resources/assets/js/v2/components/base/index.js
+++ resources/assets/js/v2/components/base/index.js
@@ -1,5 +1,7 @@
 import Layout from './layout';
 import Structures from './structures';
+import Util from './util';
 
 export { Layout }
 export { Structures }
+export { Util }
---
+++ resources/assets/js/v2/components/base/util/SlideIn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: 'top'
+    }
+  },
+  data() {
+    return {
+      style: {
+        'opacity': '0',
+        'position': 'relative',
+        [`${this.to}`]: '22px',
+        'transition': `${this.to} 200ms ease-in-out, opacity 200ms ease-in-out`,
+      }
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.style.opacity = '1';
+      this.style[this.to] = '0';
+    }, 100)
+  }
+}
+</script>
