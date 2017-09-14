2b7569a09eb88cc84ae68359e767411d7530e933
Tate Peñaranda <tate@goharvey.com>
2017-09-14 00:22:05 -0300

    Added Cache to AWS upload URL.

--- app/Http/Traits/HasKeyColumn.php
+++ app/Http/Traits/HasKeyColumn.php
@@ -3,28 +3,40 @@
 namespace App\Http\Traits;
 
 use Aws\S3\Exception\S3Exception;
-use Bugsnag, Storage;
+use App\Lib\TimeInterval;
+use Bugsnag, Cache, Storage;
 
 trait HasKeyColumn
 {
     public function getUrlAttribute()
     {
-        if (empty($this->key)) {
+        if (empty($key = $this->key)) {
             return null;
         }
 
-        try {
-            $client = Storage::disk('s3')->getDriver()->getAdapter()->getClient();
-            $command = $client->getCommand('GetObject', [
-                'Bucket' => config('filesystems.disks.s3.bucket'),
-                'Key' => $this->key,
-            ]);
-            $request = $client->createPresignedRequest($command, '+1 hour');
-        } catch (S3Exception $e) {
-            Bugsnag::notifyException($e);
+        $cacheKey = "aws-uri-for-{$key}";
+
+        $uri = Cache::remember($cacheKey, TimeInterval::hours(12)->toMinutes(), function () use ($key) {
+            try {
+                $client = Storage::disk('s3')->getDriver()->getAdapter()->getClient();
+                $command = $client->getCommand('GetObject', [
+                    'Bucket' => config('filesystems.disks.s3.bucket'),
+                    'Key' => $key,
+                ]);
+                $request = $client->createPresignedRequest($command, '+12 hours');
+            } catch (S3Exception $e) {
+                Bugsnag::notifyException($e);
+                return null;
+            }
+
+            return (string) $request->getUri();
+        });
+
+        if (empty($uri)) {
+            Cache::forget($cacheKey);
             return null;
         }
 
-        return (string) $request->getUri();
+        return $uri;
     }
 }
