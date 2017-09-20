ab61cb225bfe14cf2329e7f0ca6733ecae15e52c
laynefaler <laynefaler@gmail.com>
2017-09-19 17:36:33 -0700

    lab order optomized

--- resources/assets/js/pages/lab_orders/utils/tableDataTransform.js
+++ resources/assets/js/pages/lab_orders/utils/tableDataTransform.js
@@ -59,9 +59,11 @@ export default function (orders, tests, patientLookUp, practitionerLookup, testL
                     item_type: testList[Number(test.attributes.sku_id)].attributes.item_type,
                     price: testList[Number(test.attributes.sku_id)].attributes.price,
                     name: testList[Number(test.attributes.sku_id)].attributes.name,
-                    status: test.attributes.status !== 'Recommended' || test.attributes.status !== 'Confirmed' ? 
+                    status: test.attributes.status === 'Recommended' ?
+                        [capitalize(test.attributes.status)].concat(_.pull(['Recommended', 'Confirmed', 'Complete', 'Shipped', 'Received', 'Mailed', 'Processing', 'Canceled'], capitalize(test.attributes.status))) :
+                    test.attributes.status === 'Confirmed' ? 
                         [capitalize(test.attributes.status)].concat(_.pull(['Complete', 'Shipped', 'Received', 'Mailed', 'Processing', 'Canceled'], capitalize(test.attributes.status))) :
-                        [capitalize(test.attributes.status)].concat(_.pull(['Recommended', 'Confirmed', 'Complete', 'Shipped', 'Received', 'Mailed', 'Processing', 'Canceled'], capitalize(test.attributes.status))),
+                        [capitalize(test.attributes.status)].concat(_.pull(['Confirmed', 'Complete', 'Shipped', 'Received', 'Mailed', 'Processing', 'Canceled'], capitalize(test.attributes.status))),
                     test_id: Number(test.id),
                     sku: testList[Number(test.id)],
                     shipment_code: test.attributes.shipment_code
