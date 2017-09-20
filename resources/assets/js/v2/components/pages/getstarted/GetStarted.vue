26f71d63a25952c4c7c9b1a99d5088e4aabef01c
jonmcgill <jon.d.mcgill@gmail.com>
2017-09-20 10:26:02 -0400

    add explanatory comments on v2/GetStarted

--- resources/assets/js/v2/components/pages/getstarted/GetStarted.vue
+++ resources/assets/js/v2/components/pages/getstarted/GetStarted.vue
@@ -15,6 +15,12 @@
       Welcome,
     },
     beforeCreate() {
+      // The /get-started funnel is technically now only for users with a
+      // serviceable zip code, so we're going to reroute the user to
+      // /conditions if they do not have zip_validation data stored locally
+      // (which happens at the end of the /conditions funnel if the user's
+      // zip code is valid). If they do have zip_validation data stored,
+      // we assign that to conditions state and render the component.
       const zipValidation = localStorage.getItem('harvey_zip_validation');
       if (!zipValidation) {
         window.location.href = '/conditions';
@@ -23,6 +29,9 @@
       }
     },
     mounted() {
+      // If the user is logged in we want to make it so that they are warned
+      // before navigating away from the page that they're selections will be
+      // overwritten.
       if (App.Config.user.isLoggedIn) {
         window.onbeforeunload = () => {
           return 'All your information will be reset.';
