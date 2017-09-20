c493e6a6ce26ef13c21f02f99885cfd33529e04e
Andrew Rossi <aroccorossi@gmail.com>
2017-09-20 11:24:15 -0400

    adding shipment label url column

---
+++ database/migrations/2017_09_20_151801_add_url_column_to_lab_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUrlColumnToLabOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lab_orders', function (Blueprint $table) {
+            $table->text('shipment_label_url')->after('shipment_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('lab_orders', function (Blueprint $table) {
+          $table->dropColumn('shipment_label_url');
+      });
+    }
+}
