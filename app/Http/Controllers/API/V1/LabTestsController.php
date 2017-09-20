1617472b11b559264b0c17707cfb398396a1204a
Tate Peñaranda <tate@goharvey.com>
2017-09-20 20:52:05 -0300

    Allow LabTests 'shipment_code' to be updated .

--- app/Http/Controllers/API/V1/LabTestsController.php
+++ app/Http/Controllers/API/V1/LabTestsController.php
@@ -81,6 +81,7 @@ class LabTestsController extends BaseAPIController
 
         StrictValidator::checkUpdate($request->all(), [
             'status' => ['filled', Rule::in(LabTest::STATUSES)],
+            'shipment_code' => 'string',
         ]);
 
         $labTest->update($request->all());
