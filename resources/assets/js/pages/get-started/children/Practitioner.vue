d50c8132eaca33f60be8448f066fbd3e71564c8a
Kyle Hill <kyle@homehero.org>
2017-09-20 23:14:21 -0700

    Updated link to blog

--- resources/assets/js/pages/get-started/children/Practitioner.vue
+++ resources/assets/js/pages/get-started/children/Practitioner.vue
@@ -107,8 +107,8 @@ export default {
       isProcessing: false,
       store: this.$root.$data,
       socialIcons: [
-        { class: 'fa fa-medium', href: 'https://blog.goharvey.com/' },
-        { class: 'fa fa-instagram', href: 'https://www.instagram.com/goharveyapp/' },
+        { class: 'fa fa-medium', href: 'https://www.goharvey.com/blog' },
+        { class: 'fa fa-instagram', href: 'https://www.instagram.com/goharveyapp' },
         { class: 'fa fa-facebook', href: 'https://www.facebook.com/goharveyapp' },
         { class: 'fa fa-twitter', href: 'https://twitter.com/goharveyapp' },
         { class: 'fa fa-youtube', href: 'https://www.youtube.com/channel/UCNW4aHA1yCPUdk7OM65oNDw' },
