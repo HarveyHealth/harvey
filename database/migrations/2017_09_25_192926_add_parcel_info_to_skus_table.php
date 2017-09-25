661b33885f7b3ef904dc870d2fce9f08c529fc62
Andrew Rossi <aroccorossi@gmail.com>
2017-09-25 16:12:46 -0400

    spacing fixes

--- database/migrations/2017_09_25_192926_add_parcel_info_to_skus_table.php
+++ database/migrations/2017_09_25_192926_add_parcel_info_to_skus_table.php
@@ -24,15 +24,15 @@ class AddParcelInfoToSkusTable extends Migration
 
         // make sure only lab tests get dimensions
         foreach (\App\Models\SKU::all() as $sku) {
-          if ($sku->item_type === 'lab-test') {
-            $sku->weight = '2';
-            $sku->height = '12';
-            $sku->width = '4';
-            $sku->length = '12';
-            $sku->distance_unit = 'in';
-            $sku->mass_unit = 'lb';
-            $sku->save();
-          }
+            if ($sku->item_type === 'lab-test') {
+                $sku->weight = '2';
+                $sku->height = '12';
+                $sku->width = '4';
+                $sku->length = '12';
+                $sku->distance_unit = 'in';
+                $sku->mass_unit = 'lb';
+                $sku->save();
+            }
         }
     }
 
