1cf6f0f09216eb942060a81bfe4b6b1b7ec5224c
Andrew Rossi <aroccorossi@gmail.com>
2017-09-26 11:44:17 -0400

    creating from_address

--- app/Listeners/UpdateLabOrderWithShipmentLabel.php
+++ app/Listeners/UpdateLabOrderWithShipmentLabel.php
@@ -14,6 +14,35 @@ class UpdateLabOrderWithShipmentLabel implements ShouldQueue
         // call shippo
         // get data & save to db
 
-        return \Log::info('LabOrder has shipped ' . $event->labOrder->id);
+        // Example: https://github.com/goshippo/shippo-php-client/blob/master/examples/basic-shipment.php
+        $labTests = $event->labOrder->labTests()->get();
+
+        foreach ($labTests as $labTest) {
+          \Log::info('LabOrder has shipped with ' . $labTest->sku->id);
+        }
+
+        // From Address
+        $from_address = array(
+          'name' => '',
+          'company' => 'Harvey, Inc',
+          'street1' => '12655 W Jefferson Blvd',
+          'street2' => '#3-180',
+          'city' => 'Playa Vista',
+          'state' => 'CA',
+          'zip' => '90094',
+          'country' => 'US',
+          'phone' => '+1 800 690 9989',
+          'email' => 'support@goharvey.com',
+        );
+
+        /*
+        $shipment = Shippo_Shipment::create(
+        array(
+          'address_from'=> $from_address,
+          'address_to'=> $to_address,
+          'parcels'=> array($parcel),
+          'async'=> false,
+          ));
+        */
     }
 }
