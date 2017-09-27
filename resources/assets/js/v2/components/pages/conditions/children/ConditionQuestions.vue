9aec2446ed64e03729454937c74da0d21e3c1fae
jonmcgill <jon.d.mcgill@gmail.com>
2017-09-27 13:19:22 -0400

    style the condition question/answers section

--- resources/assets/js/v2/components/pages/conditions/children/ConditionQuestions.vue
+++ resources/assets/js/v2/components/pages/conditions/children/ConditionQuestions.vue
@@ -1,60 +1,76 @@
 <template>
-  <div>
-    <div class="block margin-0a max-width-md">
-      <SvgIcon :id="State('conditions.condition.image_url')" />
+  <div class="space-children-lg">
+    <div class="block font-centered margin-0a max-width-md">
+      <SvgIcon :id="State('conditions.condition.image_url')" :width="'80px'" :height="'80px'" />
     </div>
-    <button v-if="displayBack" @click="goBack">Back</button>
-    <button v-if="displayForward" @click="goForward">Forward</button>
-    <SlideIn v-for="(obj, qIndex) in State('conditions.condition.questions')" v-if="State('conditions.questionIndex') === qIndex" :key="qIndex">
-      <p>{{ obj.question }}</p>
-      <button v-for="(answer, aIndex) in obj.answers" :class="buttonClasses(aIndex)" @click="next(obj.question, answer, aIndex)">
-        {{ answer }}
+    <div class="margin-0a max-width-md" style="position: relative">
+      <button class="Button Button-condition-nav is-left" v-show="displayBack()" @click="goBack">
+        <i class="fa fa-chevron-left"></i>
       </button>
+      <button class="Button Button-condition-nav is-right" v-show="displayForward()" @click="goForward">
+        <i class="fa fa-chevron-right"></i>
+      </button>
+    </div>
+    <SlideIn v-for="(obj, qIndex) in State('conditions.condition.questions')" v-if="State('conditions.questionIndex') === qIndex" :key="qIndex">
+      <div class="font-centered margin-0a max-width-md">
+        <p class="heading-1">{{ obj.question }}</p>
+      </div>
+      <Row :gutter="'md'" class="space-top-xl">
+        <Column v-for="(answer, aIndex) in obj.answers" :config="{ md: '1of2' }" :key="aIndex">
+          <div :class="{'Button Button-answer':true, 'is-selected': answerIndex === aIndex}"
+               @click="next(obj.question, answer, aIndex)">
+            <span>{{ answer }}</span>
+            <i class="fa fa-check"></i>
+          </div>
+        </Column>
+      </Row>
     </SlideIn>
   </div>
 </template>
 
 <script>
-import { Util } from '../../../base';
+import { Layout, Util } from '../../../base';
 
 export default {
   components: {
+    Column: Layout.Column,
+    Row: Layout.Row,
     SlideIn: Util.SlideIn,
     SvgIcon: Util.SvgIcon,
   },
+  data() {
+    return {
+      hasAnswered: 0
+    }
+  },
   computed: {
+    answerIndex() {
+      const answers = this.State('conditions.answers');
+      const questionIndex = this.State('conditions.questionIndex');
+      return answers[questionIndex] ? answers[questionIndex].index : false;
+    }
+  },
+  methods: {
     displayBack() {
       return this.State('conditions.questionIndex') > 0;
     },
     displayForward() {
-      return this.State('conditions.answers').length > this.State('conditions.questionIndex');
-    }
-  },
-  methods: {
-    buttonClasses(answerIndex) {
-      const answers = this.State('conditions.answers');
-      const questionIndex = this.State('conditions.questionIndex');
-      return answers[questionIndex]
-        ? { button: true, 'is-selected': answers[questionIndex].index === answerIndex }
-        : { button: true };
+      return this.hasAnswered > this.State('conditions.questionIndex');
     },
     goBack() {
       return App.setState('conditions.questionIndex', this.State('conditions.questionIndex') - 1);
     },
     goForward() {
+      if (this.State('conditions.answers').length > this.hasAnswered) {
+        this.hasAnswered++;
+      }
       return App.setState('conditions.questionIndex', this.State('conditions.questionIndex') + 1);
     },
     next(question, answer, index) {
-      const answerIndex = this.State('conditions.questionIndex');
-      this.State('conditions.answers')[answerIndex] = { question, answer, index };
-      this.goForward();
+      const questionIndex = this.State('conditions.questionIndex');
+      Vue.set(this.State('conditions.answers'), questionIndex, { question, answer, index })
+      setTimeout(this.goForward, 400);
     }
   }
 }
 </script>
-
-<style>
-  .button.is-selected {
-    background: black;
-  }
-</style>
