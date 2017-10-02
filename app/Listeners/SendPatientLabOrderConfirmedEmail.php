6ec6ab4f11be43b3b5b7c517b3db7195abf96af8
Tate Peñaranda <tate@goharvey.com>
2017-10-02 19:24:47 -0300

    Typo fix.

--- app/Listeners/SendPatientLabOrderConfirmedEmail.php
+++ app/Listeners/SendPatientLabOrderConfirmedEmail.php
@@ -15,7 +15,7 @@ class SendPatientLabOrderConfirmedEmail implements ShouldQueue
 
         foreach ($invoice->items as $item) {
             $charges[] = [
-                'laboratory' => $item->sku->labTestInformation->lab_name,
+                'laboratory' => $item->sku->labTestInformation->lab_name ?? '',
                 'name' => $item->description,
                 'price' => $item->amount,
             ];
