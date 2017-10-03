75ef37747621c24be1aa82e2d63c0980a50e1630
Kyle Hill <kyle@homehero.org>
2017-10-03 02:43:38 -0700

    Possible fix to ipad bug

--- resources/views/legacy/_layouts/includes/top_nav.blade.php
+++ resources/views/legacy/_layouts/includes/top_nav.blade.php
@@ -9,11 +9,11 @@
         </div>
         <div class="nav-right">
             <span class="nav-item">
-                <a href="/about" class="link">About</a>
-                <a href="/lab-tests" class="link is-hidden-mobile">Lab Tests</a>
-                <a href="/#prices" class="link is-hidden-mobile">Pricing</a>
+                <a href="/about" class="link is-hidden-mobile">About</a>
+                <a href="/lab-tests" class="link">Lab Tests</a>
+                <a href="/#prices" class="link is-hidden-mobile is-hidden-tablet-only">Pricing</a>
                 <a href="tel:800-690-9989" class="button is-primary is-outlined is-hidden-mobile">(800) 690-9989</a>
-                <a href="/login" class="button is-primary is-outlined">Log In</a>
+                <a href="/login" class="button is-primary is-outlined is-hidden-mobile is-hidden-tablet-only">Log In</a>
                 <a href="/get-started" class="button is-primary">Get Started</a>
             </span>
         </div>
