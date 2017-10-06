351bcafeeeef04a6f231afa92b71d33124439dc2
Kyle Hill <kyle@homehero.org>
2017-10-06 15:03:48 -0700

    Move JS/CSS to CDN

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -28,8 +28,11 @@
         <link type="image/x-icon" rel="apple-touch-icon-precomposed" href="https://harvey-production.s3.amazonaws.com/assets/images/icon.png">
         <link type="image/x-icon" rel="shortcut icon" href="https://harvey-production.s3.amazonaws.com/assets/images/favicon.ico">
         <link type="image/x-icon" rel="icon" href="https://harvey-production.s3.amazonaws.com/assets/images/icon.png">
-        <!-- Stylesheets -->
+        <!-- Fonts -->
         @stack('stylesheets')
+        <script src="https://use.typekit.net/ukw4upn.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <!-- <link rel="stylesheet" href="https://unpkg.com/gh-font-awesome@1.0.0/index.css">-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
@@ -39,9 +42,15 @@
 
     <footer>
         <!-- Scripts -->
-        <script type="text/javascript" src="{{ mix('js/vendors/typekit.js') }}"></script>
-        @if (App::environment(['production', 'staging']))
-            <script type="text/javascript" src="{{ mix('js/vendors/intercom.js') }}"></script>
+        @if (App::environment(['production']))
+            <script type="text/javascript" src="https://unpkg.com/gh-intercom@1.0.0/index.js"></script>
+            <script type="text/javascript">
+                window.onload = function() {
+                    window.intercomSettings = {
+                        app_id: "tgn5rh80"
+                    };
+                }
+            </script>
         @endif
         <script>
             !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
