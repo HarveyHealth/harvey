56c6834e6e01a260c7f8a84c85eaeed90f3ff22a
laynefaler <laynefaler@gmail.com>
2017-10-06 14:08:07 -0700

    modified gulpfile

--- resources/assets/cdn/gulpfile.js
+++ resources/assets/cdn/gulpfile.js
@@ -4,20 +4,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
+var scssFilename = ''; // path
+var originalCss = ''; // path
+var minifiedCss = ''; // path
+var JSFilename = ''; // path
+var minifiedJS = ''; // path
+
 gulp.task('css', function () {
-	return gulp.src('./web/styles/index.scss')
+	return gulp.src(scssFilename)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('client.css'))
-		.pipe(gulp.dest('./assets/css'))
+		.pipe(concat('index.css'))
+		.pipe(gulp.dest(originalCss))
 		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(concat('client.min.css'))
-		.pipe(gulp.dest('./assets/dist'));
+		.pipe(concat('index.min.css'))
+		.pipe(gulp.dest(minifiedCss));
 });
 
 gulp.task('js', function () {
-	return gulp.src('./web/styles/index.scss')
+	return gulp.src(JSFilename)
 		.pipe(uglify())
-		.pipe(gulp.dest('./assets/dist'));
+		.pipe(gulp.dest(minifiedJS));
 });
 
 gulp.task('default', ['css', 'js']);
