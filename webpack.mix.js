388fb8ae3b4bce11c4f01de03f97b657cd428955
Jose Soto <josecanhelp@gmail.com>
2017-10-06 20:12:51 -0400

    Updated webpack mix file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const { mix } = require('laravel-mix');
+let SpritesmithPlugin = require('webpack-spritesmith');
+let path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -22,3 +24,35 @@ mix.options({processCssUrls: false})
 if (mix.inProduction()) {
     mix.version();
 }
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Custom Webpack Configuration
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a useful webpackConfig method that allows you to merge any short
+ | Webpack config overrides. This is a particularly appealing choice, it
+ | doesn't require you to copy and maintain a copy of the webpack.config.js file.
+ |
+ */
+
+
+mix.webpackConfig({
+    plugins: [
+        new SpritesmithPlugin({
+            src: {
+                cwd: path.resolve(__dirname, 'resources/assets/sprite'),
+                glob: '*.png'
+            },
+            target: {
+                image: path.resolve(__dirname, 'public/images/sprite.png'),
+                css: path.resolve(__dirname, 'resources/assets/scss/public/_sprite.scss')
+            },
+            retina: '@2x',
+            apiOptions: {
+                cssImageRef: "../../images/sprite.png"
+            }
+        })
+    ]
+});
