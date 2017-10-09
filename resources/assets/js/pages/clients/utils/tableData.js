24247c412de6c99a64c834ca36aafa9b87ad3f40
Andrew Rossi <aroccorossi@gmail.com>
2017-10-09 16:09:30 -0400

    reverting to prevent regression

--- resources/assets/js/pages/clients/utils/tableData.js
+++ resources/assets/js/pages/clients/utils/tableData.js
@@ -3,25 +3,22 @@ import moment from 'moment'
 
 
 export default function(clientList) {
-
-    console.log(clientList);
-
-    return clientList.data
+    return clientList
     .sort((a, b) => new Date(b.attributes.created_at.date) - new Date(a.attributes.created_at.date))
-    .map((client) => {
-        let data = client.attributes;
-        data['email_hyperlink'] = 0;
+    .map(e => {
+        let data = e.attributes
+        data['email_hyperlink'] = 0
         return {
             data: data,
             values: [
                 data.first_name && data.last_name ? `${_.capitalize(data.first_name)} ${_.capitalize(data.last_name)}` : 'N/A',
-                data.created_at ? moment(data.created_at.date).format('ddd, MMM Do') : 'N/A',
+                data.created_at ? moment(data.created_at.date).format('ddd, MMM Do YYYY') : 'N/A',
                 data.city && data.state ? `${data.city}, ${data.state}` : 'N/A',
                 data.doctor_name ? `Dr. ${data.doctor_name}` : 'N/A',
                 data.has_an_appointment ? 'Yes' : 'No',
                 data.has_completed_an_appointment ? 'Yes' : 'No'
-            ],
+            ]
         }
     })
-    .splice(0, 50);
+    .splice(0, 50)
 }
