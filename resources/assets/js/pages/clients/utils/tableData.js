1300f971fcb49aafdd91ec3cd401fd25a6b60f48
Andrew Rossi <aroccorossi@gmail.com>
2017-10-09 15:40:43 -0400

    remove year from date

--- resources/assets/js/pages/clients/utils/tableData.js
+++ resources/assets/js/pages/clients/utils/tableData.js
@@ -12,7 +12,7 @@ export default function(clientList) {
             data: data,
             values: [
                 data.first_name && data.last_name ? `${_.capitalize(data.first_name)} ${_.capitalize(data.last_name)}` : 'N/A',
-                data.created_at ? moment(data.created_at.date).format('ddd, MMM Do YYYY') : 'N/A',
+                data.created_at ? moment(data.created_at.date).format('ddd, MMM Do') : 'N/A',
                 data.city && data.state ? `${data.city}, ${data.state}` : 'N/A',
                 data.doctor_name ? `Dr. ${data.doctor_name}` : 'N/A',
                 data.has_an_appointment ? 'Yes' : 'No',
