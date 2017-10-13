d3bc0291fa93f31c0f4612484d350f1c0ae8c6ca
Tate Peñaranda <tate@goharvey.com>
2017-10-13 00:40:45 -0300

    Fix for tests.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use Carbon\Carbon;
 use Laravel\Passport\Client;
+use App\Lib\PractitionerAvailability;
 use App\Models\{
     Admin,
     Appointment,
@@ -119,8 +120,10 @@ $factory->define(PractitionerSchedule::class, function (Faker\Generator $faker) 
         $workableDays->push(Carbon::parse('next Monday')->addDay($i)->format('l'));
     }
 
-    $start_hour = rand(0, 22);
-    $start_time = "{$start_hour}:{$faker->randomElement([0, 30])}:00";
+    $start_block = collect(PractitionerAvailability::PREDEFINED_BLOCKS)->random();
+
+    list($start_hour, $star_minute) = explode(':', $start_block);
+    $start_time = "{$start_hour}:{$star_minute}";
 
     $stop_hour = rand($start_hour + 2, 24);
     $stop_minutes = (24 == $stop_hour) ? '00' : $faker->randomElement([0, 30]);
