fa3ab8e51c0993764b5700cdec3cfa16e67dcb17
laynefaler <laynefaler@gmail.com>
2017-10-23 16:02:25 -0700

    Added editor config for vue files as well as js files

--- resources/assets/js/commons/ConfirmInput.vue
+++ resources/assets/js/commons/ConfirmInput.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="phone-confirm-input-wrapper">
     <input ref="0" type="text"
-           :disabled="disabled"
-           @keydown="keyed($event, 0)"
-           @input="distribute($event, 0)" />
+        :disabled="disabled"
+        @keydown="keyed($event, 0)"
+        @input="distribute($event, 0)" />
     <input ref="1" type="text"
-           :disabled="disabled"
-           @keydown="keyed($event, 1)"
-           @input="distribute($event, 1)" />
+        :disabled="disabled"
+        @keydown="keyed($event, 1)"
+        @input="distribute($event, 1)" />
     <input ref="2" type="text"
-           :disabled="disabled"
-           @keydown="keyed($event, 2)"
-           @input="distribute($event, 2)" />
+        :disabled="disabled"
+        @keydown="keyed($event, 2)"
+        @input="distribute($event, 2)" />
     <input ref="3" type="text"
-           :disabled="disabled"
-           @keydown="keyed($event, 3)"
-           @input="distribute($event, 3)" />
+        :disabled="disabled"
+        @keydown="keyed($event, 3)"
+        @input="distribute($event, 3)" />
     <input ref="4" type="text"
-           :disabled="disabled"
-           @keydown="keyed($event, 4)"
-           @input="distribute($event, 4)" />
+        :disabled="disabled"
+        @keydown="keyed($event, 4)"
+        @input="distribute($event, 4)" />
   </div>
 </template>
 
