9415ee93c3f2e7021dbcb54e43c9d97fdac4dbf5
Tate Peñaranda <tate@goharvey.com>
2017-10-22 22:48:28 -0300

    Improvements for unit tests. - Mock for SMS class and method 'assertTextWasSent' added. - Mock of AppointmentObserver class to avoid creation of Google Calendar event every time an Appointment is created under testing environment. This new mock decreases a lot the time consumed to run the tests.

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,13 +3,15 @@
 namespace Tests;
 
 use App\Jobs\SendTransactionalEmail;
+use App\Lib\SMS;
+use App\Observers\AppointmentObserver;
 use Illuminate\Contracts\Console\Kernel;
-use Twilio\Rest\Client as Twilio;
 use Mockery;
 
 trait CreatesApplication
 {
-    protected $emailMock;
+    protected $email_mock;
+    protected $sms_mock;
 
     /**
      * Creates the application.
@@ -18,24 +20,26 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
         $app['env'] = 'testing';
 
-        app()->instance(Twilio::class, Mockery::mock(Twilio::class));
+        //Mock AppointmentObserver class to avoid creating Google Calendar events when testing.
+        $appointment_observer_mock = Mockery::mock(AppointmentObserver::class)->makePartial();
+        $appointment_observer_mock->shouldReceive('creating')->andReturn(true);
+        app()->instance(AppointmentObserver::class, $appointment_observer_mock);
 
-        $this->mockSendTransactionalEmail();
+        //Mock SMS class to avoid log entries and check if texts were sent.
+        $this->sms_mock = Mockery::mock(SMS::class)->makePartial();
+        $this->sms_mock->shouldReceive('sendMessageToNumber')->withAnyArgs()->andReturn(true);
+        app()->instance(SMS::class, $this->sms_mock);
+
+        //Mock SendTransactionalEmail class to be able to check if mails were sent.
+        $this->email_mock = Mockery::mock(SendTransactionalEmail::class)->makePartial();
+        app()->instance(SendTransactionalEmail::class, $this->email_mock);
 
         return $app;
     }
-
-    public function mockSendTransactionalEmail()
-    {
-        $this->emailMock = Mockery::mock(SendTransactionalEmail::class)->makePartial();
-
-        app()->instance(SendTransactionalEmail::class, $this->emailMock);
-    }
-
 }
