b08993507f2ceb33dbc132cc51c6a81036205010
Andrew Rossi <aroccorossi@gmail.com>
2017-10-25 11:04:04 -0400

    position the column after shipment_code

--- database/migrations/2017_10_25_145707_add_shippo_id_to_lab_orders_table.php
+++ database/migrations/2017_10_25_145707_add_shippo_id_to_lab_orders_table.php
@@ -14,7 +14,7 @@ class AddShippoIdToLabOrdersTable extends Migration
     public function up()
     {
         Schema::table('lab_orders', function (Blueprint $table) {
-            $table->string('shippo_id')->nullable();
+            $table->string('shippo_id')->after('shipment_code')->nullable();
         });
     }
 
