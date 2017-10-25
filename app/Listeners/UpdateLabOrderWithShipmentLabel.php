6fa410bcf2408c95428a17b8570d94cdb32f508a
Andrew Rossi <aroccorossi@gmail.com>
2017-10-25 10:14:56 -0400

    spacing fixes

--- app/Listeners/UpdateLabOrderWithShipmentLabel.php
+++ app/Listeners/UpdateLabOrderWithShipmentLabel.php
@@ -20,36 +20,36 @@ class UpdateLabOrderWithShipmentLabel implements ShouldQueue
 
         // From Address
         $address_from = [
-          'name' => 'Harvey, Inc',
-          'company' => 'Harvey, Inc',
-          'street1' => '12655 W Jefferson Blvd',
-          'street2' => 'Suite #3-180',
-          'city' => 'Los Angeles',
-          'state' => 'CA',
-          'zip' => '90066',
-          'country' => 'US',
-          'phone' => '+18006909989',
-          'email' => 'support@goharvey.com',
-          'test' => true,
+            'name' => 'Harvey, Inc',
+            'company' => 'Harvey, Inc',
+            'street1' => '12655 W Jefferson Blvd',
+            'street2' => 'Suite #3-180',
+            'city' => 'Los Angeles',
+            'state' => 'CA',
+            'zip' => '90066',
+            'country' => 'US',
+            'phone' => '+18006909989',
+            'email' => 'support@goharvey.com',
+            'test' => true,
         ];
 
         // To Address
         $address_to = [
-          'name' => $user->full_name,
-          'company' => '',
-          'street1' => $lab_order->address_1,
-          'street2' => $lab_order->address_2,
-          'city' => $lab_order->city,
-          'state' => $lab_order->state,
-          'zip' => $lab_order->zip,
-          'country' => 'US',
-          'phone' => $user->phone,
-          'email' => $user->email,
-          'test' => true,
+            'name' => $user->full_name,
+            'company' => '',
+            'street1' => $lab_order->address_1,
+            'street2' => $lab_order->address_2,
+            'city' => $lab_order->city,
+            'state' => $lab_order->state,
+            'zip' => $lab_order->zip,
+            'country' => 'US',
+            'phone' => $user->phone,
+            'email' => $user->email,
+            'test' => true,
         ];
 
         $parcel_info = $labTests->pluck('sku')->map(function($sku) {
-          return collect($sku->getAttributes())->only(['length', 'width', 'height', 'distance_unit', 'weight', 'mass_unit']);
+            return collect($sku->getAttributes())->only(['length', 'width', 'height', 'distance_unit', 'weight', 'mass_unit']);
         });
 
         // make sure we get our fedex account
@@ -58,16 +58,16 @@ class UpdateLabOrderWithShipmentLabel implements ShouldQueue
 
         // generate shipping label transaction from a single API call
         $transaction = \Shippo_Transaction::create([
-          'shipment' => [
-            'address_to' => $address_to,
-            'address_from' => $address_from,
-            'parcels' => $parcel_info,
-          ],
-          'carrier_account' => $carrier_object_id,
-          'servicelevel_token' => $carrier_service_level,
-          'label_file_type' => 'PDF',
-          'async' => false,
-          'test' => true,
+            'shipment' => [
+                'address_to' => $address_to,
+                'address_from' => $address_from,
+                'parcels' => $parcel_info,
+            ],
+            'carrier_account' => $carrier_object_id,
+            'servicelevel_token' => $carrier_service_level,
+            'label_file_type' => 'PDF',
+            'async' => false,
+            'test' => true,
         ]);
 
         \Log::info($transaction);
