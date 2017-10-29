d615e6ef5e0bc90a9a828c1917065d92f2e37e20
jonmcgill <jon.d.mcgill@gmail.com>
2017-10-28 22:22:35 -0400

    refactor practitioner card

--- resources/assets/js/v2/components/pages/dashboard/Dashboard.vue
+++ resources/assets/js/v2/components/pages/dashboard/Dashboard.vue
@@ -2,38 +2,57 @@
   <PageContainer>
     <PageHeader :heading="heading" />
     <div class="mw8 pa2 pa3-m">
-      <Grid :flexAt="'m'" :columns="topRowColumns" :gutters="{ s:2, m:3 }">
-        <Card :slot="1" :heading="'Appointments'">
-          <CardContent>Oh, boy</CardContent>
-        </Card>
-        <Card :slot="2" :heading="'Practitioner'" v-if="Config.user.isPatient">
-          <CardContent>Oh, boy</CardContent>
-        </Card>
-      </Grid>
+      <Loading v-if="!isDoneLoading" class="mt3" />
+      <SlideIn v-else>
+        <Grid :flexAt="'l'" :columns="topRowColumnConfig" :gutters="{ s:2, m:3 }">
+          <Card :slot="1" :heading="'Appointments'">
+            <CardContent>
+
+            </CardContent>
+          </Card>
+          <Card :slot="2" :heading="'Practitioner'" v-if="shouldShowDoctorInfo">
+            <AvatarCardHeading :heading="State('practitioners.userDoctor.attributes.name')" />
+            <CardContent>
+              <Paragraph>{{ State('practitioners.userDoctor.attributes.description') }}</Paragraph>
+            </CardContent>
+          </Card>
+        </Grid>
+      </SlideIn>
     </div>
   </PageContainer>
 </template>
 
 <script>
-import { Card, CardContent, Grid, PageHeader, PageContainer } from 'layout';
+import { Loading } from 'feedback';
+import { Paragraph } from 'typography';
+import { Card, CardContent, Grid, PageHeader, PageContainer, SlideIn, Space } from 'layout';
+import AvatarCardHeading from './AvatarCardHeading.vue';
 
 export default {
   name: 'dashboard',
-  components: {
-    Card,
-    CardContent,
-    Grid,
-    PageHeader,
-    PageContainer
-  },
+  components: { AvatarCardHeading, Card, CardContent, Grid, Loading, PageHeader, PageContainer, Paragraph, SlideIn, Space },
   data() {
     return {
-      heading: `${App.Config.user.isAdmin ? 'Admin ' : ''}Dashboard`,
-      topRowColumns: App.Config.user.isPatient ? [{ m:'1of2' }, { m:'1of2' }] : [{ m:'1of1' }]
+      heading: `${App.Config.user.isAdmin ? 'Admin ' : ''}Dashboard`
+    }
+  },
+  computed: {
+    isDoneLoading() {
+      return this.State('practitioners.data.all').length;
+    },
+    shouldShowDoctorInfo() {
+      return App.Config.user.isPatient && this.State('practitioners.userDoctor');
+    },
+    topRowColumnConfig() {
+      return this.shouldShowDoctorInfo ? [{ l:'1of2' }, { l:'1of2' }] : [{ l:'1of1' }];
     }
   },
   mounted() {
     App.setState('misc.currentPage', 'dashboard');
+
+    if (!this.State('practitioners.wasRequested')) {
+      App.Http.practitioners.get(App.Http.practitioners.getResponse);
+    }
   }
 }
 </script>
--- resources/assets/js/v2/components/typography/index.js
+++ resources/assets/js/v2/components/typography/index.js
@@ -1,3 +1,4 @@
 export { default as Heading1 } from './Heading1.vue';
+export { default as Heading2 } from './Heading2.vue';
 export { default as Heading3 } from './Heading3.vue';
 export { default as Paragraph } from './Paragraph.vue';
