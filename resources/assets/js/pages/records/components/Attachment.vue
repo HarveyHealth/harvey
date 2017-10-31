22b79a97e4e064f52ab6f2bb82b9bf66c66f5d69
laynefaler <laynefaler@gmail.com>
2017-10-31 12:23:24 -0700

    Viewing attachments and prescriptions

--- resources/assets/js/pages/records/components/Attachment.vue
+++ resources/assets/js/pages/records/components/Attachment.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="lab-body">
-        <div class="p-spacing">
-            <p>
-                You are about upload an attachment for client {{ patient.search_name }}, 
-                with adate of birth {{ patient.date_of_birth }}. 
-                Please verify the name of attachment before 
-                uploading, so we can keep things organized.
-                Anything you upload will be viewable to your doctor and doctor's assisstants only.
-                The only file format accepted is a PDF.
-            </p>
-        </div>
-        <div class="card-heading-container records-spacing">
-            <div class="width-175">
-                <label class="input__label">file upload</label>
-                <span class="custom-select bg-white">
-                    <select>
-                        <option v-for="attach in attachmentList">{{ attach.name }}</option>
-                    </select>
-                </span>
+    <div>
+        <div v-if="$parent.news" class="lab-body">
+            <div class="p-spacing">
+                <p>
+                    You are about upload an attachment for client {{ patient.search_name }}, 
+                    with adate of birth {{ patient.date_of_birth }}. 
+                    Please verify the name of attachment before 
+                    uploading, so we can keep things organized.
+                    Anything you upload will be viewable to your doctor and doctor's assisstants only.
+                    The only file format accepted is a PDF.
+                </p>
             </div>
-            <div class="width-175">
-                <label class="input__label">upload</label>
-                <div class="border-upload-container">
-                    <div class="upload-container">
-                        <i class="fa fa-book pdf-icons"></i>
-                        <p class="pdf-upload-text">Attachment (PDF)</p>
+            <div class="card-heading-container records-spacing">
+                <div class="width-175">
+                    <label class="input__label">file upload</label>
+                    <span class="custom-select bg-white">
+                        <select>
+                            <option v-for="attach in attachmentList">{{ attach.name }}</option>
+                        </select>
+                    </span>
+                </div>
+                <div class="width-175">
+                    <label class="input__label">upload</label>
+                    <div class="border-upload-container">
+                        <div class="upload-container">
+                            <i class="fa fa-book pdf-icons"></i>
+                            <p class="pdf-upload-text">Attachment (PDF)</p>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="record-image" v-if="!$parent.news">
+            <iframe class="record-image" :src="$parent.propData.attributes.url" />
         </div>
     </div>
 </template>
--- resources/assets/js/pages/records/components/Prescription.vue
+++ resources/assets/js/pages/records/components/Prescription.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="lab-body">
-        <div class="p-spacing">
-            <p>
-                You are about upload a prescription for client {{ patient.search_name }}, 
-                with adate of birth {{ patient.date_of_birth }}. 
-                Please verify the name of the pharmacy before 
-                uploading, so we can keep things organized.
-                Anything you upload will be viewable to your patient.
-                The only file format accepted is a PDF.
-            </p>
-        </div>
-        <div class="card-heading-container records-spacing">
-            <div class="width-175">
-                <label class="input__label">pharmacy</label>
-                <span class="custom-select bg-white">
-                    <select>
-                        <option v-for="script in prescriptionList">{{ script.name }}</option>
-                    </select>
-                </span>
+    <div>
+        <div v-if="$parent.news" class="lab-body">
+            <div class="p-spacing">
+                <p>
+                    You are about upload a prescription for client {{ patient.search_name }}, 
+                    with adate of birth {{ patient.date_of_birth }}. 
+                    Please verify the name of the pharmacy before 
+                    uploading, so we can keep things organized.
+                    Anything you upload will be viewable to your patient.
+                    The only file format accepted is a PDF.
+                </p>
             </div>
-            <div class="width-175">
-                <label class="input__label">upload</label>
-                <div class="border-upload-container">
-                    <div class="upload-container">
-                        <i class="fa fa-book pdf-icons"></i>
-                        <p class="pdf-upload-text">Prescription (PDF)</p>
+            <div class="card-heading-container records-spacing">
+                <div class="width-175">
+                    <label class="input__label">pharmacy</label>
+                    <span class="custom-select bg-white">
+                        <select>
+                            <option v-for="script in prescriptionList">{{ script.name }}</option>
+                        </select>
+                    </span>
+                </div>
+                <div class="width-175">
+                    <label class="input__label">upload</label>
+                    <div class="border-upload-container">
+                        <div class="upload-container">
+                            <i class="fa fa-book pdf-icons"></i>
+                            <p class="pdf-upload-text">Prescription (PDF)</p>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="record-image" v-if="!$parent.news">
+            <iframe class="record-image" :src="$parent.propData.attributes.url" />
         </div>
     </div>
 </template>
