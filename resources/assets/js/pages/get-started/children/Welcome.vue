ad3f2c155f0352cffe01c0f719670e85cdc5f71f
Kyle Hill <kyle@homehero.org>
2017-11-01 12:00:37 -0700

    Remove extra welcome.vue file

--- resources/assets/js/pages/get-started/children/Welcome.vue
+++ resources/assets/js/pages/get-started/children/Welcome.vue
@@ -3,11 +3,11 @@
     <div class="vertical-center">
       <div class="signup-container small naked">
         <div class="signup-main-icon">
-          <svg id="rocket"><use xlink:href="#checklist" /></svg>
+          <svg class="interstitial-icon icon-rocket"><use xlink:href="#rocket" /></svg>
         </div>
-        <h2 class="heading-1">Welcome to Harvey</h2>
+        <h2 class="heading-1 font-normal">Welcome to Harvey</h2>
         <p>You will need to answer a few basic questions before you can book a consultation with a Naturopathic Doctor.</p>
-        <p>This process will take 3-5 minutes. You will need to have your mobile phone ready to validate your phone number. After booking a consultation, we will ask you to fill out a more detailed patient intake form for your doctor.</p>
+        <p>This process will take 3-5 minutes. You will need to have your mobile phone ready to validate your phone number. After booking a consultation, we will ask you to fill out a more detailed client intake form for your doctor.</p>
         <button class="button button--blue" @click="$router.push('practitioner')">Let's Go!</button>
       </div>
     </div>
@@ -42,6 +42,7 @@ export default {
 
     analytics.page('Welcome');
     analytics.track('Welcome');
+    analytics.identify();
 
   },
   beforeDestroy() {
