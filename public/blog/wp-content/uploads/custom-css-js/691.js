79a9f151db76cc03131aeec644e3f61586ade9b2
Kyle Hill <kyle@homehero.org>
2017-11-01 17:51:04 -0700

    Make videos responsive

---
+++ public/blog/wp-content/uploads/custom-css-js/691.js
@@ -0,0 +1,11 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+
+    var youtube = document.getElementsByClassName("su-youtube")[0].className;
+    if (youtube.length > 0) {
+        var thumb = document.getElementsByClassName("blog-post-single-thumb")[0];
+        thumb.style.display = "none";
+    };
+
+</script>
+<!-- end Simple Custom CSS and JS -->
