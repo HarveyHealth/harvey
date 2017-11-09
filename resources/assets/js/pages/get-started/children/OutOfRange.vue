5db894a861b25757dce03a048771f03485f15217
jonmcgill <jon.d.mcgill@gmail.com>
2017-11-09 06:59:33 -0500

    bar logged-in user from signup form page

--- resources/assets/js/pages/get-started/children/OutOfRange.vue
+++ resources/assets/js/pages/get-started/children/OutOfRange.vue
@@ -2,7 +2,7 @@
   <div :class="containerClasses" v-if="!$root.$data.signup.completedSignup">
     <div class="vertical-center">
       <div class="signup-container small naked">
-        <router-link class="signup-back-button" :to="{ name: 'sign-up', path: 'signup' }">
+        <router-link class="signup-back-button" :to="{ name: 'welcome', path: 'welcome' }">
           <i class="fa fa-long-arrow-left"></i>
         </router-link>
         <div class="signup-main-icon">
