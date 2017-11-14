152bec0f014092aabc7e6009a8ab52801d1bb3b4
Javier Arias <javier@goharvey.com>
2017-11-14 13:58:09 -0300

    added support for resuming progress after error

--- app/Console/Commands/ImportUsersToFullscript.php
+++ app/Console/Commands/ImportUsersToFullscript.php
@@ -48,16 +48,30 @@ class ImportUsersToFullscript extends Command
 
         $bar = $this->output->createProgressBar($count);
 
+        // resume progress if last processed id is available
         $lastProcessedId = Redis::get(self::LAST_PROCESSED_ID_REDIS_KEY);
         if (is_numeric($lastProcessedId)) {
-            $bar->setProgress($lastProcessedId);
-            $query->where('id','>',$lastProcessedId);
+            if ($lastProcessedId < $query->max('id')){
+                $this->info('Resuming progress from id: ' . $lastProcessedId);
+                $bar->setProgress(User::has('patient')->where('id','<',$lastProcessedId)->count());
+                $query->where('id','>=',$lastProcessedId);
+            }
         }
 
-        foreach ($query->cursor() as $user){
-            dispatch(new CreateFullscriptPatient($user));
+        // proccess one by one
+        foreach ($query->orderBy('id','asc')->cursor() as $user){
+            try{
+                dispatch(new CreateFullscriptPatient($user));
+            }
+            catch(\Exception $e){
+                $bar->clear();
+                $this->info('Saving progress');
+                Redis::set(self::LAST_PROCESSED_ID_REDIS_KEY,$user->id);
+                throw $e;
+            }
             $bar->advance();
         }
+        Redis::set(self::LAST_PROCESSED_ID_REDIS_KEY,$user->id);
         $bar->finish();
     }
 }
