1dbf283129cf64a59d87662c4a2a28e1a4f60fa9
laynefaler <laynefaler@gmail.com>
2017-11-17 14:43:11 -0800

    Added style class for treatment plan

--- resources/assets/js/pages/records/components/Treatment.vue
+++ resources/assets/js/pages/records/components/Treatment.vue
@@ -1,22 +1,33 @@
 <template>
-    <div>
-        <div>
-            <img src="" alt="">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div>
-            <p></p>
+    <div class="records-container">
+        <div class="treatment-plan">
+            <textarea disabled v-model="plan" class="input--textarea soap-textarea" />
         </div>
     </div>
 </template>
 
 <script>
 export default {
-  
+    props: {
+        patient: Object,
+    },
+    computed: {
+        plan() {
+            return this.$parent.propData.attributes.plan;
+        }
+    },
+    watch: {
+        plan(val) {
+            if (!val) {
+                return this.$parent.propData.attributes.plan;
+            }
+        }
+    },
 };
 </script>
 
 <style lang="scss">
-
+    .treatment-plan {
+        padding: 20px 10px;
+    }
 </style>
