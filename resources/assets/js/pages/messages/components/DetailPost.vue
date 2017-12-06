20118abe9abd4ae562f3da7a0f787012f716b690
laynefaler <laynefaler@gmail.com>
2017-12-05 18:05:17 -0800

    Removing all messages

--- resources/assets/js/pages/messages/components/DetailPost.vue
+++ resources/assets/js/pages/messages/components/DetailPost.vue
@@ -35,7 +35,11 @@
             deleteMessages() {
                 axios.delete(`${this.$root.$data.apiUrl}/messages/${this.$props.id}`)
                 .then(() => {
-                    this.$root.$data.global.detailMessages[this.$props.threadId] = remove(this.$root.$data.global.detailMessages[this.$props.threadId], (n) =>  n.id == this.$props.id).slice(0);
+                    if (this.$root.$data.global.detailMessages[this.$props.threadId].length == 1) {
+                        this.$root.$data.global.detailMessages[this.$props.threadId] = [];
+                    } else {
+                        this.$root.$data.global.detailMessages[this.$props.threadId] = remove(this.$root.$data.global.detailMessages[this.$props.threadId], (n) =>  n.id == this.$props.id).slice(0);
+                    }
                     this.$root.$data.global.messages = Object.values(this.$root.$data.global.detailMessages)
                     .map(e => e[e.length - 1])
                     .sort((a, b) => b.id - a.id);                    
