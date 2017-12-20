15cd9251dee3b39160bca00b51604be909dcb49e
jonmcgill <jon.d.mcgill@gmail.com>
2017-12-20 15:00:54 -0500

    add users http request to retrieve intake information and render form/intake alert accordingly

--- resources/assets/js/v2/components/pages/dashboard/Dashboard.vue
+++ resources/assets/js/v2/components/pages/dashboard/Dashboard.vue
@@ -1,57 +1,60 @@
 <template>
-  <PageContainer>
-    <PageHeader :heading="Config.user.isAdmin ? 'Admin Dashboard' : 'Dashboard'" />
-    <div class="mw8 pa2 pa3-m">
-      <LoadingSpinner v-if="!isDoneLoading" class="mt3" />
-      <SlideIn v-else>
-        <Grid :flexAt="'l'" :columns="topRowColumnConfig" :gutters="{ s:2, m:3 }">
-          <Card :slot="1" :heading="'Appointments'">
-            <div v-if="upcomingAppointments.length">
-              <AppointmentCardInfo
-                v-for="(appt, i) in upcomingAppointments"
-                :appointment="appt"
-                :hasBorder="i < upcomingAppointments.length - 1"
-                :key="appt.id"
-              />
-            </div>
-            <CardContent v-else>
-              <Paragraph :weight="'thin'">No upcoming appointments scheduled</Paragraph>
-            </CardContent>
-          </Card>
-          <Card :slot="2" :heading="'Practitioner'" v-if="shouldShowDoctorInfo">
-            <AvatarCardHeading :heading="State('practitioners.userDoctor.attributes.name')" />
-            <CardContent>
-              <Paragraph :weight="'thin'">{{ State('practitioners.userDoctor.attributes.description') }}</Paragraph>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid :flexAt="'l'" :columns="[{ l:'1of2' }, { l:'1of2' }]" :gutters="{ s:2, m:3 }">
-          <Card :slot="1" :heading="'Contact Info'">
-            <AvatarCardHeading :heading="Util.misc.fullName(Config.user.info)" />
-            <CardContent>
-              <LabeledTextBlock :label="'Name'">{{ Util.misc.fullName(Config.user.info) }}</LabeledTextBlock>
-              <Spacer isBottom :size="3" />
-              <LabeledTextBlock :label="'Email'"><a :href="'tel:'+Config.user.info.email">{{ Config.user.info.email }}</a></LabeledTextBlock>
-              <Spacer isBottom :size="3" />
-              <LabeledTextBlock :label="'Phone'"><a :href="'tel:'+Config.user.info.phone">{{ Config.user.info.phone | formatPhone }}</a></LabeledTextBlock>
-              <Spacer isBottom :size="3" />
-              <LabeledTextBlock :label="'Location'">{{ Config.user.info.city }}, {{ Config.user.info.state }}</LabeledTextBlock>
-            </CardContent>
-          </Card>
-          <Card :slot="2" :heading="'Support'">
-            <AvatarCardHeading :heading="Config.support.name" />
-            <CardContent>
-              <LabeledTextBlock :label="'Support'"><a :href="'mailto:'+Config.support.email">{{ Config.support.email }}</a></LabeledTextBlock>
-              <Spacer isBottom :size="3" />
-              <LabeledTextBlock :label="'Phone'"><a :href="'tel:'+Config.support.phone">{{ Config.support.phone }}</a></LabeledTextBlock>
-              <Spacer isBottom :size="3" />
-              <LabeledTextBlock :label="'Available'">{{ Config.support.available }}</LabeledTextBlock>
-            </CardContent>
-          </Card>
-        </Grid>
-      </SlideIn>
-    </div>
-  </PageContainer>
+    <PageContainer>
+        <PageHeader :heading="pageHeading" />
+        <div class="mw8 pa2 pa3-m">
+            <LoadingSpinner v-if="!isDoneLoading" class="mt3" />
+            <SlideIn v-else>
+                <div v-if="shouldShowIntakeAlert">
+                    You should fill out the intake thingy
+                </div>
+                <Grid :flexAt="'l'" :columns="topRowColumnConfig" :gutters="{ s:2, m:3 }">
+                    <Card :slot="1" :heading="'Appointments'">
+                        <div v-if="upcomingAppointments.length">
+                            <AppointmentCardInfo
+                                v-for="(appt, i) in upcomingAppointments"
+                                :appointment="appt"
+                                :hasBorder="i < upcomingAppointments.length - 1"
+                                :key="appt.id"
+                            />
+                        </div>
+                        <CardContent v-else>
+                            <Paragraph :weight="'thin'">No upcoming appointments scheduled</Paragraph>
+                        </CardContent>
+                    </Card>
+                    <Card :slot="2" :heading="'Practitioner'" v-if="shouldShowDoctorInfo">
+                        <AvatarCardHeading :heading="State('practitioners.userDoctor.attributes.name')" />
+                        <CardContent>
+                            <Paragraph :weight="'thin'">{{ State('practitioners.userDoctor.attributes.description') }}</Paragraph>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid :flexAt="'l'" :columns="[{ l:'1of2' }, { l:'1of2' }]" :gutters="{ s:2, m:3 }">
+                    <Card :slot="1" :heading="'Contact Info'">
+                        <AvatarCardHeading :heading="Util.misc.fullName(Config.user.info)" />
+                        <CardContent>
+                            <LabeledTextBlock :label="'Name'">{{ Util.misc.fullName(Config.user.info) }}</LabeledTextBlock>
+                            <Spacer isBottom :size="3" />
+                            <LabeledTextBlock :label="'Email'"><a :href="'tel:'+Config.user.info.email">{{ Config.user.info.email }}</a></LabeledTextBlock>
+                            <Spacer isBottom :size="3" />
+                            <LabeledTextBlock :label="'Phone'"><a :href="'tel:'+Config.user.info.phone">{{ Config.user.info.phone | formatPhone }}</a></LabeledTextBlock>
+                            <Spacer isBottom :size="3" />
+                            <LabeledTextBlock :label="'Location'">{{ Config.user.info.city }}, {{ Config.user.info.state }}</LabeledTextBlock>
+                        </CardContent>
+                    </Card>
+                    <Card :slot="2" :heading="'Support'">
+                        <AvatarCardHeading :heading="Config.support.name" />
+                        <CardContent>
+                            <LabeledTextBlock :label="'Support'"><a :href="'mailto:'+Config.support.email">{{ Config.support.email }}</a></LabeledTextBlock>
+                            <Spacer isBottom :size="3" />
+                            <LabeledTextBlock :label="'Phone'"><a :href="'tel:'+Config.support.phone">{{ Config.support.phone }}</a></LabeledTextBlock>
+                            <Spacer isBottom :size="3" />
+                            <LabeledTextBlock :label="'Available'">{{ Config.support.available }}</LabeledTextBlock>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </SlideIn>
+        </div>
+    </PageContainer>
 </template>
 
 <script>
@@ -63,32 +66,57 @@ import AvatarCardHeading from './AvatarCardHeading.vue';
 import LabeledTextBlock from './LabeledTextBlock.vue';
 
 export default {
-  name: 'dashboard',
-  components: { AppointmentCardInfo, AvatarCardHeading, Card, CardContent, Grid, LabeledTextBlock, LoadingSpinner, PageHeader, PageContainer, Paragraph, SlideIn, Spacer },
-  computed: {
-    isDoneLoading() {
-      return this.State('practitioners.data.all').length && this.State('appointments.wasRequested.upcoming') && !this.State('appointments.isLoading.upcoming');
+    name: 'dashboard',
+    components: {
+        AppointmentCardInfo,
+        AvatarCardHeading,
+        Card,
+        CardContent,
+        Grid,
+        LabeledTextBlock,
+        LoadingSpinner,
+        PageHeader,
+        PageContainer,
+        Paragraph,
+        SlideIn,
+        Spacer
     },
-    shouldShowDoctorInfo() {
-      return App.Config.user.isPatient && this.State('practitioners.userDoctor');
+    computed: {
+        isDoneLoading() {
+            return this.State('practitioners.data.all').length && this.State('appointments.wasRequested.upcoming') && !this.State('appointments.isLoading.upcoming');
+        },
+        pageHeading() {
+            return App.Config.user.isAdmin ? 'Admin Dashboard' : 'Dashboard';
+        },
+        shouldShowDoctorInfo() {
+            return App.Config.user.isPatient && this.State('practitioners.userDoctor');
+        },
+        shouldShowIntakeAlert() {
+            return App.Config.user.isPatient &&
+                   this.State('users.intake.wasRequested') &&
+                  !this.State('users.intake.data.self');
+        },
+        topRowColumnConfig() {
+            return this.shouldShowDoctorInfo ? [{ l:'1of2' }, { l:'1of2' }] : [{ l:'1of1' }];
+        },
+        upcomingAppointments() {
+            return this.State('appointments.data.upcoming').filter(a => a.attributes.status === 'pending');
+        }
     },
-    topRowColumnConfig() {
-      return this.shouldShowDoctorInfo ? [{ l:'1of2' }, { l:'1of2' }] : [{ l:'1of1' }];
-    },
-    upcomingAppointments() {
-      return this.State('appointments.data.upcoming').filter(a => a.attributes.status === 'pending');
-    }
-  },
-  mounted() {
-    App.setState('misc.currentPage', 'dashboard');
+    mounted() {
+        App.setState('misc.currentPage', 'dashboard');
 
-    if (!this.State('appointments.wasRequested.upcoming')) {
-      App.Http.appointments.getUpcoming(App.Http.appointments.getUpcomingResponse);
-    }
+        if (!this.State('users.intake.data.self')) {
+            App.Http.users.getPatientIntake(App.Config.user.info.id);
+        }
 
-    if (!this.State('practitioners.wasRequested')) {
-      App.Http.practitioners.get(App.Http.practitioners.getResponse);
+        if (!this.State('appointments.wasRequested.upcoming')) {
+            App.Http.appointments.getUpcoming(App.Http.appointments.getUpcomingResponse);
+        }
+
+        if (!this.State('practitioners.wasRequested')) {
+            App.Http.practitioners.get(App.Http.practitioners.getResponse);
+        }
     }
-  }
 };
 </script>
--- resources/assets/js/v2/components/pages/intake/Intake.vue
+++ resources/assets/js/v2/components/pages/intake/Intake.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="typeform-container">
-        <a href="/dashboard" class="close-link">
+        <a href="/dashboard" class="close-link" v-show="shouldShowIntakeForm">
             <i class="fa fa-close" />
         </a>
         <div
+            v-show="shouldShowIntakeForm"
             class="typeform-widget"
             :data-url="typeformUrl"
             data-transparency=20
             data-hide-headers=false
             data-hide-footer=false
             style="width: 100%; height: 100vh;"
-        ></div>
+        />
+        <div v-if="!shouldShowIntakeForm">You already took the form thingy</div>
     </div>
 </template>
 
@@ -22,15 +24,22 @@ export default {
             typeformUrl: `https://goharvey.typeform.com/to/XGnCna?harvey_id=${Laravel.user.id}&intake_validation_token=${Laravel.user.intake_validation_token}`
         };
     },
+    computed: {
+        shouldShowIntakeForm() {
+            return this.State('users.intake.wasRequested') && !this.State('users.intake.data.self');
+        }
+    },
     mounted() {
-        // add Typeform script
-        /* eslint-disable */
-        (function() {
-            var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/";
-            if(!gi.call(d,id)) { js=ce.call(d,"script");
-            js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q); }
-        })();
-        /* eslint-enable */
+        App.Http.users.getPatientIntake(App.Config.user.info.id, () => {
+            // add Typeform script if intake data does not exist
+            if (!this.State('users.intake.data.self')) {
+                /* eslint-disable */
+                var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/";
+                if(!gi.call(d,id)) { js=ce.call(d,"script");
+                js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q); }
+                /* eslint-enable */
+            }
+        });
     }
 };
 </script>
