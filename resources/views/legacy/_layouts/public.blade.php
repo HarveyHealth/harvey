c802015d075ca00a5ef434ee3bd6fffe048a6398
laynefaler <laynefaler@gmail.com>
2017-12-21 10:59:27 -0800

    Fixed video minification

--- resources/views/legacy/_layouts/public.blade.php
+++ resources/views/legacy/_layouts/public.blade.php
@@ -34,55 +34,12 @@
 
     <script type="text/javascript" src="https://unpkg.com/gh-bideo@1.0.0/index.js"></script>
     <script type="text/javascript" async>
-        if (document.body.className.match('home')) {
-            var detectMobile = window.matchMedia('(max-width: 768px)').matches;
-            var detectIE = false;
-            var ua = window.navigator.userAgent;
-            var msie = ua.indexOf('MSIE ');
-            if (msie > 0) {
-                // IE 10 or older => return version number
-                // return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-                detectIE = true;
-            }
-            var trident = ua.indexOf('Trident/');
-            if (trident > 0) {
-                // IE 11 => return version number
-                // var rv = ua.indexOf('rv:');
-                // return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-                detectIE = true;
-            }
-            var edge = ua.indexOf('Edge/');
-            if (edge > 0) {
-               // Edge (IE 12+) => return version number
-               // return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-               detectIE = true;
-            }
-            try {
-                var videoLink = 'https://d35oe889gdmcln.cloudfront.net/assets/videos/hero-video.mp4';
-                var bv = new Bideo();
-                bv.init({
-                    videoEl: document.querySelector('#hero-video'),
-                    container: document.querySelector('body'),
-                    resize: true,
-                    autoplay: true,
-                    src: [{
-                        src: videoLink,
-                        type: 'video/mp4'
-                    }],
-                    onLoad: function() {
-                        // If not mobile and not IE, play video
-                        if ((detectMobile === false) && (detectIE === false)) {
-                            document.querySelector('#video-cover').style.display = 'none';
-                        }
-                    }
-                });
-            } catch(e){};
-        }
+        if (document.body.className.match('home')) {var detectMobile = window.matchMedia('(max-width: 768px)').matches;var detectIE = false;var ua = window.navigator.userAgent;var msie = ua.indexOf('MSIE ');if (msie > 0) {detectIE = true;}var trident = ua.indexOf('Trident/');if (trident > 0) {detectIE = true;}var edge = ua.indexOf('Edge/');if (edge > 0) {detectIE = true;}try {var videoLink = 'https://d35oe889gdmcln.cloudfront.net/assets/videos/hero-video.mp4';var bv = new Bideo();bv.init({videoEl: document.querySelector('#hero-video'),container: document.querySelector('body'),resize: true,autoplay: true,src: [{src: videoLink,type: 'video/mp4'}],onLoad: function() {if ((detectMobile === false) && (detectIE === false)) {document.querySelector('#video-cover').style.display = 'none';}}});} catch(e){};}
     </script>
     <script type="text/javascript" src="https://unpkg.com/gh-zepto@1.0.0/index.js" async></script>
     <script type="text/javascript" src="https://unpkg.com/gh-juicer-js@1.0.0/index.js" async></script>
     <script type="text/javascript" src="https://unpkg.com/gh-modernizr@1.0.0/index.js"></script>
-    <script type="text/javascript" src="{{ mix('js/app_public.js') }}" async></script>
+    <script type="text/javascript" src="{{ mix('js/app_public.js') }}"></script>
     <script type="text/javascript" src="https://unpkg.com/gh-lity-js@1.0.0/index.js" async></script>
     <script type="text/javascript">
         @isset($conditions)
