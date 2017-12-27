7f0aacca054ea61bab91d0f2ebf73facda7b38d9
Tate Peñaranda <tate@goharvey.com>
2017-12-27 23:47:59 +0100

    Fix for wrong foreign key table on lab_tests_log table migration.

--- app/Http/Traits/HasLogTable.php
+++ app/Http/Traits/HasLogTable.php
@@ -63,17 +63,17 @@ trait HasLogTable
     }
 
     public static function getLogTableSchema() {
-        $foreign_key = self::getModel()->getForeignKey();
-        $table_name = static::getLogTableName();
+        $model_foreign_key = self::getModel()->getForeignKey();
+        $model_table_name = self::getModel()->getTable();
 
-        return function (Blueprint $table) use ($foreign_key, $table_name) {
+        return function (Blueprint $table) use ($model_foreign_key, $model_table_name) {
             $table->increments('id');
-            $table->integer($foreign_key)->unsigned()->index();
+            $table->integer($model_foreign_key)->unsigned()->index();
             $table->string('attribute')->index();
             $table->string('from');
             $table->string('to');
             $table->integer('updated_by_user_id')->nullable()->unsigned()->index();
-            $table->foreign($foreign_key)->references('id')->on($table_name);
+            $table->foreign($model_foreign_key)->references('id')->on($model_table_name);
             $table->foreign('updated_by_user_id')->references('id')->on('users');
             $table->timestamps();
             $table->softDeletes();
