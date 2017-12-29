b51a755813615f4cc6881aef08bb1d514f9c0873
Javier Arias <javier@goharvey.com>
2017-12-29 16:47:52 -0300

    test fixes

--- tests/Feature/InvoicesTest.php
+++ tests/Feature/InvoicesTest.php
@@ -7,6 +7,7 @@ use App\Models\{
     Patient,
     Invoice,
     InvoiceItem,
+    Admin,
     User
 };
 
@@ -39,6 +40,66 @@ class InvoiceTest extends TestCase
         foreach ($response->original['data'] as $item) {
             $this->assertEquals($item['attributes']['patient_id'], $patient->id);
         }
+    }
+
+    public function test_it_does_not_allows_a_patient_to_view_others_invoices()
+    {
+        $patient = factory(Patient::class)->create();
+        $patient->invoices()->save(factory(Invoice::class)->make());
+
+        $patient1 = factory(Patient::class)->create();
+        $patient1->invoices()->save(factory(Invoice::class)->make());
+
+        Passport::actingAs($patient->user);
+        $response = $this->json('GET', "api/v1/invoices/{$patient->invoices()->first()->id}");
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        Passport::actingAs($patient1->user);
+        $response = $this->json('GET', "api/v1/invoices/{$patient->invoices()->first()->id}");
+        $response->assertStatus(ResponseCode::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_it_allows_an_admin_to_view_all_invoices()
+    {
+        factory(Patient::class, 2)->create()->each(function ($patient) {
+            $patient->invoices()->saveMany(factory(Invoice::class, 3)->make());
+        });
+
+        Passport::actingAs(factory(Admin::class)->create()->user);
+        $response = $this->json('GET', 'api/v1/invoices');
+
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        $this->assertCount(6, $response->original['data']);
+    }
+
+    public function test_it_can_include_items()
+    {
+        // Given a practitioner with a scheduled invoice
+        $invoice = factory(Invoice::class)->create();
+        $admin = factory(Admin::class)->create();
+
+        // When they attempt to view the information for a specific invoice
+        // and include patient and user info
+        Passport::actingAs($admin->user);
+        $response = $this->json('GET', "api/v1/invoices/{$invoice->id}?include=invoice_item");
+
+        // Then it is successful
+        $response->assertStatus(ResponseCode::HTTP_OK);
+
+        // And they can see the invoice information
+        $response->assertJsonStructure([
+            'data' => [
+                'type',
+                'id',
+                'attributes',
+                'links',
+                'relationships'
+            ],
+            'included' => [
+                'invoice_items'
+            ]
+        ]);
     }
 
 }
