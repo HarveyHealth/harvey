c21ca7e4acafedddf729fe7d729bd03daf70380b
jonmcgill <jon.d.mcgill@gmail.com>
2018-01-02 15:57:55 -0500

    handle no email or facebook cancellation with signup alerts

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Lib\ZipCodeValidator;
 use App\Models\{Appointment, Patient, User};
 use Illuminate\Http\Request;
-use Auth, Carbon, Socialite;
+use Auth, Carbon, Redirect, Socialite;
 
 class AuthController extends Controller
 {
@@ -35,10 +35,22 @@ class AuthController extends Controller
     // Obtain the user information from provider.  Check if the user already exists in our
     // database by looking up their provider_id in the database.
     // If the user exists, log them in. Otherwise, create a new user then log them in.
-    public function handleFacebookProviderCallback()
+    public function handleFacebookProviderCallback(Request $request)
     {
+        // Check if user accepted Facebook's requests
+        if (!$request->has('code') || $request->has('denied')) {
+            $facebook_redirect_alert = 'Facebook authorization rejected by user.';
+            return redirect('/get-started')->with(compact('facebook_redirect_alert'));
+        }
+
         // Grab facebook user information
         $user = Socialite::driver('facebook')->user();
+
+        // Check if user rejected email
+        if (!$request->has('email')) {
+            $facebook_redirect_alert = 'A valid email address is required for a Harvey account. Remove Harvey from your Facebook Apps settings and try again.';
+            return redirect('/get-started')->with(compact('facebook_redirect_alert'));
+        }
 
         // Determine if user currently exists from previous facebook signin
         if ($existing_user = User::where('facebook_provider_id', $user->id)->first()) {
@@ -55,6 +67,8 @@ class AuthController extends Controller
               return redirect($this->determineView($current_legacy_user));
             }
 
+            // A user may try using the button on the login page without having first gone through
+            // the zip code step. We'll redirect here if we do not have a zip in session.
             if (session('no_zip')) {
                 session(['no_zip' => false]);
                 return redirect(route('conditions'));
