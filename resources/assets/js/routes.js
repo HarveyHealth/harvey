4fa58677d322b8e2431f3545abb99ae78ab33943
laynefaler <laynefaler@gmail.com>
2018-01-03 14:09:01 -0800

    Removed the clients page from the user nav and remove the route

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -92,10 +92,6 @@ let routes = [
         component: require('./pages/lab_orders/LabOrders.vue')
     },
     {
-        path: '/clients',
-        component: require('./pages/clients/Clients.vue')
-    },
-    {
         path: '/records',
         component: require('./pages/records/Records.vue')
     },
