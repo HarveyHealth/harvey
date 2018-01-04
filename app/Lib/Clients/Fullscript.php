fb6aea8d5d9655c794b61849e0b58432541cf441
Tate Peñaranda <tpenaranda@users.noreply.github.com>
2018-01-04 16:37:59 +0100

    Fix for unit tests and some Fullscript method calls broken after merging BaseClient class. (#642)
    * Fix for unit tests and some Fullscript method calls.
    
    * Fix for BaseClient class methods calls.
    
    * Remove 'int' requirement for updatePatient param.
    
    * Fix for failing test on FullscriptClientTest class.
    
    * Don't encode body on the BaseClient side.
    
    * Carbon usage when setting 'date_of_birth' value.

--- app/Lib/Clients/Fullscript.php
+++ app/Lib/Clients/Fullscript.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Lib\Clients;
-/**
- *
- */
+
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Client;
+use Exception;
+
 class Fullscript extends BaseClient
 {
-    protected $base_endpoint = '';
+    protected $base_endpoint;
 
-    public function __construct($client = null)
+    public function __construct(Client $client = null)
     {
         parent::__construct($client);
         $this->headers = [
@@ -17,38 +19,39 @@ class Fullscript extends BaseClient
             'Content-Type' => 'application/json'
         ];
         $this->base_endpoint = config('services.fullscript.api_host');
-    }
 
+        if (empty($this->base_endpoint)) {
+            throw new Exception('Fullscript API Host not set, please check your ".env" or ".env.testing" file.');
+        }
+    }
 
     /**
      * List Patients by email or external_ref
      */
-    public function getPatients($external_ref = '', $email = '', $limit = null, $page = null){
+    public function getPatients($external_ref = '', $email = '', $limit = null, $page = null)
+    {
         // build arguments array
         $params = [];
         $params = array_filter(compact('external_ref', 'email', 'limit', 'page'));
 
         //perform call to API
 
-        try{
+        try {
             $response = $this->get('patients', $params);
-        }
-        catch(\GuzzleHttp\Exception\ClientException $e){
+        } catch (ClientException $e) {
             $response = $e->getResponse();
             if ($response->getStatusCode() == 400) {
                 throw new FullscriptClientException('bad input parameter', $response->getStatusCode());
-            }
-            else{
+            } else {
                 $message = $e->getMessage();
                 $body = json_decode($response->getBody());
-                if (isset($body->error)){
+                if (isset($body->error)) {
                     $message = $body->error;
                 }
                 // Exceeded rate limit 429
                 throw new FullscriptClientException($message, $response->getStatusCode());
             }
         }
-
 
         return json_decode($response->getBody());
     }
@@ -58,13 +61,11 @@ class Fullscript extends BaseClient
     * @param array $data
     * @return array the updated patient
     */
-    public function updatePatient($id, $data){
-        //perform call to API
-
-        try{
-            $response = $this->put("patients/{$id}", json_encode($data,JSON_FORCE_OBJECT));
-        }
-        catch(\GuzzleHttp\Exception\ClientException $e){
+    public function updatePatient($id, array $data)
+    {
+        try {
+            $response = $this->put("patients/{$id}", ['body' => json_encode($data, JSON_FORCE_OBJECT)]);
+        } catch (ClientException $e) {
             $response = $e->getResponse();
             switch ($response->getStatusCode()) {
                 case 400:
@@ -80,7 +81,7 @@ class Fullscript extends BaseClient
                 default:
                     $message = $e->getMessage();
                     $body = json_decode($response->getBody());
-                    if (isset($body->error)){
+                    if (isset($body->error)) {
                         $message = $body->error;
                     }
                     // Exceeded rate limit 429
@@ -97,15 +98,13 @@ class Fullscript extends BaseClient
         *  "last_name": "Wise",
         *  "email": "chris@fullscript.com",
         *  "date_of_birth": "1971-03-15",
-        *  "external_ref": "9991-2233-1222"
+        *  "external_ref": "<user_id>"
      */
-    public function createPatient($data){
-        //perform call to API
-
-        try{
-            $response = $this->post("patients", json_encode($data,JSON_FORCE_OBJECT));
-        }
-        catch(\GuzzleHttp\Exception\ClientException $e){
+    public function createPatient(array $data)
+    {
+        try {
+            $response = $this->post('patients', ['body' => json_encode($data, JSON_FORCE_OBJECT)]);
+        } catch (ClientException $e) {
             $response = $e->getResponse();
             switch ($response->getStatusCode()) {
                 case 405:
@@ -114,7 +113,7 @@ class Fullscript extends BaseClient
                 default:
                     $message = $e->getMessage();
                     $body = json_decode($response->getBody());
-                    if (isset($body->error)){
+                    if (isset($body->error)) {
                         $message = $body->error;
                     }
                     // Exceeded rate limit 429
@@ -124,8 +123,8 @@ class Fullscript extends BaseClient
 
         return json_decode($response->getBody());
     }
-
-
 }
 
-class FullscriptClientException extends \Exception{}
+class FullscriptClientException extends Exception
+{
+}
