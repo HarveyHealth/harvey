16606c277562be34da4a0afd0f5efa2303986f0b
laynefaler <laynefaler@gmail.com>
2018-01-05 15:39:56 -0800

    Working on some unit tests for messages

---
+++ tests/Frontend/pages/Messages/Messages.general.specs.js
@@ -0,0 +1,4 @@
+// import Vue from 'vue';
+// import Messages from '../../../../resources/assets/js/pages/messages/Messages.vue';
+// import AppStub from '../../AppStub';
+// import LaravelStub from '../../LaravelStub';
