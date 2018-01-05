b943598e04937988d9696217832af891387f6585
laynefaler <laynefaler@gmail.com>
2018-01-05 15:55:31 -0800

    Setup detail messages mounted unit test

--- tests/Frontend/pages/Messages/Messages.general.specs.js
+++ tests/Frontend/pages/Messages/Messages.general.specs.js
@@ -7,8 +7,8 @@ window.Laravel = LaravelStub;
 window.Vue = Vue;
 
 describe('Messages (General):', () => {
-    // const App = AppStub(Messages, 'Messages');
-    // const _Messages = App.vm.$children[0];
+    const App = AppStub(Messages, 'Messages');
+    const _Messages = App.vm.$children[0];
     
     context('When mounted', () => {
         it('it has a mounted hook (initial test)', () => {
