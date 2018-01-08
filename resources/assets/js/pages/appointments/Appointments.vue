0b7736d1706b660154b426e732822c6df06afa1a
laynefaler <laynefaler@gmail.com>
2018-01-08 12:26:44 -0800

    Updates to appointments

--- resources/assets/js/pages/appointments/Appointments.vue
+++ resources/assets/js/pages/appointments/Appointments.vue
@@ -115,7 +115,7 @@
         <label class="input__label">Billing Info</label>
         <div class="input__item">Duration: {{ appointment.currentDuration }}</div>
         <div class="input__item">Billed to: {{ billing.brand }} ****{{ billing.last4 }}</div>
-        <div class="input__item" data-test="appointment_amount_charged">Charged: {{ appointment.duration.data === '60' ? '$150' : '$75' }}</div>
+        <div class="input__item" data-test="appointment_amount_charged">Charged: {{ selectedRowData.amount ? '$' + Number(selectedRowData.amount).toFixed(0) : appointment.duration.data === '60' ? '$150' : '$75' }}</div>
       </div>
 
       <Purpose
