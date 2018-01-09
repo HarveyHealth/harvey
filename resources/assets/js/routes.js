536f487703675bee60553a890e53abdc94f9f02d
jonmcgill <jon.d.mcgill@gmail.com>
2018-01-09 12:10:52 -0500

    remove conditions components, state, config, vue route

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -27,10 +27,6 @@ switch(context) {
     rootRoute.name = 'dashboard';
     rootRoute.component = require('./v2/components/pages/dashboard/Dashboard');
     break;
-  case 'conditions':
-    rootRoute.name = 'conditions';
-    rootRoute.component = require('./v2/components/pages/conditions/Conditions.vue');
-    break;
   // case 'intake':
   //   rootRoute.name = 'intake';
   //   rootRoute.component = require('./pages/intake/Intake');
