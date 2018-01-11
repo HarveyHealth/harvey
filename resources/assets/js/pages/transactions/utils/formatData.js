56ad22cc3c12adc77fc7cfba0363b20ffb52cd56
laynefaler <laynefaler@gmail.com>
2018-01-11 15:18:39 -0800

    Added dollar signs to price and total

--- resources/assets/js/pages/transactions/utils/formatData.js
+++ resources/assets/js/pages/transactions/utils/formatData.js
@@ -7,8 +7,8 @@ export function formatTableData(dictionary, time) {
         data.date = moment.tz(e.attributes.paid_on.date, e.attributes.paid_on.timezone).tz(time).format('MMM Do YYYY, h:mma z');
         data.discount = e.attributes.discount;
         data.card = `${e.attributes.card_brand} **** ${e.attributes.card_last_four}`;
-        data.price = e.attributes.subtotal;
-        data.total = e.attributes.amount;
+        data.price = '$' + e.attributes.subtotal;
+        data.total = '$' + e.attributes.amount;
         data.patientId = e.attributes.patient_id;
         data.details = e.attributes.description;
         data.service = e.attributes.description;
