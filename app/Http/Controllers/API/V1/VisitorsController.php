3f820c2cde574b347ff10ae665a232b584611b18
Tate Peñaranda <tate@goharvey.com>
2018-01-23 21:46:57 +0100

    Added Harvey availability endpoint.

--- app/Http/Controllers/API/V1/VisitorsController.php
+++ app/Http/Controllers/API/V1/VisitorsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Lib\{TimeInterval, TransactionalEmail, Validation\StrictValidator};
+use App\Lib\Validation\StrictValidator;
+use App\Lib\{TimeInterval, TransactionalEmail, ZipCodeValidator};
+use App\Models\LabTest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Validation\Rule;
 use ResponseCode;
-use Illuminate\Support\Facades\Redis;
 
 class VisitorsController extends BaseAPIController
 {
@@ -37,5 +39,34 @@ class VisitorsController extends BaseAPIController
         Redis::expire($redisKey, TimeInterval::day()->toSeconds());
 
         return response()->json(['status' => 'Email sent.'], ResponseCode::HTTP_ACCEPTED);
+    }
+
+
+    public function getHarveyAvailability(Request $request)
+    {
+        $states_collection = ZipCodeValidator::getStatesCollection();
+
+        $consultations_serviceable_filter = function ($i) { return app(ZipCodeValidator::class)->isServiceable($i); };
+        $consultations_serviceable_states = $states_collection->filter($consultations_serviceable_filter)->values();
+
+        $availability = [
+            'data' => [
+                'consultations' => [
+                    'not_serviceable' => $states_collection->diff($consultations_serviceable_states)->values(),
+                    'serviceable' => $consultations_serviceable_states,
+                ],
+                'supplements' => [
+                    'not_serviceable' => [],
+                    'serviceable' => $states_collection,
+                ],
+                'lab_tests' => [
+                    'not_serviceable' => LabTest::getUnserviceableStatesCollection(),
+                    'require_consultation' => LabTest::getRequireConsultationStatesCollection(),
+                    'serviceable' => LabTest::getServiceableStatesCollection(),
+                ],
+            ],
+        ];
+
+        return response()->json($availability, ResponseCode::HTTP_OK);
     }
 }
