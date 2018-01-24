588c0cd83a3ed950e0e3f7ed7512f756f20c9ca9
jonmcgill <jon.d.mcgill@gmail.com>
2018-01-24 14:52:50 -0500

    setup controller, route, and blade

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,10 +12,6 @@ use App\Models\{Condition, LabTestInformation};
  */
 class PagesController extends Controller
 {
-    protected function sendConditionsView($conditions, $lab_tests, $index, $get_zip)
-    {
-        return view('pages.conditions')->with(compact('conditions', 'lab_tests', 'index', 'get_zip'));
-    }
     /**
      * Show the home page.
      *
@@ -46,29 +42,13 @@ class PagesController extends Controller
         return view('legacy.pages.lab_tests')->with(compact('lab_tests', 'index'));
     }
 
-    public function getConditions()
-    {
-        // If no condition is specified, redirect to first condition
-        return redirect("/conditions/".Condition::first()->slug);
-    }
-
-    public function getCondition(string $conditionSlug = null)
-    {
-        if($conditionSlug === 'get-zip') {
-            return $this->sendConditionsView(Condition::getAllFromCache(), null, null, true);
-        }
-
-        $condition = Condition::where('slug', $conditionSlug)->first();
-
-        // If valid condition, render view with conditions and proper index
-        // Else redirect to first condition in db
-        return $condition
-            ? $this->sendConditionsView(Condition::getAllFromCache(), LabTestInformation::publicFromCache(), $condition->id - 1, false)
-            : redirect("/conditions/".Condition::first()->slug);
-    }
-
     public function getConsultations()
     {
         return view('legacy.pages.consultations');
+    }
+
+    public function getAvailability()
+    {
+        return view('legacy.pages.availability');
     }
 }
---
+++ resources/views/legacy/pages/availability.blade.php
@@ -0,0 +1,8 @@
+@extends('legacy._layouts.public')
+@section('page_title','Availability')
+@section('body_class','availability')
+@section('main_content')
+<section>
+    <h1>Availability</h1>
+</section>
+@endsection
