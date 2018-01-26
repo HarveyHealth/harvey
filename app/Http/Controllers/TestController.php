949b1889bae8cc79d844ea5ab15ecc3d28d2207e
Tate Peñaranda <tate@goharvey.com>
2018-01-27 00:48:54 +0100

    Clean up of TestController.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,35 +9,6 @@ class TestController extends Controller
 {
     public function index()
     {
-    	$customer_id = 'cus_A4qUQuiPXt1vWV';
-
-    	$amount = 15000;
-
-    	$data = [
-            		'customer' => $customer_id,
-            		'amount' => $amount,
-            		'currency' => 'usd',
-            		'description' => 'Because I wanted to, man'
-            	];
-
-        try {
-
-        	$charge = \Stripe\Charge::create($data);
-        	print_r($charge);
-
-        	if ($charge->paid) {
-
-        		echo $charge->source->last4;
-
-        	} else {
-
-        	}
-        	
-        } catch (\Exception $e) {
-
-        	print_r($e->getMessage());
-        	exit;
-        }
-        
-	}
+        return 'This is a test.';
+    }
 }
