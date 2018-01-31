873a1360c8f13ef8cb32c7541667a75e7baac4c7
laynefaler <laynefaler@gmail.com>
2018-01-31 10:21:32 -0800

    Updates to messages v2

--- resources/assets/js/v2/components/pages/messages/Messages.vue
+++ resources/assets/js/v2/components/pages/messages/Messages.vue
@@ -18,7 +18,7 @@ export default {
         PageContainer,
         PageHeader,
         AddNewButton,
-        CardMessage,
+        CardMessage
     },
     mounted() {
         App.State.misc.currentPage = 'messages';
