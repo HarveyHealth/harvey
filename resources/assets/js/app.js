31118ea5d47ef31a5a70da26634cc7fcfdbe4c7c
Layne F <14255968+laynef@users.noreply.github.com>
2018-02-02 08:46:44 -0800

    [Har 1329] Added date of birth field in profile (#688)
    * Working on adding date of birth on the profile page
    
    * Working on date format
    
    * Uncaught linter changes
    
    * Fixed formating
    
    * Added vue mask on date
    
    * Added birthdates to profile page
    
    * Fixed admin view
    
    * Profile admin select updates
    
    * Fixed practitioner view
    
    * No date of birth for practitioners
    
    * Profile date of birth for patients only

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -410,7 +410,15 @@ const app = new Vue({
                 .then(response => {
                     let data = response.data.data;
                     if (response.data.included) {
-                        data.included = response.data.included[0];
+                        let includes = response.data.included[0];
+                        if (includes && includes.attributes && includes.attributes.birthdate && includes.attributes.birthdate.date) {
+                            includes.attributes.birthdate.format_date = moment(includes.attributes.birthdate.date).format('MM/DD/YYYY');
+                        } else {
+                            includes.attributes.birthdate = { format_date: '' };
+                        }
+                        data.included = includes;
+                    } else {
+                        data.included = { attributes: { birthdate: { format_date: '' } } };
                     }
                     this.global.user = data;
                     this.global.loadingUser = false;
